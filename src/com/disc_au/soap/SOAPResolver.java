47266e8079696b4687387187f8a054c34dc69588
Xander Plooy <xander.plooy@autogeneral.com.au>
2013-03-25 06:52:30 +0000

    EPI-6 Automated Deployment: web_ctm  - working on “packaged WAR deployment” compatibility:   - DEBUG COMMIT: com.disc_au.soap.SOAPClientThread class check config root for XSL System ID, is possibly ending up as null inside a WAR package   - adjust the SOAPResolver class (debug output??)…
    git-svn-id: http://itrepos/apps/web_ctm/deploy_PRELIVE@2112 5f447213-944a-4844-baf2-6ecc2693b7b8

--- src/com/disc_au/soap/SOAPResolver.java
+++ src/com/disc_au/soap/SOAPResolver.java
@@ -3,13 +3,15 @@ package com.disc_au.soap;
 import java.io.InputStream;
 
 import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.URIResolver;
 import javax.xml.transform.stream.StreamSource;
 
 public class SOAPResolver implements URIResolver {
-	public Source resolve(String href, String base) {
+	public Source resolve(String href, String base) throws TransformerException {
+System.out.print("RESOLVARRRRR HREF " + href + " | BASE " + base + " | ");
 		InputStream resolverStream = this.getClass().getClassLoader().getResourceAsStream(href);
-System.out.println("RESOLVARRRRR HREF " + href + " | BASE " + base + " | STREAM " + resolverStream.toString());
+System.out.println("STREAM " + resolverStream.toString());
 		return new StreamSource(resolverStream);
 	}
 }
