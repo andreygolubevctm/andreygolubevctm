cdc25b333136a8524c5ba926d53c07a5b913d193
Xander Plooy <xander.plooy@autogeneral.com.au>
2013-03-26 01:14:06 +0000

    EPI-6 Automated Deployment: web_ctm  - working on “packaged WAR deployment” compatibility:   - DEBUG COMMIT: com.disc_au.soap.SOAPClientThread class check config root for XSL System ID, is possibly ending up as null inside a WAR package   - trying a known “systemid” file for the SystemId property…
    git-svn-id: http://itrepos/apps/web_ctm/deploy_PRELIVE@2138 5f447213-944a-4844-baf2-6ecc2693b7b8

--- src/com/disc_au/soap/SOAPClientThread.java
+++ src/com/disc_au/soap/SOAPClientThread.java
@@ -588,7 +588,8 @@ public class SOAPClientThread implements Runnable {
 	 * @return the string
 	 */
 	private String translate(String xslFile, String xml, String parms, String requestXml) {
-		URL systemId = this.getClass().getClassLoader().getResource(xslFile);
+//		URL systemId = this.getClass().getClassLoader().getResource(xslFile);
+		URL systemId = this.getClass().getClassLoader().getResource("../aggregator/systemid");
 		Source xsltSource = new StreamSource(this.getClass().getClassLoader().getResourceAsStream(xslFile));
 //		xsltSource.setSystemId(systemId.toString());
 System.out.println("TRANSLATE XSL FILE: " + xslFile + " | CONFIG ROOT " + this.configRoot + " | SYSTEM ID: " + systemId + " | RESOURCE " + this.getClass().getClassLoader().getResource(this.configRoot) + " | XSL FILE RESOURCE " + this.getClass().getClassLoader().getResource(xslFile));
