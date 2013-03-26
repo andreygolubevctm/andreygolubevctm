c40a8294a6f8e4a9c042a08cbd2b173058590598
Xander Plooy <xander.plooy@autogeneral.com.au>
2013-03-26 06:40:28 +0000

    EPI-6 Automated Deployment: web_ctm  - working on “packaged WAR deployment” compatibility:   - DEBUG COMMIT: com.disc_au.soap.SOAPClientThread class check config root for XSL System ID, is possibly ending up as null inside a WAR package   - try the custom SOAPResolver base path without the leading slash…
    git-svn-id: http://itrepos/apps/web_ctm/deploy_PRELIVE@2182 5f447213-944a-4844-baf2-6ecc2693b7b8

--- src/com/disc_au/soap/SOAPResolver.java
+++ src/com/disc_au/soap/SOAPResolver.java
@@ -10,7 +10,7 @@ import javax.xml.transform.stream.StreamSource;
 public class SOAPResolver implements URIResolver {
 	public Source resolve(String href, String base) throws TransformerException {
 //System.out.print("SOAPResolver called: HREF " + href + " | BASE " + base + " | VOODOO DOLL " + base.replaceFirst("^.*/WEB-INF(/classes)?(.+)$", "$2") + href + " KITTENS | ");
-		String resolverFile = base.replaceFirst("^.*/WEB-INF(/classes)?(.+)$", "$2") + href;
+		String resolverFile = base.replaceFirst("^.*/WEB-INF/(classes/)?(.+)$", "$2") + href;
 System.out.println("RESOLVAR " + resolverFile);
 
 		// First, try on the classpath (assume given path has a leading slash)
@@ -18,7 +18,7 @@ System.out.println("RESOLVAR " + resolverFile);
 
 		// If that fails, do a folder hierarchy dance to support looking more locally (non-packed-WAR environment)
 		if ( xsltSourceInput == null ) {
-			resolverFile = ".." + resolverFile;
+			resolverFile = "../" + resolverFile;
 System.out.println("FAILED AT PATH, GOING UP: " + resolverFile);
 			xsltSourceInput = this.getClass().getClassLoader().getResourceAsStream(resolverFile);
 		}
