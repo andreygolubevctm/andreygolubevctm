22e561dfd8a4315902d846767c15c272d88f490e
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-06-29 11:52:47 +1000

    Reverted out commit that should have gone against CAR-1013 Jira Job CAR-1026
    git-svn-id: http://itrepos/apps/web_ctm/trunk@22306 5f447213-944a-4844-baf2-6ecc2693b7b8
    (cherry picked from commit c6accfa)

--- database-deployment-scripts/toBeScheduled/CAR-1013 Add AI to list of providers on choosi.sql
+++
@@ -1,8 +0,0 @@
-/** Updater **/
-UPDATE ctm.content_control SET contentValue='Budget Direct, Ozicare, 1st for Women, Real Insurance and AI Insurance' WHERE verticalId=3 AND styleCodeId=8 AND contentKey='footerParticipatingSuppliers';
-
-/** Checker - returns one result to confirm contentValue field **/
-SELECT * FROM ctm.content_control WHERE verticalId=3 AND styleCodeId=8 AND contentKey='footerParticipatingSuppliers';
-
-/** Rollback **/
--- UPDATE ctm.content_control SET contentValue='Budget Direct,  Ozicare, 1st for Women and Real Insurance' WHERE verticalId=3 AND styleCodeId=8 AND contentKey='footerParticipatingSuppliers';
