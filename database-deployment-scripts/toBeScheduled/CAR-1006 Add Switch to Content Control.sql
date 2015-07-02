fe258ad752b9bb0975620819410b205c41e71d3c
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-07-02 10:42:42 +1000

    Modified so that override switch defaults to N (so it's off yo) Jira Job CAR-1006
    git-svn-id: http://itrepos/apps/web_ctm/trunk@22487 5f447213-944a-4844-baf2-6ecc2693b7b8
    (cherry picked from commit 9f7f294)

--- database-deployment-scripts/toBeScheduled/CAR-1006 Add Switch to Content Control.sql
+++ database-deployment-scripts/toBeScheduled/CAR-1006 Add Switch to Content Control.sql
@@ -1,5 +1,5 @@
 /** Updater **/
-INSERT INTO ctm.content_control (styleCodeId, verticalId, contentCode, contentKey, effectiveStart, effectiveEnd, contentValue) VALUES ('1', '3', 'RegoLookup', 'regoLookupIsAvailable', '2014-06-02 00:00:00', '2040-12-31 23:59:59', 'Y');
+INSERT INTO ctm.content_control (styleCodeId, verticalId, contentCode, contentKey, effectiveStart, effectiveEnd, contentValue) VALUES ('1', '3', 'RegoLookup', 'regoLookupIsAvailable', '2014-06-02 00:00:00', '2040-12-31 23:59:59', 'N');
 
 /** Checker - One record exists after update **/
 SELECT * FROM ctm.content_control WHERE styleCodeId=1 AND verticalId=3 AND contentCode='RegoLookup' AND contentKey='regoLookupIsAvailable';
