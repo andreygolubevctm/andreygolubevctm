cd8e051e0d8c02832da63029b894ce409f869b1d
Fernando Leon <Fernando.Leon@comparethemarket.com.au>
2015-07-03 15:37:35 +1000

    Forgot to add rollback on sql Jira Job AGG-2230
    git-svn-id: http://itrepos/apps/web_ctm/trunk@22582 5f447213-944a-4844-baf2-6ecc2693b7b8
    (cherry picked from commit c2a6471)

--- database-deployment-scripts/toBeScheduled/AGG-2230-marketing_optIn_text_standard.sql
+++ database-deployment-scripts/toBeScheduled/AGG-2230-marketing_optIn_text_standard.sql
@@ -2,7 +2,7 @@
 SET @NEW_CONTENT_VAL = 'I agree to receive news &amp; offer emails from comparethemarket.com.au &amp; the insurance provider that presents the lowest price.';
 SET @OLD_CONTENT_VAL = 'I agree to receive news &amp; offer emails from Compare the Market &amp; the insurance provider that presents the lowest price.';
 
--- Tester Expect 0
+-- Tester  BEFORE update Expect 0
 select count(contentControlId) from ctm.content_control where contentKey = 'okToEmail' and styleCodeId = 1 and verticalId = 7 and contentCode = 'Optin' and
  contentValue like @NEW_CONTENT_VAL;
 
@@ -17,9 +17,16 @@ UPDATE ctm.content_control
  and styleCodeId = 1 and verticalId = 7 and contentCode = 'Optin'
  and contentValue like @OLD_CONTENT_VAL;
 
--- Tester expect same as Tester b
+-- Tester AFTER update expect same as Tester b
 
 select count(contentControlId) from ctm.content_control where contentKey = 'okToEmail' and styleCodeId = 1 and verticalId = 7 and contentCode = 'Optin' and
  contentValue like @NEW_CONTENT_VAL;
 
-
+/*
+-- rollback
+ UPDATE ctm.content_control 
+ set contentValue = @OLD_CONTENT_VAL 
+ where contentKey = 'okToEmail' 
+ and styleCodeId = 1 and verticalId = 7 and contentCode = 'Optin'
+ and contentValue like @NEW_CONTENT_VAL;
+*/
