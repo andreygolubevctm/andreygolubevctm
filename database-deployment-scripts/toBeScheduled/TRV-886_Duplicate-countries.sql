274c8d9cf9aed89975d14fa06ebd63c00d0b9d87
Fernando Leon <Fernando.Leon@comparethemarket.com.au>
2015-07-07 08:39:36 +1000

    sql for removing duplicate countries Jira Job TRV-886
    git-svn-id: http://itrepos/apps/web_ctm/trunk@22641 5f447213-944a-4844-baf2-6ecc2693b7b8
    (cherry picked from commit 7f0230a)

---
+++ database-deployment-scripts/toBeScheduled/TRV-886_Duplicate-countries.sql
@@ -0,0 +1,11 @@
+/*tester expect 1*/
+select count(*) from ctm.country_master where countryName like 'Bali';
+
+delete from ctm.country_master where isoCode = 'BAL';
+
+/* tester expect 0*/
+select count(*) from ctm.country_master where countryName like 'Bali';
+
+/* roll back 
+insert into ctm.country_master (isoCode,countryName) values ('ID','Bali');
+*/
