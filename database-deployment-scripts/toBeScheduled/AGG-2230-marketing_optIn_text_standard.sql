403bc91960bbbfb4891b6a3c7733cfac340780b8
Fernando Leon <Fernando.Leon@comparethemarket.com.au>
2015-07-09 14:09:03 +1000

    putting limits on the update and testing expectations.
    Jira Job AGG-2230
    
    git-svn-id: http://itrepos/apps/web_ctm/trunk@22752 5f447213-944a-4844-baf2-6ecc2693b7b8
    (cherry picked from commit 5e617a9)

--- database-deployment-scripts/toBeScheduled/AGG-2230-marketing_optIn_text_standard.sql
+++ database-deployment-scripts/toBeScheduled/AGG-2230-marketing_optIn_text_standard.sql
@@ -3,26 +3,32 @@ SET @NEW_CONTENT_VAL = 'I agree to receive news &amp; offer emails from comparet
 SET @OLD_CONTENT_VAL = 'I agree to receive news &amp; offer emails from Compare the Market &amp; the insurance provider that presents the lowest price.';
 
 -- Tester  BEFORE update Expect 0
+SET @NEW_CONTENT_VAL = 'I agree to receive news &amp; offer emails from comparethemarket.com.au &amp; the insurance provider that presents the lowest price.';
 select count(contentControlId) from ctm.content_control where contentKey = 'okToEmail' and styleCodeId = 1 and verticalId = 7 and contentCode = 'Optin' and
  contentValue like @NEW_CONTENT_VAL;
 
--- Tester B note value as this is should be the same the last test
+-- Tester B expect 1
+SET @OLD_CONTENT_VAL = 'I agree to receive news &amp; offer emails from Compare the Market &amp; the insurance provider that presents the lowest price.';
 select count(contentControlId) from ctm.content_control where contentKey = 'okToEmail' and styleCodeId = 1 and verticalId = 7 and contentCode = 'Optin' and
  contentValue like @OLD_CONTENT_VAL;
 
 -- Updater
+SET @OLD_CONTENT_VAL = 'I agree to receive news &amp; offer emails from Compare the Market &amp; the insurance provider that presents the lowest price.';
+SET @NEW_CONTENT_VAL = 'I agree to receive news &amp; offer emails from comparethemarket.com.au &amp; the insurance provider that presents the lowest price.'; 
 UPDATE ctm.content_control 
  set contentValue = @NEW_CONTENT_VAL 
  where contentKey = 'okToEmail' 
  and styleCodeId = 1 and verticalId = 7 and contentCode = 'Optin'
- and contentValue like @OLD_CONTENT_VAL;
+ and contentValue like @OLD_CONTENT_VAL LIMIT 1;
 
--- Tester AFTER update expect same as Tester b
-
+-- Tester AFTER update expect 1
+SET @NEW_CONTENT_VAL = 'I agree to receive news &amp; offer emails from comparethemarket.com.au &amp; the insurance provider that presents the lowest price.';
 select count(contentControlId) from ctm.content_control where contentKey = 'okToEmail' and styleCodeId = 1 and verticalId = 7 and contentCode = 'Optin' and
  contentValue like @NEW_CONTENT_VAL;
 
 /*
+SET @OLD_CONTENT_VAL = 'I agree to receive news &amp; offer emails from Compare the Market &amp; the insurance provider that presents the lowest price.';
+SET @NEW_CONTENT_VAL = 'I agree to receive news &amp; offer emails from comparethemarket.com.au &amp; the insurance provider that presents the lowest price.'; 
 -- rollback
  UPDATE ctm.content_control 
  set contentValue = @OLD_CONTENT_VAL 
