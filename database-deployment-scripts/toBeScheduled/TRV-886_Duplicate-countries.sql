b88cd14d624d18287e36513d974d6c52a522b06a
Fernando Leon <Fernando.Leon@comparethemarket.com.au>
2015-07-09 10:39:20 +1000

    just added the limit 1 on delete Jira Job TRV-886
    git-svn-id: http://itrepos/apps/web_ctm/trunk@22733 5f447213-944a-4844-baf2-6ecc2693b7b8
    (cherry picked from commit c7f7386)

--- database-deployment-scripts/toBeScheduled/TRV-886_Duplicate-countries.sql
+++ database-deployment-scripts/toBeScheduled/TRV-886_Duplicate-countries.sql
@@ -1,7 +1,7 @@
 /*tester expect 1*/
 select count(*) from ctm.country_master where countryName like 'Bali';
 
-delete from ctm.country_master where isoCode = 'BAL';
+delete from ctm.country_master where isoCode = 'BAL' LIMIT 1;
 
 /* tester expect 0*/
 select count(*) from ctm.country_master where countryName like 'Bali';
