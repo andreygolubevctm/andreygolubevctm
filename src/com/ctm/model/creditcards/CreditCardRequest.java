6c45f1f83b1e9fbf3648e93baa02612f0364d55c
Vitor Oba <vitor.oba@comparethemarket.com.au>
2015-07-14 16:03:58 +1000

    CTMIT-468 Added @NotEmpty in CreditCardRequest

--- src/com/ctm/model/creditcards/CreditCardRequest.java
+++ src/com/ctm/model/creditcards/CreditCardRequest.java
@@ -3,18 +3,22 @@ package com.ctm.model.creditcards;
 import com.ctm.web.validation.Name;
 import com.ctm.web.validation.Numeric;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * Created by voba on 26/06/2015.
  */
 public class CreditCardRequest {
     @Name
+    @NotEmpty(message = "Please enter your full name")
     private String name;
 
     @Email
+    @NotEmpty(message = "Please enter your email")
     private String email;
 
     @Numeric(message = "Please enter a valid postcode")
+    @NotEmpty(message = "Please enter the Postcode/Suburb")
     private String postcode;
 
     public String getPostcode() {
