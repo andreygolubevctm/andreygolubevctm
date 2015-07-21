a89e2b13e74ecea0649d0e8e7232594366048def
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2015-07-21 12:26:45 +1000

    UTL-295: Fix "postal address is the same" on iThings

--- WebContent/framework/modules/js/utilities/utilitiesEnquiryFields.js
+++ WebContent/framework/modules/js/utilities/utilitiesEnquiryFields.js
@@ -78,9 +78,10 @@
 
         // If the residential and postal addresses are the same on entering this page, tick the postal match checkbox
         var residentialAddressValues = _getSerializedAddressValues($(".elasticsearch_container_utilities_application_details_address")),
-            postalAddressValues = _getSerializedAddressValues($(".elasticsearch_container_utilities_application_details_postal"));
-        if(residentialAddressValues == postalAddressValues)
-            $("#utilities_application_details_postalMatch").trigger("click");
+            postalAddressValues = _getSerializedAddressValues($(".elasticsearch_container_utilities_application_details_postal")),
+            isPostalMatch = (residentialAddressValues == postalAddressValues);
+
+        $("#utilities_application_details_postalMatch").prop("checked", isPostalMatch).trigger("change");
     }
 
     function _getSerializedAddressValues($el) {
