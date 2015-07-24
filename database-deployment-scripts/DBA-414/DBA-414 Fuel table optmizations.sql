d573154507bea7c0ac4a5ab903c20014da5f5ed9
Vitor Oba <vitor.oba@comparethemarket.com.au>
2015-07-24 10:12:25 +1000

    DBA-414: Add fuel_results_regional.state to be enum

--- database-deployment-scripts/DBA-414/DBA-414 Fuel table optmizations.sql
+++ database-deployment-scripts/DBA-414/DBA-414 Fuel table optmizations.sql
@@ -14,6 +14,7 @@ ALTER ONLINE TABLE `aggregator`.`fuel_results` CHANGE Price Price SMALLINT UNSIG
 ALTER ONLINE TABLE `aggregator`.`fuel_results_regional` CHANGE UpdateId UpdateId SMALLINT UNSIGNED NOT NULL;
 ALTER ONLINE TABLE `aggregator`.`fuel_results_regional` CHANGE FuelId FuelId TINYINT UNSIGNED NOT NULL; -- Values from 2-9
 ALTER ONLINE TABLE `aggregator`.`fuel_results_regional` CHANGE AvgPrice AvgPrice SMALLINT UNSIGNED NOT NULL;
+ALTER ONLINE TABLE `aggregator`.`fuel_results_regional` CHANGE `state` `state` ENUM('ACT','NSW','NT','QLD','SA','UNK','VIC','WA') NOT NULL DEFAULT 'UNK';
 -- ALTER ONLINE TABLE `aggregator`.`fuel_results` CHANGE RecordedTime RecordedTime DATETIME DEFAULT '0000-00-00'; ????????
 
 -- fuel_updates
