e74acaa7aa5d065d5e048c215badfaaa03b19f5d
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2015-07-27 10:48:40 +1000

    UTL-297: Enable session cam for UTL

---
+++ database-deployment-scripts/toBeScheduled/UTL-297 Enable user tracking.sql
@@ -0,0 +1,9 @@
+-- Test
+SELECT *
+FROM ctm.content_control
+WHERE contentKey = "userTrackingEnabled"
+AND verticalId = 5
+AND styleCodeId = 1;
+
+-- Run
+INSERT INTO `ctm`.`content_control` (`styleCodeId`, `verticalId`, `contentCode`, `contentKey`, `effectiveStart`, `effectiveEnd`, `contentValue`) VALUES ('1', '5', 'User Tracking', 'userTrackingEnabled', '2015-07-27 00:00:00', '2038-01-01 00:00:00', 'Y');
