ccf655328f6ff610725b169f133bddc6515fd2ac
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2015-07-28 16:55:46 +1000

    HLT-2450: * Added fix * Added unit test

---
+++ test/java/com/ctm/services/simples/SimplesSearchServiceTest.java
@@ -0,0 +1,41 @@
+package com.ctm.services.simples;
+
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SimplesSearchServiceTest {
+
+    @Test
+    public void shouldHandleWhitespace() throws Exception {
+        SimplesSearchService simplesSearchService = new SimplesSearchService();
+        PageContext pageContext = mock(PageContext.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(pageContext.getRequest()).thenReturn(request);
+        when(request.getParameter("search_terms")).thenReturn("Bobby McTest");
+        simplesSearchService.init(pageContext);
+        assertEquals(SimplesSearchService.SearchMode.OTHER, simplesSearchService.getSearchMode());
+
+        when(request.getParameter("search_terms")).thenReturn(" Bobby McSpace");
+        simplesSearchService.init(pageContext);
+        assertEquals(SimplesSearchService.SearchMode.OTHER, simplesSearchService.getSearchMode());
+
+        when(request.getParameter("search_terms")).thenReturn(null);
+        simplesSearchService.init(pageContext);
+        assertEquals(SimplesSearchService.SearchMode.OTHER, simplesSearchService.getSearchMode());
+
+        when(request.getParameter("search_terms")).thenReturn("0455555555");
+        simplesSearchService.init(pageContext);
+        assertEquals(SimplesSearchService.SearchMode.PHONE, simplesSearchService.getSearchMode());
+
+        when(request.getParameter("search_terms")).thenReturn(" 0455555505");
+        simplesSearchService.init(pageContext);
+        assertEquals(SimplesSearchService.SearchMode.PHONE, simplesSearchService.getSearchMode());
+
+    }
+}
