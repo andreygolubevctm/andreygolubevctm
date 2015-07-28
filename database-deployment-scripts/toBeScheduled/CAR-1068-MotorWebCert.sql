92030cfad8238159dd377cf51758437e2799dbab
twilson <timothy.wilson@comparethemarket.com.au>
2015-07-28 14:21:27 +1000

    CAR-1068 - Updates to Service config

--- database-deployment-scripts/toBeScheduled/CAR-1068-MotorWebCert.sql
+++ database-deployment-scripts/toBeScheduled/CAR-1068-MotorWebCert.sql
@@ -1,7 +1,15 @@
+/* Vars */
+SET @SERVICE_MASTER_ID = (SELECT `serviceMasterId` FROM `ctm`.`service_master` WHERE `verticalId` = '0' AND `serviceCode` = 'motorwebRegoLookupService');
+
+/* Update Dev certificate location */
+UPDATE `ctm`.`service_properties` SET `servicePropertyValue` = 'WEB-INF/classes/motorweb-certificate-testEnv.p12'
+  WHERE `serviceMasterId` = @SERVICE_MASTER_ID AND servicePropertyKey = 'certificate' AND `environmentCode` = 0;
+
+/* Insert prod certificate location */
+INSERT INTO `ctm`.`service_properties` (`serviceMasterId`, `environmentCode`, `styleCodeId`, `providerId`, `servicePropertyKey`, `servicePropertyValue`, `effectiveStart`, `effectiveEnd`, `scope`)
+VALUES
+  (@SERVICE_MASTER_ID, 'PRO', 0, 0, "password", "jvgfhsn", "2015-05-28 00:00:00", "2040-12-12 11:59:59", "SERVICE"),
+  (@SERVICE_MASTER_ID, 'PRO', 0, 0, "certificate", "WEB-INF/classes/motorweb-certificate-prodEnv.p12", "2015-05-28 00:00:00", "2040-12-12 11:59:59", "SERVICE");
+
 /* Test */
-SELECT * from ctm.service_properties
-WHERE serviceMasterId = (SELECT serviceMasterId FROM ctm.service_master WHERE serviceCode = 'motorwebRegoLookupService') and servicePropertyKey = 'certificate';
-
-UPDATE service_properties SET servicePropertyValue = 'WEB-INF/classes/motorweb-certificate.p12'
-WHERE serviceMasterId = (SELECT serviceMasterId FROM ctm.service_master WHERE serviceCode = 'motorwebRegoLookupService') and servicePropertyKey = 'certificate';
-
+SELECT * FROM `ctm`.`service_properties` WHERE `serviceMasterId` = @SERVICE_MASTER_ID and `servicePropertyKey` = 'certificate';
