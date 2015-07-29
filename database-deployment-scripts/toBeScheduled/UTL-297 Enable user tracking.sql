3ad4643bdff71e0b840ed12c541447fe10ed1eaa
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2015-07-29 10:27:19 +1000

    UTL-297: Change effective end

--- database-deployment-scripts/toBeScheduled/UTL-297 Enable user tracking.sql
+++ database-deployment-scripts/toBeScheduled/UTL-297 Enable user tracking.sql
@@ -6,4 +6,4 @@ AND verticalId = 5
 AND styleCodeId = 1;
 
 -- Run
-INSERT INTO `ctm`.`content_control` (`styleCodeId`, `verticalId`, `contentCode`, `contentKey`, `effectiveStart`, `effectiveEnd`, `contentValue`) VALUES ('1', '5', 'User Tracking', 'userTrackingEnabled', '2015-07-27 00:00:00', '2038-01-01 00:00:00', 'Y');
+INSERT INTO `ctm`.`content_control` (`styleCodeId`, `verticalId`, `contentCode`, `contentKey`, `effectiveStart`, `effectiveEnd`, `contentValue`) VALUES ('1', '5', 'User Tracking', 'userTrackingEnabled', '2015-07-27 00:00:00', '2015-09-15 00:00:00', 'Y');
