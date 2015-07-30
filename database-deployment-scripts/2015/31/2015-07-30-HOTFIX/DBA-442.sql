d72d08ceb20e7ab4ed896cbcd75e4e819307bb7b
David Degnan <david.degnan@comparethemarket.com.au>
2015-07-30 14:10:11 +1000

    Added update to repopulate Tasmanian sites with state

--- database-deployment-scripts/2015/31/2015-07-30-HOTFIX/DBA-442.sql
+++ database-deployment-scripts/2015/31/2015-07-30-HOTFIX/DBA-442.sql
@@ -1,2 +1,4 @@
 ALTER TABLE aggregator.fuel_sites
 	CHANGE COLUMN `state` `state` ENUM('ACT','NSW','NT','QLD','SA','TAS','UNK','VIC','WA') NOT NULL DEFAULT 'UNK' AFTER `Name`;
+
+UPDATE aggregator.fuel_sites SET state='TAS' WHERE state='' AND postcode LIKE ('7%');
