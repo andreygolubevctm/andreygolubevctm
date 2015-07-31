84d27d14fc02564f93450479d7c320c3fd633beb
Adrian Diente <adrian.diente@comparethemarket.com.au>
2015-07-31 15:49:43 +1000

    TRV-948: tweaked rollback script

--- database-deployment-scripts/2015/32/TRV-948-ticks-client-code-added-to-db.sql
+++ database-deployment-scripts/2015/32/TRV-948-ticks-client-code-added-to-db.sql
@@ -12,6 +12,6 @@ UPDATE `ctm`.`service_properties` SET `servicePropertyValue`='SPName=au.com.Comp
 -- [TEST] SELECT count(servicePropertiesId) AS total FROM ctm.service_properties WHERE providerid=323 AND servicePropertyKey = 'outBoundParams' AND servicePropertyValue = 'SPName=au.com.Compare';
 -- BEFORE UPDATE RESULT: 0
 
--- UPDATE `ctm`.`service_properties` SET `servicePropertyValue`='SPName=au.com.Compare,clientID=507' WHERE providerid=323 AND servicePropertyKey = 'outBoundParams' AND servicePropertyValue = 'SPName=au.com.Compare,clientID=507';
+-- UPDATE `ctm`.`service_properties` SET `servicePropertyValue`='SPName=au.com.Compare' WHERE providerid=323 AND servicePropertyKey = 'outBoundParams' AND servicePropertyValue = 'SPName=au.com.Compare,clientID=507';
 
 -- AFTER UPDATE RESULT: 1
