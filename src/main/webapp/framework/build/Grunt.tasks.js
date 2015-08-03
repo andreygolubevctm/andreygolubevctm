9c22312a37fae42da4738db4fa406176ca3a4d29
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2015-08-03 10:14:00 +1000

    TRVBER-355: structure change

--- src/main/webapp/framework/build/Grunt.tasks.js
+++ src/main/webapp/framework/build/Grunt.tasks.js
@@ -65,7 +65,7 @@ module.exports = function(grunt,tools,brandMapping,rootOverride){
 		dest: tools.getBrandFile(brand,'js','dest','bootstrap'),
 		options: {
 			sourceMap: tools.getBrandFile(brand,'js','dest','bootstrap') + '.map',
-		sourceMappingURL: tools.relativizer(tools.getBrandFile(brand,'js','dest','bootstrap') + '.map','../../../')//override - fine grained control - use a function. NOTE: Fixes insane relative root magic with the ../'s
+			sourceMappingURL: tools.relativizer(tools.getBrandFile(brand,'js','dest','bootstrap') + '.map','../../../')//override - fine grained control - use a function. NOTE: Fixes insane relative root magic with the ../'s
 		}
 	};
 
@@ -160,7 +160,7 @@ module.exports = function(grunt,tools,brandMapping,rootOverride){
 	jshint.gruntfile = { src: '../Gruntfile.js' }; //TODO: should probably check this file too.
 	jshint.meerkat = { src: [tools.getFrameworkPath('meerkat') + '*.js'] }; // Source files to JSHint
 	jshint.modules = { src: [tools.getFrameworkPath('modules','js/core') + '*.js'] };
-	
+
 	//Dynamic task name creation
 	verticals.forEach(function(vertical) {
 		jshint['modules_'+vertical] = {
@@ -208,7 +208,7 @@ module.exports = function(grunt,tools,brandMapping,rootOverride){
 //----------------------------------------------------------------------
 
 //The csslint API is a bit awkward: For each option (rule), a value of false ignores the rule, a value of 2 will set it to become an error. Otherwise all rules are considered warnings.
-	
+
 	csslint.options = {
 		csslintrc: tools.getFrameworkPath('modules','less') + '.csslintrc'
 	};
@@ -231,15 +231,15 @@ module.exports = function(grunt,tools,brandMapping,rootOverride){
 //----------------------------------------------------------------------
 // LESS: Compile LESS to CSS
 //----------------------------------------------------------------------
-/**
- * Up until recently the tools we're using grunt-recess to compile LESS to CSS.
- * Since the RECESS project hasn't updated and I wanted to take advantage
- * of source maps, i've replaced grunt-recess with grunt-contrib-less.
- * grunt-contrib-less comes with LESS 1.6.0 and allows us to enable source
- * maps.
- * NOTE: Sourcemaps get their paths repaired by the replace task below!
- * You can specify multiple builds, including minification, etc.
- */
+	/**
+	 * Up until recently the tools we're using grunt-recess to compile LESS to CSS.
+	 * Since the RECESS project hasn't updated and I wanted to take advantage
+	 * of source maps, i've replaced grunt-recess with grunt-contrib-less.
+	 * grunt-contrib-less comes with LESS 1.6.0 and allows us to enable source
+	 * maps.
+	 * NOTE: Sourcemaps get their paths repaired by the replace task below!
+	 * You can specify multiple builds, including minification, etc.
+	 */
 
 	less.options = {
 		strictmath: true,
@@ -377,7 +377,7 @@ module.exports = function(grunt,tools,brandMapping,rootOverride){
 	tools.getComponents().forEach(function(folder) {
 		folder = "components_" + folder;
 
-        watch[brand + "_" + folder + "_modules"] = {
+		watch[brand + "_" + folder + "_modules"] = {
 			files: ['<%= uglify.' + brand + '_' + folder + '_modules.src %>'],
 			tasks: ['jshint:modules_' + folder, 'build_' + brand + '_' + folder + '_js']
 		};
@@ -503,16 +503,16 @@ module.exports = function(grunt,tools,brandMapping,rootOverride){
 	};
 
 
-/********************************************************************************
- * Return dynamic grunt tasks to be loaded in the gruntfile.
- */
+	/********************************************************************************
+	 * Return dynamic grunt tasks to be loaded in the gruntfile.
+	 */
 	return {
-		pkg: grunt.file.readJSON('../package.json'),
+		pkg: grunt.file.readJSON('../frontend/package.json'),
 		banner: '/*!\n'  +
-				' * <%= pkg.name %> v<%= pkg.version %>\n'  +
-				' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n'  +
-				' * <%= pkg.homepage %>\n'  +
-				' */\n\n',
+		' * <%= pkg.name %> v<%= pkg.version %>\n'  +
+		' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n'  +
+		' * <%= pkg.homepage %>\n'  +
+		' */\n\n',
 		uglify: uglify,
 		jshint: jshint,
 		cssmetrics: cssmetrics,
@@ -522,113 +522,113 @@ module.exports = function(grunt,tools,brandMapping,rootOverride){
 		watch: watch,
 		notify: notify
 	}
-	
+
 };
 
 
 //----------------------------------------------------------------------
 // Deprecated code, experimental or things to keep around:
 //----------------------------------------------------------------------
-	
-	//shell: { tasks: [{ cmd: 'whoami' }] } //Example way to call shell commands
 
-	/**
-	 * Runs tasks in parallel threads.
-	 * This unfortunately for now, just adds overhead for our 'quick' tasks.
-	 * Explicit definitions give fine control on concurrency.
-	 */
-	/*
-	concurrent: {
-		jshint_all: ['jshint:gruntfile','jshint:meerkat','jshint:modules'],
-		less_brand: ['less:brand','less:brand_min'],
-		js_brand: ['concat:brand_meerkat_modules','uglify:brand_meerkat_modules']
-		//this is a special option - only used to get the jshint happening while the less is being done too, and then we concurrent the js_brand. Efficient!
-		, less_jshint_brand: ['concurrent:less_brand','concurrent:jshint_all']
-	},
-	*/
+//shell: { tasks: [{ cmd: 'whoami' }] } //Example way to call shell commands
 
-	/**
-	 * Javascript Code style checker is awesome, but this will require a lot 
-	 * of changes to make our style consistant, and we'll get to many conflicts
-	 * to do this during this heavy dev.
-	 * package.json entry:  "grunt-jscs-checker": "~0.4.1"
-	 */
-	/*
-	jscs: {
-		options: {
-			config: tools.getFrameworkPath('bootstrap','js') + '.jscs.json',
-		},
-		grunt: {
-			src: 'Gruntfile.js' // This file
-		}//,
-		//modules: {
-		//	src: [
-		//		tools.getFrameworkPath('modules','js/core') + '*.js',
-		//		tools.getFrameworkPath('modules','js/<%= mapping.vertical %>') + '*.js'
-		//	] // Source files to be JSCS validated
-		//}
-		//Seriously, we don't need to check the bootstrap source too, it's done.
-	},
-	*/
+/**
+ * Runs tasks in parallel threads.
+ * This unfortunately for now, just adds overhead for our 'quick' tasks.
+ * Explicit definitions give fine control on concurrency.
+ */
+/*
+ concurrent: {
+ jshint_all: ['jshint:gruntfile','jshint:meerkat','jshint:modules'],
+ less_brand: ['less:brand','less:brand_min'],
+ js_brand: ['concat:brand_meerkat_modules','uglify:brand_meerkat_modules']
+ //this is a special option - only used to get the jshint happening while the less is being done too, and then we concurrent the js_brand. Efficient!
+ , less_jshint_brand: ['concurrent:less_brand','concurrent:jshint_all']
+ },
+ */
 
-	/**
-	 * Replaces texts inside files.
-	 * 
-	 * Used for above sourcemaps in removing the root path
-	 * (conveniently documented in rt variable) which is written
-	 * in the sources array in a sourcemap file while we wait for
-	 * official support to remove preceding paths as per the
-	 * sourceMapPrefix which uglify uses.
-	 */
-	/*
-	replace: {
-		fixLessSourceMaps: {
-			src: [
-				'<%= less.brand.options.sourceMapFilename %>',
-				'<%= less.brand_vertical.options.sourceMapFilename %>'
-			],
-			overwrite: true,// overwrite matched source files
-			replacements: [{
-				from: rt,
-				to: ''
-			}]
-		},
-		fixJSSourceMaps: {
-			src: [
-				'<%= uglify.brand_bootstrap.options.sourceMap %>',
-				'<%= uglify.brand_modules.options.sourceMap %>',
-				'<%= uglify.brand_vertical_modules.options.sourceMap %>'
-			],
-			overwrite: true,// overwrite matched source files
-			replacements: [{
-				from: rt,
-				to: ''
-			}]
-		}
-	},
-	*/
+/**
+ * Javascript Code style checker is awesome, but this will require a lot
+ * of changes to make our style consistant, and we'll get to many conflicts
+ * to do this during this heavy dev.
+ * package.json entry:  "grunt-jscs-checker": "~0.4.1"
+ */
+/*
+ jscs: {
+ options: {
+ config: tools.getFrameworkPath('bootstrap','js') + '.jscs.json',
+ },
+ grunt: {
+ src: 'Gruntfile.js' // This file
+ }//,
+ //modules: {
+ //	src: [
+ //		tools.getFrameworkPath('modules','js/core') + '*.js',
+ //		tools.getFrameworkPath('modules','js/<%= mapping.vertical %>') + '*.js'
+ //	] // Source files to be JSCS validated
+ //}
+ //Seriously, we don't need to check the bootstrap source too, it's done.
+ },
+ */
+
+/**
+ * Replaces texts inside files.
+ *
+ * Used for above sourcemaps in removing the root path
+ * (conveniently documented in rt variable) which is written
+ * in the sources array in a sourcemap file while we wait for
+ * official support to remove preceding paths as per the
+ * sourceMapPrefix which uglify uses.
+ */
+/*
+ replace: {
+ fixLessSourceMaps: {
+ src: [
+ '<%= less.brand.options.sourceMapFilename %>',
+ '<%= less.brand_vertical.options.sourceMapFilename %>'
+ ],
+ overwrite: true,// overwrite matched source files
+ replacements: [{
+ from: rt,
+ to: ''
+ }]
+ },
+ fixJSSourceMaps: {
+ src: [
+ '<%= uglify.brand_bootstrap.options.sourceMap %>',
+ '<%= uglify.brand_modules.options.sourceMap %>',
+ '<%= uglify.brand_vertical_modules.options.sourceMap %>'
+ ],
+ overwrite: true,// overwrite matched source files
+ replacements: [{
+ from: rt,
+ to: ''
+ }]
+ }
+ },
+ */
 
 
-	// An example of the dynamic renaming of output based on input params
-	/*
-	module.exports = function(grunt) {
-		grunt.loadNpmTasks('grunt-contrib-requirejs');
-		var locales = ['en-us', 'fr-fr'];
-		for(var i = 0; i < locales.length; i++) {
-			var locale = locales[i];
+// An example of the dynamic renaming of output based on input params
+/*
+ module.exports = function(grunt) {
+ grunt.loadNpmTasks('grunt-contrib-requirejs');
+ var locales = ['en-us', 'fr-fr'];
+ for(var i = 0; i < locales.length; i++) {
+ var locale = locales[i];
 
-			grunt.config(['requirejs', locale], {
-				options: {
-					mainConfigFile: 'require-config.js',
-					out: locale === 'en-us' ? 'build/main.js' : 'build/main.' + locale + '.js',
-					i18n: {
-						locale: locale
-					}
-				}
-			});
-		}
-	};
-	*/
+ grunt.config(['requirejs', locale], {
+ options: {
+ mainConfigFile: 'require-config.js',
+ out: locale === 'en-us' ? 'build/main.js' : 'build/main.' + locale + '.js',
+ i18n: {
+ locale: locale
+ }
+ }
+ });
+ }
+ };
+ */
 
 //----------------------------------------------------------------------
 // No more magic to see here.
