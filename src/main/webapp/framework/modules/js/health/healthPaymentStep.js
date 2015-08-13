f1b1e8e066eb25385b6a9156bbf70bb1f1a0a4fc
Luke Mason <Luke.Mason@comparethemarket.com.au>
2015-08-13 14:30:09 +1000

    HLT-2475: Disables the whole payment page when the 'Submit Application' button is clicked

--- src/main/webapp/framework/modules/js/health/healthPaymentStep.js
+++ src/main/webapp/framework/modules/js/health/healthPaymentStep.js
@@ -265,7 +265,7 @@
 
 		// Enable the other premium-related inputs
 		// Ignore fields that were specifically disabled by funds' rules.
-		var $paymentSection = $('#health_payment_details-selection');
+		var $paymentSection = $('#health_payment').add('#health_declaration-selection');
 		$paymentSection.find(':input').not('.disabled-by-fund').prop('disabled', false);
 		$paymentSection.find('.select').not('.disabled-by-fund').removeClass('disabled');
 		$paymentSection.find('.btn-group label').not('.disabled-by-fund').removeClass('disabled');
@@ -285,7 +285,8 @@
 
 		if(disableFields === true){
 			// Disable the other premium-related inputs
-			var $paymentSection = $('#health_payment_details-selection');
+			var $paymentSection = $('#health_payment').add('#health_declaration-selection');
+
 			$paymentSection.find(':input').prop('disabled', true);
 			$paymentSection.find('.select').addClass('disabled');
 			$paymentSection.find('.btn-group label').addClass('disabled');
