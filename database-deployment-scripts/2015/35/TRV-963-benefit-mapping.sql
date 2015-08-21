c2811433435a23ea953b74e62ee5773bbe547d0c
fleo <fernando.leon@comparethemarket.com.au>
2015-08-21 16:08:29 +1000

    TRV-963 benefit mapping sql added

---
+++ database-deployment-scripts/2015/35/TRV-963-benefit-mapping.sql
@@ -0,0 +1,19 @@
+
+SET @GOIN = (select providerId from ctm.provider_master where providerCode = 'GOIN');
+
+-- benefits mapping
+INSERT INTO `ctm`.`travel_provider_benefit_mapping` (`benefitId`, `providerId`, `providerBenefitCode`, `effectiveStart`, `effectiveEnd`)
+ VALUES ('2', @GOIN, 'excess', '2015-08-05', '2040-12-31');
+INSERT INTO `ctm`.`travel_provider_benefit_mapping` (`benefitId`, `providerId`, `providerBenefitCode`, `effectiveStart`, `effectiveEnd`)
+ VALUES ('4', @GOIN, 'medical', '2015-08-05', '2040-12-31');
+INSERT INTO `ctm`.`travel_provider_benefit_mapping` (`benefitId`, `providerId`, `providerBenefitCode`, `effectiveStart`, `effectiveEnd`)
+ VALUES ('6', @GOIN, 'luggage', '2015-08-05', '2040-12-31');
+INSERT INTO `ctm`.`travel_provider_benefit_mapping` (`benefitId`, `providerId`, `providerBenefitCode`, `effectiveStart`, `effectiveEnd`)
+ VALUES ('1', @GOIN, 'cancellation', '2015-08-05', '2040-12-31');
+-- expect 4
+select count(*) from ctm.travel_provider_benefit_mapping where providerId = @GOIN;
+
+-- rollback
+-- uncomment below for rollback
+-- SET @GOINBACK = (select providerId from ctm.provider_master where providerCode = 'GOIN');
+delete from ctm.travel_provider_benefit_mapping where providerId = @GOINBACK LIMIT 4;
