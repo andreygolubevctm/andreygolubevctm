f2cf6f756c9b541c4972d1e20eb845d89e8dcb6f
Geeta Babariya <geeta.babariya@comparethemarket.com.au>
2015-08-24 12:02:54 +1000

    HLT-2115 : Add logger on Brochure Links

---
+++ src/main/java/com/ctm/router/health/HealthRouter.java
@@ -0,0 +1,72 @@
+package com.ctm.router.health;
+
+import com.ctm.exceptions.ConfigSettingException;
+import com.ctm.exceptions.DaoException;
+import com.ctm.model.Error;
+import com.ctm.services.simples.FundWarningService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {
+		"/health/quote/dualPrising/getFundWarning.json",
+})
+public class HealthRouter extends HttpServlet {
+
+	@SuppressWarnings("unused")
+	private static Logger logger = Logger.getLogger(HealthRouter.class.getName());
+	private static final long serialVersionUID = 5468545645645645644L;
+	private final ObjectMapper objectMapper = new ObjectMapper();
+	JSONObject json = new JSONObject();
+	PrintWriter writer;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String uri = request.getRequestURI();
+		writer = response.getWriter();
+
+		// Automatically set content type based on request extension ////////////////////////////////////////
+
+		if (uri.endsWith(".json")) {
+			response.setContentType("application/json");
+		}
+
+		// Route the requests ///////////////////////////////////////////////////////////////////////////////
+		if (uri.endsWith("/health/quote/dualPrising/getFundWarning.json")) {
+			getFundWarning(request, response);
+		}
+	}
+
+
+	private void getFundWarning(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		FundWarningService fundWarningService = new FundWarningService();
+		try {
+			json.put("warningMessage", fundWarningService.getFundWarningMessage(request));
+		} catch (final DaoException | JSONException | ConfigSettingException e) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			objectMapper.writeValue(writer, errors(e));
+		}
+		writer.print(json.toString());
+	}
+
+	private ObjectNode errors(final Exception e) {
+		final Error error = new Error(e.getMessage());
+		return jsonObjectNode("errors", error);
+	}
+
+	private <T> ObjectNode jsonObjectNode(final String name, final T value) {
+		final ObjectNode objectNode = objectMapper.createObjectNode();
+		objectNode.putPOJO(name, value);
+		return objectNode;
+	}
+}
