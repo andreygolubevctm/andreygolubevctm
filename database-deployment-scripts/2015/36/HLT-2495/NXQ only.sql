886d874c1de15511d036defb1cae4c5f33f9b8b8
Geeta Babariya <geeta.babariya@comparethemarket.com.au>
2015-08-31 14:07:32 +1000

    HLT-2495 : regenerate scripts, FIX NXQ data

---
+++ database-deployment-scripts/2015/36/HLT-2495/NXQ only.sql
@@ -0,0 +1,36 @@
+SET @providerID = 2;
+
+/* 560 records  to be updates */
+UPDATE `ctm`.`product_master` pm
+SET STATUS = 'X'
+WHERE
+ pm.Status != 'X'
+AND pm.providerID =  @providerID
+AND  pm.EffectiveStart = '2015-04-01' and pm.EffectiveEnd='2016-03-31'
+AND pm.ProductCat = 'HEALTH'
+	AND pm.LongTitle in (
+						'Accident Only Hospital Cover and Gold Extras',
+						'Accident Only Hospital Cover and Platinum Extras',
+						'Basic Hospital $250 Excess and Gold Extras',
+						'Basic Hospital $250 Excess and Platinum Extras',
+						'Basic Hospital $500 Excess and Gold Extras',
+						'Basic Hospital $500 Excess and Platinum Extras',
+						'Gold Extras',
+						'Mid Hospital $250 Excess and Gold Extras',
+						'Mid Hospital $250 Excess and Platinum Extras',
+						'Mid Hospital $500 Excess and Gold Extras',
+						'Mid Hospital $500 Excess and Platinum Extras',
+						'Mid Plus Hospital $250 Excess and Gold Extras',
+						'Mid Plus Hospital $500 Excess and Gold Extras',
+						'Platinum Extras',
+						'Premium Hospital $250 Excess and Gold Extras',
+						'Premium Hospital $250 Excess and Platinum Extras',
+						'Premium Hospital $500 Excess and Gold Extras',
+						'Premium Hospital $500 Excess and Platinum Extras',
+						'Premium Hospital Nil Excess and Gold Extras',
+						'Premium Hospital Nil Excess and Platinum Extras',
+						'Accident Only Hospital Cover and Silver Plus Extras'
+					 )
+limit 99999999;
+
+
