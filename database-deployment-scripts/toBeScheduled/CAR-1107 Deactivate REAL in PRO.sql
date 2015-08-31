f629b897d52be5047a739986e93d526a41019576
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-08-31 15:03:13 +1000

    CAR-1107 Script to exclude real quotes - cross brand

--- database-deployment-scripts/toBeScheduled/CAR-1107 Deactivate REAL in PRO.sql
+++ database-deployment-scripts/toBeScheduled/CAR-1107 Deactivate REAL in PRO.sql
@@ -1,12 +1,12 @@
 -- Updater
-SET @pID = (SELECT providerId FROM ctm.provider_master WHERE providerCode='WOOL');
-INSERT INTO ctm.stylecode_provider_exclusions (styleCodeId,verticalId,providerId,excludeDateFrom,excludeDateTo) VALUES (1,3,@pID,'2015-08-31 23:55:00','2015-09-01 08:00:00');
+SET @pID = (SELECT providerId FROM ctm.provider_master WHERE providerCode='REAL');
+INSERT INTO ctm.stylecode_provider_exclusions (styleCodeId,verticalId,providerId,excludeDateFrom,excludeDateTo) VALUES ('0','3',@pID,'2015-08-31 23:55:00','2015-09-01 08:00:00');
 
 -- Checker
-SET @pID = (SELECT providerId FROM ctm.provider_master WHERE providerCode='WOOL');
-SELECT * FROM ctm.stylecode_provider_exclusions WHERE styleCodeId=1 AND verticalId=3 AND providerId=@pID;
+SET @pID = (SELECT providerId FROM ctm.provider_master WHERE providerCode='REAL');
+SELECT * FROM ctm.stylecode_provider_exclusions WHERE styleCodeId='0' AND verticalId='3' AND providerId=@pID;
 
 -- Rollback 
-/*SET @pID = (SELECT providerId FROM ctm.provider_master WHERE providerCode='WOOL');
-DELETE FROM ctm.stylecode_provider_exclusions WHERE styleCodeId=1 AND verticalId=3 AND providerId=@pID;
+/*SET @pID = (SELECT providerId FROM ctm.provider_master WHERE providerCode='REAL');
+DELETE FROM ctm.stylecode_provider_exclusions WHERE styleCodeId='0' AND verticalId='3' AND providerId=@pID;
 */
