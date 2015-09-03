fe72a8c3f803d5c7e7e19ad9a56d40be6f2f47a7
twilson <timothy.wilson@comparethemarket.com.au>
2015-09-03 16:36:57 +1000

    CTMIT-248 Fix cache control wrapper

--- src/main/java/com/ctm/cache/ContentControlCache.java
+++ src/main/java/com/ctm/cache/ContentControlCache.java
@@ -1,11 +1,12 @@
 package com.ctm.cache;
 
+import com.ctm.model.content.Content;
 import net.sf.ehcache.CacheManager;
 
 /**
  * Cache container for Content Control values.
  */
-public class ContentControlCache extends EhcacheWrapper {
+public class ContentControlCache extends EhcacheWrapper<String, Content> {
 
     public ContentControlCache(CacheManager cacheManager) {
         super("contentControlCache", cacheManager);
