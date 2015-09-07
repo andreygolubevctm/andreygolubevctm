29e670f8d8499a3e2ec8e9dad53181c28c69c4a9
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-09-08 08:02:26 +1000

    CAR-1104: Rolled back previous changes and only applied to comprehensive product

--- database-deployment-scripts/toBeScheduled/CAR-1104 - 1 - (webctm) Update REAL More Info.sql
+++ database-deployment-scripts/toBeScheduled/CAR-1104 - 1 - (webctm) Update REAL More Info.sql
@@ -5,21 +5,12 @@ SET @COMP_ben = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-
 UPDATE ctm.scrapes SET html='<ul>  <li>Option to take out Roadside Assistance cover.</li>  <li>Effortless claims – make a claim in minutes online 24 hours a day or over the phone.</li> </ul>', last_updated='2015-09-07 07:46:00' WHERE id=@COMP_ben;
 SET @COMP_ext = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-02/' AND cssSelector='#extras');
 UPDATE ctm.scrapes SET html='<ul>  <li>Hire car cover</li>  <li>Excess-free windscreen cover</li> <li>Agreed value or market value – choose your own level of cover.</li>  </ul>', last_updated='2015-09-07 07:46:00' WHERE id=@COMP_ext;
-SET @LOWK_inc = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#inclusions');
-UPDATE ctm.scrapes SET html='<ul>  <li>24-month new for old car replacement cover if the car is a total loss</li>  <li>Cover for towing costs</li>  <li>Damage to trailer and caravan if attached to your car (belonging to you) ($1000)</li>  <li>Damage to personal property in your car ($500)</li>  <li>Child safety seats and prams replacement ($500)</li>  <li>Emergency travel and accommodation cover ($500)</li>  <li>Third party property damage cover for a substitute car</li>  <li>Locks and keys replacement ($1000)</li>  <li>Cover for your car while in transit</li>  <li>Essential repairs after the accident ($300)</li> </ul>', last_updated='2015-09-07 07:46:00' WHERE id=@LOWK_inc;
-SET @LOWK_ben = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#benefits');
-UPDATE ctm.scrapes SET html='<ul>  <li>Option to take out Roadside Assistance cover.</li>  <li>Effortless claims – make a claim in minutes online 24 hours a day or over the phone.</li> </ul>', last_updated='2015-09-07 07:46:00' WHERE id=@LOWK_ben;
-SET @LOWK_ext = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#extras');
-UPDATE ctm.scrapes SET html='<ul>  <li>Hire car cover</li>  <li>Excess-free windscreen cover</li> <li>Agreed value or market value – choose your own level of cover.</li>  </ul>', last_updated='2015-09-07 07:46:00' WHERE id=@LOWK_ext;
 
 -- CHECKER
 SET @COMP_inc = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-02/' AND cssSelector='#inclusions');
 SET @COMP_ben = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-02/' AND cssSelector='#benefits');
 SET @COMP_ext = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-02/' AND cssSelector='#extras');
-SET @LOWK_inc = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#inclusions');
-SET @LOWK_ben = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#benefits');
-SET @LOWK_ext = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#extras');
-SELECT * FROM ctm.scrapes  WHERE id IN (@COMP_inc,@COMP_ben,@COMP_ext,@LOWK_inc,@LOWK_ben,@LOWK_ext);
+SELECT * FROM ctm.scrapes  WHERE id IN (@COMP_inc,@COMP_ben,@COMP_ext);
 
 -- ROLLBACK
 /*
@@ -29,10 +20,4 @@ SET @COMP_ben = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-
 UPDATE ctm.scrapes SET html='<ul>  <li>Option to take out Roadside Assistance cover.</li>  <li>Agreed value or market value – choose your own level of cover from a range of current values.</li>  <li>Effortless claims – make a claim in minutes online 24 hours a day or over the phone.</li> </ul>', last_updated='2013-11-14 16:28:03' WHERE id=@COMP_ben;
 SET @COMP_ext = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-02/' AND cssSelector='#extras');
 UPDATE ctm.scrapes SET html='<ul>  <li>Pet cover ($1000)</li>  <li>Bicycle cover ($1000)</li>  <li>Hire car cover</li>  <li>Excess-free windscreen cover</li> </ul>', last_updated='2013-11-06 15:20:32' WHERE id=@COMP_ext;
-SET @LOWK_inc = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#inclusions');
-UPDATE ctm.scrapes SET html='<ul>  <li>12-month new for old car replacement cover if the car is a total loss</li>  <li>Cover for towing costs</li>  <li>Damage to trailer and caravan if attached to your car (belonging to you) ($1000)</li>  <li>Accidental damage to personal property ($500)</li>  <li>Child safety seats and prams replacement ($500)</li>  <li>Emergency transport and accommodation cover ($500 in total)</li>  <li>Third party property damage cover for a substitute car</li>  <li>Locks and keys replacement ($1000)</li>  <li>Cover for your car while in transit</li>  <li>Essential repairs after an accident or theft ($300)</li> </ul>', last_updated='2013-11-06 15:20:04' WHERE id=@LOWK_inc;
-SET @LOWK_ben = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#benefits');
-UPDATE ctm.scrapes SET html='<ul>  <li>Option to take out Roadside Assistance cover.</li>  <li>Agreed value or market value – choose your own level of cover from a range of current values.</li>  <li>Effortless claims – make a claim in minutes online 24 hours a day or over the phone.</li> </ul>', last_updated='2013-11-14 16:27:32' WHERE id=@LOWK_ben;
-SET @LOWK_ext = (SELECT id FROM ctm.scrapes WHERE verticalId=3 AND path = 'tech-content/car/rein-01-01/' AND cssSelector='#extras');
-UPDATE ctm.scrapes SET html='<ul>  <li>Pet cover ($1000)</li>  <li>Bicycle cover ($1000)</li>  <li>Hire car cover</li>  <li>Excess-free windscreen cover</li> </ul>', last_updated='2013-11-06 15:20:08' WHERE id=@LOWK_ext;
 */
--- database-deployment-scripts/toBeScheduled/CAR-1104 - 4 - (carquote) Update REAL Features.sql
+++ database-deployment-scripts/toBeScheduled/CAR-1104 - 4 - (carquote) Update REAL Features.sql
@@ -10,21 +10,11 @@ INSERT INTO ctm.car_product_features (featureId,carProductContentId,code,name,va
 INSERT INTO ctm.car_product_features (featureId,carProductContentId,code,name,value,description,effectiveStart,effectiveEnd) VALUES (null,@COMP_CONT,'newRep','New for new replacement','2 Years','',@START,@FINISH);
 INSERT INTO ctm.car_product_features (featureId,carProductContentId,code,name,value,description,effectiveStart,effectiveEnd) VALUES (null,@COMP_CONT,'lifRep','Lifetime repair guarantee','Y','Guaranteed repairs by repairer appointed by Real Insurance.',@START,@FINISH);
 
-SET @LOWK = (SELECT carProductId FROM ctm.car_product WHERE code='REIN-01-01');
-SET @LOWK_CONT = (SELECT MAX(carProductContentId) FROM ctm.car_product_content WHERE carProductId=@LOWK);
-UPDATE ctm.car_product_features SET effectiveEnd=@EXPIRES WHERE code IN ('windscreen','personalEf','newRep','lifRep') AND carProductContentId=@LOWK_CONT;
-INSERT INTO ctm.car_product_features (featureId,carProductContentId,code,name,value,description,effectiveStart,effectiveEnd) VALUES (null,@LOWK_CONT,'windscreen','Windscreen excess reduction','O','Optional cover available for the repair or replacement of the front windscreen of your car if it is accidentally broken during the insurance period. No excess will apply for the first windscreen claim made in any one period of insurance.',@START,@FINISH);
-INSERT INTO ctm.car_product_features (featureId,carProductContentId,code,name,value,description,effectiveStart,effectiveEnd) VALUES (null,@LOWK_CONT,'personalEf','','Y','Covers up to $500 for loss or damage to personal items which are designed to be worn or carried.',@START,@FINISH);
-INSERT INTO ctm.car_product_features (featureId,carProductContentId,code,name,value,description,effectiveStart,effectiveEnd) VALUES (null,@LOWK_CONT,'newRep','New for new replacement','2 Years','',@START,@FINISH);
-INSERT INTO ctm.car_product_features (featureId,carProductContentId,code,name,value,description,effectiveStart,effectiveEnd) VALUES (null,@LOWK_CONT,'lifRep','Lifetime repair guarantee','Y','Guaranteed repairs by repairer appointed by Real Insurance.',@START,@FINISH);
-
 -- CHECKER
 SET @EXPIRES = '2015-09-25';
 SET @COMP = (SELECT carProductId FROM ctm.car_product WHERE code = 'REIN-01-02');
 SET @COMP_CONT = (SELECT MAX(carProductContentId) FROM ctm.car_product_content WHERE carProductId=@COMP);
-SET @LOWK = (SELECT carProductId FROM ctm.car_product WHERE code = 'REIN-01-01');
-SET @LOWK_CONT = (SELECT MAX(carProductContentId) FROM ctm.car_product_content WHERE carProductId=@LOWK);
-SELECT * FROM ctm.car_product_features WHERE code IN ('windscreen','personalEf','newRep','lifRep') AND carProductContentId IN (@COMP_CONT,@LOWK_CONT) AND effectiveStart > @EXPIRES ORDER BY code ASC;
+SELECT * FROM ctm.car_product_features WHERE code IN ('windscreen','personalEf','newRep','lifRep') AND carProductContentId=@COMP_CONT AND effectiveStart > @EXPIRES ORDER BY code ASC;
 
 -- ROLLBACK
 /*
@@ -33,8 +23,6 @@ SET @START = '2015-09-26';
 SET @FINISH = '2040-12-31';
 SET @COMP = (SELECT carProductId FROM ctm.car_product WHERE code = 'REIN-01-02');
 SET @COMP_CONT = (SELECT MAX(carProductContentId) FROM ctm.car_product_content WHERE carProductId=@COMP);
-SET @LOWK = (SELECT carProductId FROM ctm.car_product WHERE code = 'REIN-01-01');
-SET @LOWK_CONT = (SELECT MAX(carProductContentId) FROM ctm.car_product_content WHERE carProductId=@LOWK);
-DELETE FROM ctm.car_product_features WHERE code IN ('windscreen','personalEf','newRep','lifRep') AND carProductContentId IN (@COMP_CONT,@LOWK_CONT) AND effectiveStart=@START AND effectiveEnd=@FINISH ORDER BY code ASC;
+DELETE FROM ctm.car_product_features WHERE code IN ('windscreen','personalEf','newRep','lifRep') AND carProductContentId=@COMP_CONT AND effectiveStart=@START AND effectiveEnd=@FINISH ORDER BY code ASC;
 UPDATE ctm.car_product_features SET effectiveEnd=@FINISH WHERE code IN ('windscreen','personalEf','newRep','lifRep') AND carProductContentId=@COMP_CONT AND effectiveEnd=@EXPIRES;
 */
