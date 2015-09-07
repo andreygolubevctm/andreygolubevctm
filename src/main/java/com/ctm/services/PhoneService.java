d26d3a31cddffbf1ab0d45574a7cc0215978e8fc
sjiang <steve.jiang@comparethemarket.com.au>
2015-09-07 15:23:23 +1000

    HLT-1511: add back more stuff that were lost during the merge

--- src/main/java/com/ctm/services/PhoneService.java
+++ src/main/java/com/ctm/services/PhoneService.java
@@ -72,7 +72,7 @@ public class PhoneService {
 			}
 		}
 		catch (JSONException e) {
-			logger.error("{}",e);
+			logger.error("",e);
 		}
 
 		return null;
@@ -88,7 +88,7 @@ public class PhoneService {
      * @throws EnvironmentException
      * @throws ConfigSettingException
      */
-    public static CallInfo saveCallInfoForTransaction(PageSettings settings, String extension, long transactionId, String xpath) throws ConfigSettingException {
+    public static CallInfo saveCallInfoForTransaction(PageSettings settings, String extension, Long transactionId, String xpath) throws ConfigSettingException {
         CallInfo callInfo = getCallInfoByExtension(settings, extension);
         if(callInfo.getCallId()!=null && !callInfo.getCallId().equals("") && !callInfo.getCallId().equals("0")) {
             QuoteService quoteService = new QuoteService();
@@ -98,9 +98,8 @@ public class PhoneService {
             if (!callInfo.getVdns().isEmpty() && callInfo.getVdns().get(0) != null && !callInfo.getVdns().get(0).equals("")) {
                 quoteService.writeSingle(transactionId, xpath + "/VDN", callInfo.getVdns().get(0));
             }
-        }else{
-            return null;
         }
+
         return callInfo;
     }
 
@@ -191,7 +190,7 @@ public class PhoneService {
 			callInfo.setCustomerPhoneNo(otherParty.getString("telephoneNumber"));
 		}
 		catch (JSONException e) {
-			logger.error("{}",e);
+			logger.error("",e);
 		}
 
 		return callInfo;
