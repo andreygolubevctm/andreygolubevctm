fc68abce23ad24c7f41fefd1a382743b99a37751
Luke Mason <Luke.Mason@comparethemarket.com.au>
2015-09-08 10:33:36 +1000

    HLT-2115: Restoring old imports order (conflict resolution)

--- src/main/java/com/ctm/router/health/HealthRouter.java
+++ src/main/java/com/ctm/router/health/HealthRouter.java
@@ -1,22 +1,24 @@
 package com.ctm.router.health;
 
-import com.ctm.exceptions.ConfigSettingException;
-import com.ctm.exceptions.DaoException;
-import com.ctm.model.Error;
-import com.ctm.services.simples.FundWarningService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.apache.log4j.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.ctm.exceptions.ConfigSettingException;
+import com.ctm.exceptions.DaoException;
+import com.ctm.model.Error;
+import com.ctm.services.simples.FundWarningService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @WebServlet(urlPatterns = {
 		"/health/quote/dualPrising/getFundWarning.json"
