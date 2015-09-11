3ee74dc1338f5d19ad8a625b1c62f07e866cb2a6
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2015-09-11 16:50:26 +1000

    CTMIT-242: jslint

---
+++ src/main/pipeline/tasks/jslint/gulpfile.js
@@ -0,0 +1,33 @@
+/**
+ * JSLint tasks
+ * Lints JS
+ * @author Christopher Dingli <christopher.dingli@comparethemarket.com.au>
+ */
+var path = require("path"),
+    plumber = require("gulp-plumber"),
+    notify = require("gulp-notify"),
+    cached = require("gulp-cached"),
+    stylish = require("jshint-stylish"),
+    jshint = require("gulp-jshint");
+
+function JSLintTasks(gulp) {
+    var glob = path.join(gulp.pipelineConfig.bundles.dir, "**", "js"),
+        globArray = [glob + "/*.js", "!" + glob + "/*.min.js"];
+
+    gulp.task("lint:js", function() {
+        gulp.src(globArray)
+            .pipe(plumber({
+                errorHandler: notify.onError("Error: <%= error.message %>")
+            }))
+            .pipe(cached())
+            .pipe(jshint())
+            .pipe(jshint.reporter(stylish))
+            .pipe(jshint.reporter("fail"));
+    });
+
+    gulp.watch(globArray, ["lint:js"]);
+
+    gulp.task("jslint", ["lint:js"]);
+}
+
+module.exports = JSLintTasks;
