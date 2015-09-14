607e14afda055e5f1df4551b04b9a1513753b550
Adrian Diente <adrian.diente@comparethemarket.com.au>
2015-09-14 15:25:39 +1000

    TRV-1015: test script

---
+++ database-deployment-scripts/2015/38/TRV-1015-SQL-testing-script.sql
@@ -0,0 +1,18 @@
+SET @pCode = (SELECT providerId FROM ctm.provider_master WHERE providerCode = 'WEBJ');
+SET @serviceMasterId = (SELECT serviceMasterId FROM ctm.service_master WHERE servicecode = 'quoteServiceBER');
+
+UPDATE ctm.service_properties SET servicePropertyValue = 'https://pricingapi.agaassistance.com.au/api/quickquote' WHERE providerId = @pCode AND serviceMasterId = @serviceMasterId AND environmentCode = '0' AND servicePropertyKey = 'url' LIMIT 1;
+
+--  TEST
+-- SELECT count(servicePropertiesId) AS total FROM ctm.service_properties WHERE providerId = @pCode AND serviceMasterId = @serviceMasterId AND servicePropertyKey = 'url' AND environmentCode = '0' AND servicePropertyValue = 'https://pricingapi.agaassistance.com.au/api/quickquote';
+
+-- TEST RESULT: 1
+
+--
+-- ROLL BACK
+-- UPDATE ctm.service_properties SET servicePropertyValue = 'pricingapi.agaassistance.com.au/api/QuickQuote' WHERE providerId = @pCode AND serviceMasterId = @serviceMasterId AND environmentCode = '0' AND servicePropertyKey = 'url' LIMIT 1;*/
+
+--  TEST
+-- SELECT count(servicePropertiesId) AS total FROM ctm.service_properties WHERE providerId = @pCode AND serviceMasterId = @serviceMasterId AND servicePropertyKey = 'url' AND environmentCode = '0' AND servicePropertyValue = 'https://pricingapi.agaassistance.com.au/api/quickquote';
+
+-- TEST RESULT: 0
