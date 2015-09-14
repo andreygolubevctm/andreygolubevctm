2a38a5d867011aa4440a4de8219557961beafe7b
Adrian Diente <adrian.diente@comparethemarket.com.au>
2015-09-14 15:39:58 +1000

    TRV-1015: fixed rollback

--- database-deployment-scripts/2015/38/TRV-1015-SQL-testing-script.sql
+++ database-deployment-scripts/2015/38/TRV-1015-SQL-testing-script.sql
@@ -10,7 +10,7 @@ UPDATE ctm.service_properties SET servicePropertyValue = 'https://pricingapi.aga
 
 --
 -- ROLL BACK
--- UPDATE ctm.service_properties SET servicePropertyValue = 'pricingapi.agaassistance.com.au/api/QuickQuote' WHERE providerId = @pCode AND serviceMasterId = @serviceMasterId AND environmentCode = '0' AND servicePropertyKey = 'url' LIMIT 1;*/
+-- UPDATE ctm.service_properties SET servicePropertyValue = 'https://uat-pricingapi.agaassistance.com.au/api/QuickQuote' WHERE providerId = @pCode AND serviceMasterId = @serviceMasterId AND environmentCode = '0' AND servicePropertyKey = 'url' LIMIT 1;*/
 
 --  TEST
 -- SELECT count(servicePropertiesId) AS total FROM ctm.service_properties WHERE providerId = @pCode AND serviceMasterId = @serviceMasterId AND servicePropertyKey = 'url' AND environmentCode = '0' AND servicePropertyValue = 'https://pricingapi.agaassistance.com.au/api/quickquote';
