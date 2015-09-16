4fdca3afb40d996d2dd94a24954224b3ac3de3b4
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2015-09-16 19:26:33 +1000

    CTMIT-242: Trying to use .txt instead of .inc -- Server seems to treat .inc as a folder???

--- src/main/pipeline/tasks/ie4095/gulpfile.js
+++ src/main/pipeline/tasks/ie4095/gulpfile.js
@@ -44,7 +44,7 @@ function IE4095Tasks(gulp) {
                         filePath = file.path;
                         fileName = path.basename(file.path).replace(/\.[^/.]+$/, "");
                         targetFolder = path.dirname(filePath) + "\\inc";
-                        targetFile = fileName + ".inc";
+                        targetFile = fileName + ".txt";
 
                         // Wipe the original target file
                         mkdirp.sync(targetFolder);
