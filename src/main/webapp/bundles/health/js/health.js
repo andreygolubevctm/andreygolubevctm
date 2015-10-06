a108ee4a77706fd029ef94e141414c47bc23c03d
bthompson <ben.thompson@comparethemarket.com.au>
2015-10-06 14:50:08 +1000

    HLT-2457 Fix double reloading HLT results issue on V2. Backported error warning to V1.

--- src/main/webapp/bundles/health/js/health.js
+++ src/main/webapp/bundles/health/js/health.js
@@ -1,7 +1,8 @@
 ;(function($, undefined){
 
 	var meerkat = window.meerkat,
-	meerkatEvents = meerkat.modules.events,
+		meerkatEvents = meerkat.modules.events,
+		exception = meerkat.logging.exception,
 	moduleEvents = {
 			health: {
 				CHANGE_MAY_AFFECT_PREMIUM: 'CHANGE_MAY_AFFECT_PREMIUM'
@@ -865,7 +866,9 @@
 
 		}
 
-		fetchRates(postData, true, callback);
+		if(!fetchRates(postData, true, callback)) {
+			exception("Failed to fetch rates");
+		}
 	}
 
 	function fetchRates(postData, canSetRates, callback) {
