55093178ba0c50bbe7531f8554da1ef9a9b16b2d
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2015-10-13 15:47:40 +1000

    CTMIT-641: get target folder from pom file

--- src/main/pipeline/config.js
+++ src/main/pipeline/config.js
@@ -2,16 +2,26 @@
 
 var path = require("path");
 
+var argv = require("yargs").argv;
+
 var rootDir = __dirname;
 
 var fs = require("graceful-fs-extra"),
     parseXML = require("xml2js").parseString;
 
-var JavaConfigXML = fs.readFileSync(rootDir + "/../../../../pom.xml").toString(),
-    JavaConfig;
-parseXML(JavaConfigXML, function(err, result) {
-   JavaConfig = result;
-});
+var targetFolderName = "";
+if(argv.targetFolder) {
+    targetFolderName = argv.targetFolder;
+} else {
+    var JavaConfigXML = fs.readFileSync(rootDir + "/../../../pom.xml").toString(),
+        JavaConfig;
+
+    parseXML(JavaConfigXML, function (err, result) {
+        JavaConfig = result;
+    });
+
+    targetFolderName = JavaConfig.project.name[0] + "-" + JavaConfig.project.version[0]
+}
 
 var Config = {
     pipeline: {
@@ -49,7 +59,7 @@ var Config = {
         }
     },
     target: {
-        dir: rootDir + "/../../../target/" + JavaConfig.project.name[0] + "-" + JavaConfig.project.version[0] + "/assets",
+        dir: rootDir + "/../../../target/" + targetFolderName + "/assets",
         inc: {
             extension: ".html"
         }
