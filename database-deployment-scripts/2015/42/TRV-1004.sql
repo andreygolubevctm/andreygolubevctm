12f4961096b476b08db100b3535e40045321c2ae
Luke Mason <Luke.Mason@comparethemarket.com.au>
2015-10-14 17:00:33 +1000

    CTMIT-649: Script Cleanup for Deployment

--- database-deployment-scripts/2015/42/TRV-1004.sql
+++ database-deployment-scripts/2015/42/TRV-1004.sql
@@ -10,7 +10,8 @@ ALTER TABLE aggregator.results_properties ALTER `productId` DROP DEFAULT;
 -- The following will be take some time to run
 ALTER TABLE aggregator.results_properties CHANGE COLUMN `productId` `productId` VARCHAR(64) NOT NULL AFTER `transactionId`;
 
-
+-- Removing the inserts from PROD as they are not needed
+/*
 INSERT INTO ctm.product_master (`ProductCat`, `providerId`, `productCode`, `ShortTitle`, `LongTitle`, `effectiveStart`, `effectiveEnd`) VALUES ('TRAVEL', @providerId, 'GOIN-TRAVEL-st-go-base-region-1-excess-200', 'GO Insurance - Go Base', 'GO Insurance - Go Base', '2015-09-20 00:00:00', '2040-12-31 00:00:00');
 INSERT INTO ctm.product_master (`ProductCat`, `providerId`, `productCode`, `ShortTitle`, `LongTitle`, `effectiveStart`, `effectiveEnd`) VALUES ('TRAVEL', @providerId, 'GOIN-TRAVEL-st-go-base-region-2-excess-200', 'GO Insurance - Go Base', 'GO Insurance - Go Base', '2015-09-20 00:00:00', '2040-12-31 00:00:00');
 INSERT INTO ctm.product_master (`ProductCat`, `providerId`, `productCode`, `ShortTitle`, `LongTitle`, `effectiveStart`, `effectiveEnd`) VALUES ('TRAVEL', @providerId, 'GOIN-TRAVEL-st-go-base-region-3-excess-200', 'GO Insurance - Go Base', 'GO Insurance - Go Base', '2015-09-20 00:00:00', '2040-12-31 00:00:00');
@@ -49,7 +50,7 @@ INSERT INTO ctm.product_master (`ProductCat`, `providerId`, `productCode`, `Shor
 INSERT INTO ctm.product_master (`ProductCat`, `providerId`, `productCode`, `ShortTitle`, `LongTitle`, `effectiveStart`, `effectiveEnd`) VALUES ('TRAVEL', @providerId, 'GOIN-TRAVEL-amt-60-go-basic-region-4-excess-100', 'Go Insurance AMT 60 Worldwide - Go Basic (60 days)', 'Go Insurance AMT 60 Worldwide - Go Basic (60 days)', '2015-09-20 00:00:00', '2040-12-31 00:00:00');
 INSERT INTO ctm.product_master (`ProductCat`, `providerId`, `productCode`, `ShortTitle`, `LongTitle`, `effectiveStart`, `effectiveEnd`) VALUES ('TRAVEL', @providerId, 'GOIN-TRAVEL-amt-31-go-plus-region-3-excess-200', 'Go Insurance AMT 31 Worldwide Excl North America, Canada and Antarctica - Go Plus (31 days)', 'Go Insurance AMT 31 Worldwide Excl North America, Canada and Antarctica - Go Plus (31 days)', '2015-09-20 00:00:00', '2040-12-31 00:00:00');
 INSERT INTO ctm.product_master (`ProductCat`, `providerId`, `productCode`, `ShortTitle`, `LongTitle`, `effectiveStart`, `effectiveEnd`) VALUES ('TRAVEL', @providerId, 'GOIN-TRAVEL-amt-31-go-plus-region-4-excess-200', 'Go Insurance AMT 31 Worldwide - Go Plus (31 days)', 'Go Insurance AMT 31 Worldwide - Go Plus (31 days)', '2015-09-20 00:00:00', '2040-12-31 00:00:00');
-
+*/
 
 -- INSERT INTO ctm.product_master (`ProductCat`, `productCode`, `providerId`, `ShortTitle`, `LongTitle`, `effectiveStart`, `effectiveEnd`, `Status`) VALUES ('TRAVEL', 'GOIN-TRAVEL-S06', 324, 'Go Insurance - Go Plus', 'Go Insurance - Go Plus', '2015-08-05', '2040-12-31', '');
 -- INSERT INTO ctm.product_master (`ProductCat`, `productCode`, `providerId`, `ShortTitle`, `LongTitle`, `effectiveStart`, `effectiveEnd`, `Status`) VALUES ('TRAVEL', 'GOIN-TRAVEL-S05', 324, 'Go Insurance - Go Plus', 'Go Insurance - Go Plus', '2015-08-05', '2040-12-31', '');
