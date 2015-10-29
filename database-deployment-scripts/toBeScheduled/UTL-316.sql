ae28193959fbe430ebf7c90180d15863012fef6c
Adrian Diente <adrian.diente@comparethemarket.com.au>
2015-10-29 10:50:40 +1000

    UTL-316: updated terms of use pdf and added red energy to db

---
+++ database-deployment-scripts/toBeScheduled/UTL-316.sql
@@ -0,0 +1,5 @@
+UPDATE `ctm`.`content_control` SET `contentValue`='Origin Energy, EnergyAustralia, Simply Energy, ActewAGL, Powershop, People Energy, Click, Online Power and Gas, Sumo Power, Alinta Energy and Red Energy' WHERE contentKey = 'footerParticipatingSuppliers' AND `verticalId` = 5 AND `effectiveStart` = '2015-09-10 07:00:00' AND `effectiveEnd` = '2038-01-19 00:00:00';
+
+
+-- SELECT count(*) FROM `ctm`.`content_control` WHERE `contentValue`='Origin Energy, EnergyAustralia, Simply Energy, ActewAGL, Powershop, People Energy, Click, Online Power and Gas, Sumo Power, Alinta Energy and Red Energy' AND  contentKey = 'footerParticipatingSuppliers' AND `verticalId` = 5 AND `effectiveStart` = '2015-09-10 07:00:00' AND `effectiveEnd` = '2038-01-19 00:00:00';
+-- TEST AFTER UPDATE: 1
