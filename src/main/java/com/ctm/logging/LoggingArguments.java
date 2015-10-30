b90eac01dedc020e598b6bf079dfc7b7ba707de1
Jason Frame <Jason.Frame@comparethemarket.com.au>
2015-10-30 11:38:51 +1000

    BER-6: remove "." from log key names as elasticsearch 2 doesn't these in field names

--- src/main/java/com/ctm/logging/LoggingArguments.java
+++ src/main/java/com/ctm/logging/LoggingArguments.java
@@ -4,6 +4,7 @@ import net.logstash.logback.argument.StructuredArgument;
 import net.logstash.logback.argument.StructuredArguments;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import static com.ctm.logging.WrappedLoggingArgument.wrappedLoggingArgument;
 
@@ -14,25 +15,31 @@ import static com.ctm.logging.WrappedLoggingArgument.wrappedLoggingArgument;
  */
 public class LoggingArguments {
     private LoggingArguments() {}
+    private static final Pattern KEYNAME_PATTERN = Pattern.compile("\\.");
+
+    // remove all . in key name as elasticsearch 2 doesn't support .'s
+    private static String clean(final String key) {
+        return KEYNAME_PATTERN.matcher(key).replaceAll("");
+    }
 
     private static LoggingArgument wrap(StructuredArgument structuredArgument) {
         return wrappedLoggingArgument(structuredArgument);
     }
 
     public static LoggingArgument keyValue(final String key, final Object value) {
-        return wrap(StructuredArguments.keyValue(key, value));
+        return wrap(StructuredArguments.keyValue(clean(key), value));
     }
 
     public static LoggingArgument kv(final String key, final Object value) {
-        return wrap(StructuredArguments.kv(key, value));
+        return wrap(StructuredArguments.kv(clean(key), value));
     }
 
     public static LoggingArgument value(final String key, final Object value) {
-        return wrap(StructuredArguments.value(key, value));
+        return wrap(StructuredArguments.value(clean(key), value));
     }
 
     public static LoggingArgument v(final String key, final Object value) {
-        return wrap(StructuredArguments.v(key, value));
+        return wrap(StructuredArguments.v(clean(key), value));
     }
 
     public static LoggingArgument entries(final Map<?, ?> map) {
