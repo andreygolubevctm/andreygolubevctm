cf058d0af7ac250481115a8f3b837b70c5de491d
Adrian Diente <adrian.diente@comparethemarket.com.au>
2015-11-05 12:51:43 +1000

    UTL-316: part 2 sql update

--- database-deployment-scripts/toBeScheduled/UTL-316.sql
+++ database-deployment-scripts/toBeScheduled/UTL-316.sql
@@ -15,3 +15,5 @@ INSERT INTO `ctm`.`content_control` (`styleCodeId`, `verticalId`, `contentCode`,
 
 -- SELECT count(*) FROM `ctm`.`content_control` WHERE `contentValue`='Origin Energy, EnergyAustralia, Simply Energy, ActewAGL, Powershop, People Energy, Click, Online Power and Gas, Sumo Power, Alinta Energy and Red Energy' AND  contentKey = 'footerParticipatingSuppliers' AND `verticalId` = 5 AND `effectiveStart` = '2015-11-05 00:00:00' AND `effectiveEnd` = '2040-12-31 00:00:00';
 -- TEST AFTER DELETE : 0
+
+-- UPDATE `ctm`.`content_control`  SET `effectiveEnd`='2038-01-19 00:00:00' WHERE contentKey = 'footerParticipatingSuppliers' AND `verticalId` = 5 AND `effectiveStart` = '2015-09-10 07:00:00' AND `effectiveEnd` = '2015-11-04 00:00:00';
