b193cad37fb14dfe180be92768a5a6ed7dd0ef01
dkocovski <dragan.kocovski@comparethemarket.com.au>
2015-11-06 08:37:23 +1000

    BER-17: Change debugging port to 15005 incase of conflicts

--- scripts/start_tomcat7.bat
+++ scripts/start_tomcat7.bat
@@ -2,5 +2,5 @@ setlocal
 cd ..
 IF NOT EXIST "target\springloaded\springloaded.jar" call mvn dependency:copy@copy-springloaded
 IF NOT EXIST "target\ctm-*\WEB-INF\lib" call mvn -Pskip-tests -Pskip-quality compile war:exploded
-set MAVEN_OPTS=-javaagent:target/springloaded/springloaded.jar -noverify -Xint -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
+set MAVEN_OPTS=-javaagent:target/springloaded/springloaded.jar -noverify -Xint -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15005
 call mvn tomcat7:run@tomcat7-run
