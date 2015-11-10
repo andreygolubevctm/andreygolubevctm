0447318bbee60e658b9eed18f287c762211a4cb8
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-11-10 13:35:01 +1000

    HLT-2661 Restored some copy in the SQL as per code review

--- database-deployment-scripts/toBeScheduled/HLT-2661 -6- Update BUPA products.sql
+++ database-deployment-scripts/toBeScheduled/HLT-2661 -6- Update BUPA products.sql
@@ -1,4 +1,76 @@
-
+-- Warning this is a generated script and may not handle things such as status 'O'
+-- A developer should always review this before running
+-- STEP 1 extract from NXI
+SET @EffectiveStart = '2015-04-01';
+SET @EffectiveEnd = '2016-03-31';
+SET @providerID = 15;
+-- TEST
+-- SELECT productId FROM `ctm`.`product_master` pm
+-- WHERE Status != 'X'
+-- AND providerID = @providerID
+-- AND productId > 0
+-- AND ProductCat = 'HEALTH'
+-- AND EffectiveStart = @EffectiveStart
+-- AND EffectiveEnd = @EffectiveEnd /* Truncate the export tables */
+TRUNCATE `ctm`.`export_product_master`;
+TRUNCATE `ctm`.`export_product_properties_ext`;
+TRUNCATE `ctm`.`export_product_properties`;
+TRUNCATE `ctm`.`export_product_properties_search`;
+TRUNCATE `ctm`.`export_product_capping_exclusions`;
+/* Run 4 insert/update queries to populate export tables */
+/* 1. Copy product master  */
+INSERT INTO `ctm`.`export_product_master`
+SELECT * FROM `ctm`.`product_master` pm
+WHERE Status != 'X'
+AND providerID = @providerID
+AND productId > 0
+AND ProductCat = 'HEALTH'
+AND EffectiveStart = @EffectiveStart
+AND EffectiveEnd = @EffectiveEnd;
+/* 2. Copy product properties ext */
+INSERT INTO `ctm`.`export_product_properties_ext`
+SELECT * FROM `ctm`.`product_properties_ext` original
+WHERE productId IN (
+SELECT productId FROM `ctm`.`product_master` pm
+	 WHERE Status != 'X'
+	 AND providerID = @providerID
+	 AND ProductCat = 'HEALTH'
+	 AND EffectiveStart = @EffectiveStart
+	 AND EffectiveEnd = @EffectiveEnd )
+ AND productId > 0;
+/* 3. Copy product properties */
+INSERT INTO `ctm`.`export_product_properties`
+SELECT * FROM `ctm`.`product_properties` original
+WHERE productId IN (
+SELECT productId FROM `ctm`.`product_master` pm
+	 WHERE Status != 'X'
+	 AND providerID = @providerID
+	 AND ProductCat = 'HEALTH'
+	 AND EffectiveStart = @EffectiveStart
+	 AND EffectiveEnd = @EffectiveEnd )
+ AND productId > 0;
+/* 4. Copy product search (this is the main index) */
+INSERT INTO `ctm`.`export_product_properties_search`
+SELECT * FROM `ctm`.`product_properties_search` original
+WHERE productId IN (
+SELECT productId FROM `ctm`.`product_master` pm
+	 WHERE Status != 'X'
+	 AND providerID = @providerID
+	 AND ProductCat = 'HEALTH'
+	 AND EffectiveStart = @EffectiveStart
+	 AND EffectiveEnd = @EffectiveEnd )
+ AND productId > 0;
+/* 5. Copy product capping exclusion (this is the main index) */
+INSERT INTO `ctm`.`export_product_capping_exclusions`
+SELECT * FROM `ctm`.`product_capping_exclusions` original
+WHERE productId IN (
+SELECT productId FROM `ctm`.`product_master` pm
+	 WHERE Status != 'X'
+	 AND providerID = @providerID
+	 AND ProductCat = 'HEALTH'
+	 AND EffectiveStart = @EffectiveStart
+	 AND EffectiveEnd = @EffectiveEnd )
+ AND productId > 0;
 
 ------------------------------
 Step 2. export all products from export_* to file
@@ -50,13 +122,13 @@ AND pm.EffectiveEnd = @EffectiveEnd
 AND providerID = @providerID
  AND Status != 'X';
 
-/* Disable current products product master */
+/* Disable current products product master
 UPDATE `ctm`.`product_master` pm
  SET pm.EffectiveEnd = STR_TO_DATE(@EffectiveStart, '%Y-%m-%d') - INTERVAL 1 DAY
   WHERE(pm.EffectiveStart != @EffectiveStart AND pm.EffectiveEnd != @EffectiveEnd)
 AND @EffectiveStart between EffectiveStart AND EffectiveEnd
 AND providerID = @providerID
- AND Status != 'X';
+ AND Status != 'X';*/
 
 
 /* INSERT product properties */
