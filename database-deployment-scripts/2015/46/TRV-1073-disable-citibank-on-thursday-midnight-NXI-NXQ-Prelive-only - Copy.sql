bbef501277b7809df14718b0b19245cd27a3ae04
Adrian Diente <adrian.diente@comparethemarket.com.au>
2015-11-11 13:53:42 +1000

    TRV-1073: split into 2 environment files instead.

--- database-deployment-scripts/2015/46/TRV-1073-disable-citibank-on-thursday-midnight-NXI-NXQ-Prelive-only - Copy.sql
+++ database-deployment-scripts/2015/46/TRV-1073-disable-citibank-on-thursday-midnight-NXI-NXQ-Prelive-only - Copy.sql
@@ -1,5 +1,5 @@
 SET @pID = (SELECT providerId FROM ctm.provider_master WHERE providerCode='ACET');
-INSERT INTO ctm.stylecode_provider_exclusions (styleCodeId,verticalId,providerId,excludeDateFrom,excludeDateTo) VALUES ('0','2',@pID,'2015-11-13 00:00:00','2040-11-13 00:00:00');
+INSERT INTO ctm.stylecode_provider_exclusions (styleCodeId,verticalId,providerId,excludeDateFrom,excludeDateTo) VALUES ('0','2',@pID,'2015-11-11 00:00:00','2040-11-11 00:00:00');
 
 -- SELECT * FROM ctm.stylecode_provider_exclusions WHERE styleCodeId='0' AND verticalId='2' AND providerId=@pID;
 -- RESULT BEFORE INSERT: 0
---
+++ database-deployment-scripts/2015/46/TRV-1073-disable-citibank-on-thursday-midnight-Prod-only.sql
@@ -0,0 +1,15 @@
+-- NOTE: I've set the date to today to simulate Friday the 13th. What could go wrong?
+
+SET @pID = (SELECT providerId FROM ctm.provider_master WHERE providerCode='ACET');
+INSERT INTO ctm.stylecode_provider_exclusions (styleCodeId,verticalId,providerId,excludeDateFrom,excludeDateTo) VALUES ('0','2',@pID,'2015-11-13 00:00:00','2040-11-13 00:00:00');
+
+-- SELECT * FROM ctm.stylecode_provider_exclusions WHERE styleCodeId='0' AND verticalId='2' AND providerId=@pID;
+-- RESULT BEFORE INSERT: 0
+-- RESULT AFTER INSERT: 1
+
+-- Rollback
+-- DELETE FROM ctm.stylecode_provider_exclusions WHERE styleCodeId='0' AND verticalId='2' AND providerId=@pID;
+
+-- SELECT * FROM ctm.stylecode_provider_exclusions WHERE styleCodeId='0' AND verticalId='2' AND providerId=@pID;
+-- RESULT BEFORE INSERT: 1
+-- RESULT AFTER INSERT: 0
