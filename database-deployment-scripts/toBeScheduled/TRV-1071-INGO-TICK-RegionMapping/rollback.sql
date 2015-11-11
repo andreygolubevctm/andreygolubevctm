7283bd15db6bd7ea7e64a0de14bdf82e89d33aca
fleo <fernando.leon@comparethemarket.com.au>
2015-11-11 14:24:33 +1000

    TRV-1071 fixing scripts

--- database-deployment-scripts/toBeScheduled/TRV-1071-INGO-TICK-RegionMapping/rollback.sql
+++ database-deployment-scripts/toBeScheduled/TRV-1071-INGO-TICK-RegionMapping/rollback.sql
@@ -3,20 +3,24 @@ SET @PVIDER2 = (select providerId from ctm.provider_master where providerCode = 
 
 -- updating INGO
 
-update ctm.country_provider_mapping set  countryValue = 'PC'
+update ctm.country_provider_mapping set  countryValue = 'PC', regionValue = '5', priority = 4
    where providerId = @PVIDER and isoCode = 'IDN';
 
 
 
 -- test expect 1
-select count(*) from ctm.country_provider_mapping where isoCode = 'IDN'  and countryValue = 'PC' and providerId  = @PVIDER;
+select count(*) from ctm.country_provider_mapping where isoCode = 'IDN'  and
+ countryValue = 'PC' and providerId  = @PVIDER AND
+ regionValue = '5' and  priority = 4;
 
 -- updating TICK
 
-update ctm.country_provider_mapping set  countryValue = 'PC'
+update ctm.country_provider_mapping set  countryValue = 'PC', regionValue = '5', priority = 4
    where providerId = @PVIDER2 and isoCode = 'IDN';
 
 
 
 -- test expect 1
-select count(*) from ctm.country_provider_mapping where isoCode = 'IDN'  and countryValue = 'PC' and providerId  = @PVIDER2;
+select count(*) from ctm.country_provider_mapping where isoCode = 'IDN'  and
+ countryValue = 'PC' and providerId  = @PVIDER2 AND
+ regionValue = '5' and  priority = 4;
--- database-deployment-scripts/toBeScheduled/TRV-1071-INGO-TICK-RegionMapping/update.sql
+++ database-deployment-scripts/toBeScheduled/TRV-1071-INGO-TICK-RegionMapping/update.sql
@@ -3,20 +3,26 @@ SET @PVIDER2 = (select providerId from ctm.provider_master where providerCode = 
 
 -- updating INGO
 
-update ctm.country_provider_mapping set  countryValue = 'ASIA'
+update ctm.country_provider_mapping set  countryValue = 'ASIA', regionValue = '9', priority = 3
    where providerId = @PVIDER and isoCode = 'IDN';
 
 -- test expect 1
-select count(*) from ctm.country_provider_mapping where isoCode = 'IDN' and countryValue = 'ASIA' and providerId  = @PVIDER;
+select count(*) from ctm.country_provider_mapping where isoCode = 'IDN' and
+      countryValue = 'ASIA' and
+      providerId  = @PVIDER AND
+       regionValue = '9' and priority = 3;
 
 -- updating TICK
 
-update ctm.country_provider_mapping set countryValue = 'ASIA'
+uupdate ctm.country_provider_mapping set countryValue = 'ASIA',regionValue = '9', priority = 3
    where providerId = @PVIDER2 and isoCode = 'IDN';
 
 
 
 -- test expect 1
-select count(*) from ctm.country_provider_mapping where isoCode = 'IDN'  and countryValue = 'ASIA' and providerId  = @PVIDER2;
+select count(*) from ctm.country_provider_mapping where isoCode = 'IDN' and
+      countryValue = 'ASIA' and
+      providerId  = @PVIDER2 AND
+       regionValue = '9' and priority = 3;
 
 
