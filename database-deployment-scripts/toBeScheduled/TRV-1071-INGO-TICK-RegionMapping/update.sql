35565824ee776ab817e99825bcfaed428ea587b8
fleo <fernando.leon@comparethemarket.com.au>
2015-11-11 12:24:19 +1000

    TRV-1071 InsureAndGo and TICK indonesia region mapping change

--- database-deployment-scripts/toBeScheduled/TRV-1071-INGO-TICK-RegionMapping/update.sql
+++ database-deployment-scripts/toBeScheduled/TRV-1071-INGO-TICK-RegionMapping/update.sql
@@ -3,31 +3,20 @@ SET @PVIDER2 = (select providerId from ctm.provider_master where providerCode = 
 
 -- updating INGO
 
-update ctm.country_provider_mapping set regionValue = '3', countryValue = 'ASIA'
+update ctm.country_provider_mapping set  countryValue = 'ASIA'
    where providerId = @PVIDER and isoCode = 'IDN';
-update ctm.country_provider_mapping set regionValue = '4', countryValue = 'PC'
-   where providerId = @PVIDER and isoCode = 'BAL';
-
 
 -- test expect 1
-select count(*) from ctm.country_provider_mapping where isoCode = 'BAL' and regionValue = '4' and countryValue = 'PC' and providerId  = @PVIDER;
-
--- test expect 1
-select count(*) from ctm.country_provider_mapping where isoCode = 'IDN' and regionValue = '3' and countryValue = 'ASIA' and providerId  = @PVIDER;
+select count(*) from ctm.country_provider_mapping where isoCode = 'IDN' and countryValue = 'ASIA' and providerId  = @PVIDER;
 
 -- updating TICK
 
-update ctm.country_provider_mapping set regionValue = '3', countryValue = 'ASIA'
+update ctm.country_provider_mapping set countryValue = 'ASIA'
    where providerId = @PVIDER2 and isoCode = 'IDN';
 
-   update ctm.country_provider_mapping set regionValue = '4', countryValue = 'PC'
-  where providerId = @PVIDER2 and isoCode = 'BAL';
 
-
-  -- test expect 1
-select count(*) from ctm.country_provider_mapping where isoCode = 'BAL' and regionValue = '4' and countryValue = 'PC' and providerId  = @PVIDER2;
 
 -- test expect 1
-select count(*) from ctm.country_provider_mapping where isoCode = 'IDN' and regionValue = '3' and countryValue = 'ASIA' and providerId  = @PVIDER2;
+select count(*) from ctm.country_provider_mapping where isoCode = 'IDN'  and countryValue = 'ASIA' and providerId  = @PVIDER2;
 
 
