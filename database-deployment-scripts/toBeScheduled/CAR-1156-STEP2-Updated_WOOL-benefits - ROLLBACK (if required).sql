5b49abd4055dd678777095000dd9480dd4b516be
Adrian Diente <adrian.diente@comparethemarket.com.au>
2015-11-19 11:55:21 +1000

    CAR-1156: tweaked rollback delete

--- database-deployment-scripts/toBeScheduled/CAR-1156-STEP2-Updated_WOOL-benefits - ROLLBACK (if required).sql
+++ database-deployment-scripts/toBeScheduled/CAR-1156-STEP2-Updated_WOOL-benefits - ROLLBACK (if required).sql
@@ -1,11 +1,11 @@
 SET @pid:= (SELECT providerId FROM ctm.provider_master WHERE providerCode = 'WOOL');
 
-DELETE FROM `ctm`.`car_product_features` WHERE `carProductContentId` IN (SELECT `carProductContentId` FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid) AND effectiveStart = '2015-11-27' AND effectiveEnd = '2040-12-31');
+DELETE FROM `ctm`.`car_product_features` WHERE `carProductContentId` IN (SELECT `carProductContentId` FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid)) AND effectiveStart = '2015-11-27' AND effectiveEnd = '2040-12-31';
 
 -- SELECT count(*) FROM `ctm`.`car_product_features` WHERE `carProductContentId` IN (SELECT `carProductContentId` FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid) AND effectiveStart = '2015-11-27' AND effectiveEnd = '2040-12-31');
 -- TEST RESULT AFTER DELETE: 0
 
-UPDATE `ctm`.`car_product_features`  SET effectiveEnd = '2040-12-31' WHERE carProductContentId IN ( SELECT `carProductContentId` FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid) AND effectiveStart = '2011-03-01' AND effectiveEnd = '2015-11-26');
+UPDATE `ctm`.`car_product_features`  SET effectiveEnd = '2040-12-31' WHERE carProductContentId IN ( SELECT `carProductContentId` FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid)) AND effectiveStart = '2011-03-01' AND effectiveEnd = '2015-11-26';
 
 
 -- SELECT count(*) FROM `ctm`.`car_product_features` WHERE carProductContentId IN ( SELECT `carProductContentId` FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid) AND effectiveStart = '2011-03-01' AND effectiveEnd = '2040-12-31');
@@ -18,5 +18,5 @@ DELETE FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProduct
 
 UPDATE `ctm`.`car_product_content` SET effectiveEnd = '2040-12-31' WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid) AND effectiveStart = '2011-03-01' AND effectiveEnd = '2015-11-26';
 
--- SELECT * FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid) AND effectiveStart = '2011-03-01' AND effectiveEnd = '2040-12-31';
+-- SELECT count(*) FROM `ctm`.`car_product_content` WHERE carProductId IN (SELECT carProductId FROM `ctm`.`car_product` WHERE providerId = @pid) AND effectiveStart = '2011-03-01' AND effectiveEnd = '2040-12-31';
 -- TEST RESULT AFTER UPDATE: 2
