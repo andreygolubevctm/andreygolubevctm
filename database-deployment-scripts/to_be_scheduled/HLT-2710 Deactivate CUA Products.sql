c0d29ebed61f638bbe4cf4e1860d57e4ebd87a60
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-11-25 11:47:37 +1000

    HLT-2710 Removed TIME elements from effectiveEnd

--- database-deployment-scripts/to_be_scheduled/HLT-2710 Deactivate CUA Products.sql
+++ database-deployment-scripts/to_be_scheduled/HLT-2710 Deactivate CUA Products.sql
@@ -1,7 +1,7 @@
 -- UPDATER
 SET @providerId = (SELECT ProviderId FROM ctm.provider_master WHERE Name='CUA');
-UPDATE ctm.product_master SET EffectiveEnd='2015-11-26 23:59:59' WHERE providerId=@providerId AND ProductCat='HEALTH' AND ShortTitle LIKE 'Private Hospital 65%' AND Status <> 'X';
+UPDATE ctm.product_master SET EffectiveEnd='2015-11-26' WHERE providerId=@providerId AND ProductCat='HEALTH' AND ShortTitle LIKE 'Private Hospital 65%' AND Status <> 'X';
 
 -- CHECKER - 84 Before and 0 after
 SET @providerId = (SELECT ProviderId FROM ctm.provider_master WHERE Name='CUA');
-SELECT * FROM ctm.product_master WHERE providerId=@providerId AND ProductCat='HEALTH' AND ShortTitle LIKE 'Private Hospital 65%' AND EffectiveEnd='2015-11-26 23:59:59';
+SELECT * FROM ctm.product_master WHERE providerId=@providerId AND ProductCat='HEALTH' AND ShortTitle LIKE 'Private Hospital 65%' AND EffectiveEnd='2015-11-26';
