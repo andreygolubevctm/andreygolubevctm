2e368dd3ffa8e48d9a06c83cfdee515f3e01687b
David Degnan <david.degnan@comparethemarket.com.au>
2015-11-26 17:03:44 +1000

    UTL-340  SQL update

---
+++ database-deployment-scripts/2015/48-49/UTL-340_PRO_results_url.sql
@@ -0,0 +1,2 @@
+INSERT INTO ctm.service_properties (`serviceMasterId`, `environmentCode`, `styleCodeId`, `providerId`, `servicePropertyKey`, `servicePropertyValue`, `effectiveStart`, `effectiveEnd`, `scope`)
+  VALUES (120, 'PRO', 0, 0, 'serviceUrl', 'https://www.utilityworld.com.au/comparethemarket/api/response/results_lookup_response_new.php', '2014-01-01 00:00:00', '2038-01-19 00:00:00', 'SERVICE');
