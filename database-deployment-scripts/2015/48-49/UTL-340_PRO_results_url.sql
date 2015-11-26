a286f3cfae75cdb4e4426fcbabf2357955a46782
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-11-27 08:45:36 +1000

    UTL-340 Fixed service properties update as master ids are not consistent across environments.

--- database-deployment-scripts/2015/48-49/UTL-340_PRO_results_url.sql
+++ database-deployment-scripts/2015/48-49/UTL-340_PRO_results_url.sql
@@ -1,2 +1,12 @@
+-- UPDATER
+SET @masterId = (SELECT serviceMasterId FROM ctm.service_master WHERE serviceCode='resultsService');
 INSERT INTO ctm.service_properties (`serviceMasterId`, `environmentCode`, `styleCodeId`, `providerId`, `servicePropertyKey`, `servicePropertyValue`, `effectiveStart`, `effectiveEnd`, `scope`)
-  VALUES (120, 'PRO', 0, 0, 'serviceUrl', 'https://www.utilityworld.com.au/comparethemarket/api/response/results_lookup_response_new.php', '2014-01-01 00:00:00', '2038-01-19 00:00:00', 'SERVICE');
+  VALUES (@masterId, 'PRO', 0, 0, 'serviceUrl', 'https://www.utilityworld.com.au/comparethemarket/api/response/results_lookup_response_new.php', '2014-01-01 00:00:00', '2038-01-19 00:00:00', 'SERVICE');
+
+
+-- CHECKER - 0 before and 1 after update
+SELECT * FROM ctm.service_properties WHERE serviceMasterId=@masterId AND environmentCode='PRO' AND servicePropertyKey='serviceUrl';
+
+/* ROLLBACK
+DELETE FROM ctm.service_properties WHERE serviceMasterId=@masterId AND environmentCode='PRO' AND servicePropertyKey='serviceUrl';
+*/
