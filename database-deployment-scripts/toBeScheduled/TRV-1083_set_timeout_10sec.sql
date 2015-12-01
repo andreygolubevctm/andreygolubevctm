58819533805c7e9193906c15cde88b851a670e71
David Degnan <david.degnan@comparethemarket.com.au>
2015-12-01 12:19:23 +1000

    TRV-1083 - need to wrap it

--- database-deployment-scripts/toBeScheduled/TRV-1083_set_timeout_10sec.sql
+++ database-deployment-scripts/toBeScheduled/TRV-1083_set_timeout_10sec.sql
@@ -1,3 +1,3 @@
-SET @MASTERID = SELECT serviceMasterId FROM ctm.service_master WHERE serviceCode='quoteServiceBER' AND verticalId=2;
+SET @MASTERID = (SELECT serviceMasterId FROM ctm.service_master WHERE serviceCode='quoteServiceBER' AND verticalId=2);
 
 UPDATE ctm.service_properties SET servicePropertyValue='10' WHERE servicePropertyKey='timeout' AND servicemasterId=@MASTERID;
