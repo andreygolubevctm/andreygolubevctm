32d9484fb69cb334549661f0d9fcbf79cf141e58
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-12-02 14:27:56 +1000

    LIF-350 Updated Checker comment for Adrian the Complainer and Luke the Deployer

--- database-deployment-scripts/2015/50-51/2015-12-03 OOC LIF-350/LIF-350 Add Content Control.sql
+++ database-deployment-scripts/2015/50-51/2015-12-03 OOC LIF-350/LIF-350 Add Content Control.sql
@@ -6,7 +6,7 @@ INSERT INTO ctm.content_control (contentControlId,styleCodeId,verticalId,content
 INSERT INTO ctm.content_control (contentControlId,styleCodeId,verticalId,contentCode,contentKey,contentStatus,effectiveStart,effectiveEnd,contentValue) VALUES (null,'1','6','Competition','competitionCheckboxText','','2015-12-03 09:00:00','2016-03-03 08:59:59','Yes, I agree to the <a target=\'_blank\'  href=\'/static/competitions/1000competition.pdf\'>competition terms and conditions</a>.');
 INSERT INTO ctm.content_control (contentControlId,styleCodeId,verticalId,contentCode,contentKey,contentStatus,effectiveStart,effectiveEnd,contentValue) VALUES (null,'1','8','Competition','competitionCheckboxText','','2015-12-03 09:00:00','2016-03-03 08:59:59','Yes, I agree to the <a target=\'_blank\'  href=\'/static/competitions/1000competition.pdf\'>competition terms and conditions</a>.');
 
--- CHECKER 0 before 4 after
+-- CHECKER 0 before 6 after
 SELECT * FROM ctm.content_control WHERE verticalId IN (6,8) AND contentCode='Competition' AND contentKey IN ('competitionId','competitionEnabled','competitionCheckboxText') AND effectiveStart='2015-12-03 09:00:00' AND effectiveEnd='2016-03-03 08:59:59' ORDER BY verticalId ASC;
 
 /* ROLLBACK
