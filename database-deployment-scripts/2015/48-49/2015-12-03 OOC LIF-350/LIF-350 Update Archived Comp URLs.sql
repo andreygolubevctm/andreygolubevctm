b74eb02f4044bd00a4c588c70518b7bd8999e1b6
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2015-12-02 14:15:30 +1000

    LIF-350 Updated to reflect files moved out of archive folder

--- database-deployment-scripts/2015/48-49/2015-12-03 OOC LIF-350/LIF-350 Update Archived Comp URLs.sql
+++ database-deployment-scripts/2015/48-49/2015-12-03 OOC LIF-350/LIF-350 Update Archived Comp URLs.sql
@@ -1,12 +1,12 @@
 -- UPDATER
 -- 12 Updated
-UPDATE ctm.content_control SET contentValue=REPLACE(contentValue,'http://www.comparethemarket.com.au/competition/','https://www.comparethemarket.com.au/static/competitions/archived/') WHERE contentCode='Competition' AND contentKey='competitionCheckboxText' AND contentValue LIKE '%comparethemarket.com.au/competition/%';
+UPDATE ctm.content_control SET contentValue=REPLACE(contentValue,'http://www.comparethemarket.com.au/competition/','https://www.comparethemarket.com.au/static/competitions/') WHERE contentCode='Competition' AND contentKey='competitionCheckboxText' AND contentValue LIKE '%comparethemarket.com.au/competition/%';
 -- 9 Updated
-UPDATE ctm.content_control SET contentValue=REPLACE(contentValue,'http://comparethemarket.com.au/competition/','https://www.comparethemarket.com.au/static/competitions/archived/') WHERE contentCode='Competition' AND contentKey='competitionCheckboxText' AND contentValue LIKE '%comparethemarket.com.au/competition/%';
+UPDATE ctm.content_control SET contentValue=REPLACE(contentValue,'http://comparethemarket.com.au/competition/','https://www.comparethemarket.com.au/static/competitions/') WHERE contentCode='Competition' AND contentKey='competitionCheckboxText' AND contentValue LIKE '%comparethemarket.com.au/competition/%';
 
 -- CHECKER 21 Rows before and zero after
 SELECT * FROM ctm.content_control WHERE contentCode='Competition' AND contentKey='competitionCheckboxText' AND contentValue LIKE '%comparethemarket.com.au/competition/%';
 
 /* ROLLBACK
-UPDATE ctm.content_control SET contentValue=REPLACE(contentValue,'https://www.comparethemarket.com.au/static/competitions/archived/','http://www.comparethemarket.com.au/competition/') WHERE contentCode='Competition' AND contentKey='competitionCheckboxText' AND contentValue LIKE '%comparethemarket.com.au/static/competitions/%';
+UPDATE ctm.content_control SET contentValue=REPLACE(contentValue,'https://www.comparethemarket.com.au/static/competitions/','http://www.comparethemarket.com.au/competition/') WHERE contentCode='Competition' AND contentKey='competitionCheckboxText' AND contentValue LIKE '%comparethemarket.com.au/static/competitions/%';
 */
