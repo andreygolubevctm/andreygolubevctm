36bf2a3aa76b84d90f5b76cb86611e7d33d90b5b
dkocovski <dragan.kocovski@comparethemarket.com.au>
2015-12-11 11:45:42 +1000

    BER-72: turn off springloaded for now since it doesnt work any more with tomcat7-maven-plugin

--- scripts/start_tomcat7.bat
+++ scripts/start_tomcat7.bat
@@ -3,5 +3,5 @@ cd ..
 IF NOT EXIST "target\springloaded\springloaded.jar" call mvn dependency:copy@copy-springloaded
 IF NOT EXIST "target\ctm-*\WEB-INF\lib" call mvn -Pskip-tests -Pskip-quality compile war:exploded
 rem set MAVEN_OPTS=-javaagent:target/springloaded/springloaded.jar -noverify -Xint -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15005
-set MAVEN_OPTS=-javaagent:target/springloaded/springloaded.jar -noverify -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15005
+set MAVEN_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15005
 call mvn tomcat7:run@tomcat7-run
