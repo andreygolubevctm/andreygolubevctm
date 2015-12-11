184051158eff473343a932a0e8351d62bad48e37
fleo <fernando.leon@comparethemarket.com.au>
2015-12-11 14:45:10 +1000

    LIF-349 Cr comments response

--- database-deployment-scripts/2015/50-51/2015-12-11 LIF349 For NXQ ONLY/FOR-NXQ-ONLY-update.sql
+++ database-deployment-scripts/2015/50-51/2015-12-11 LIF349 For NXQ ONLY/FOR-NXQ-ONLY-update.sql
@@ -3,12 +3,12 @@
 -- TO BE RUN ONLY ON NXQ ANY OTHER ENVIRONMENT IGNORE
 
 
-UPDATE ctm.content_control SET effectiveStart='2015-12-10' WHERE verticalId IN (6,8) AND contentkey='footerParticipatingSuppliers' AND effectiveStart='2015-12-15 07:00:00' limit 2;
+UPDATE ctm.content_control SET effectiveStart='2015-12-10 00:00:00' WHERE verticalId IN (6,8) AND contentkey='footerParticipatingSuppliers' AND effectiveStart='2015-12-15 07:00:00' limit 2;
 
 -- test expect 2
-select count(*) from ctm.content_control where effectiveStart='2015-12-10' and verticalId in (6,8) and contentKey = 'footerParticipatingSuppliers';
+select count(*) from ctm.content_control where effectiveStart='2015-12-10 00:00:00' and verticalId in (6,8) and contentKey = 'footerParticipatingSuppliers';
 
 -- rollback
--- update ctm.content_control set effectiveStart='2015-12-15 07:00:00' where verticalId in (6,8) and contentkey='footerParticipatingSuppliers' AND effectiveStart='2015-12-10' limit 2;
+-- update ctm.content_control set effectiveStart='2015-12-15 07:00:00' where verticalId in (6,8) and contentkey='footerParticipatingSuppliers' AND effectiveStart='2015-12-10 00:00:00' limit 2;
 -- test expect 2
 -- select count(*) from ctm.content_control where effectiveStart='2015-12-15 07:00:00' and verticalId in (6,8) and contentKey = 'footerParticipatingSuppliers';
