3e2567357ece7711b779b20bb0d5194e115450ae
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2015-12-14 17:44:57 +1000

    BER-72: moving in fix

--- src/main/java/com/ctm/web/energy/apply/adapter/EnergyApplyServiceRequestAdapter.java
+++ src/main/java/com/ctm/web/energy/apply/adapter/EnergyApplyServiceRequestAdapter.java
@@ -21,13 +21,9 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
-/**
- * Created by lbuchanan on 30/11/2015.
- */
 public class EnergyApplyServiceRequestAdapter implements WebRequestAdapter<EnergyApplyPostRequestPayload, EnergyApplicationDetails> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EnergyApplyServiceRequestAdapter.class);
-
 
     @Override
     public EnergyApplicationDetails adapt(EnergyApplyPostRequestPayload energyApplyPostRequestPayload) {
@@ -46,99 +42,121 @@ public class EnergyApplyServiceRequestAdapter implements WebRequestAdapter<Energ
                 energyApplyPostRequestPayload.getUtilities().getApplication().getDetails());
 
 
+
         if (details.isPresent()) {
             // - ApplicantDetails
-            ApplicantDetails applicantDetails = ApplicantDetails.newBuilder()
-                    .firstName(details.get().getFirstName())
-                    .lastName(details.get().getLastName())
-                    .title(details.get().getTitle())
-                    .dateOfBirth(LocalDateUtils.parseAUSLocalDate(details.get().getDob()))
-                    .build();
-            energyApplicationDetailsBuilder = energyApplicationDetailsBuilder.applicantDetails(applicantDetails);
+            energyApplicationDetailsBuilder = adaptApplicantDetails(energyApplicationDetailsBuilder, details);
 
             // - ContactDetails
-            ContactDetails contactDetails = ContactDetails.newBuilder()
-                    .email(details.get().getEmail())
-                    .mobileNumber(details.get().getMobileNumberinput())
-                    .otherPhoneNumber(details.get().getOtherPhoneNumberinput())
-                    .build();
-            energyApplicationDetailsBuilder = energyApplicationDetailsBuilder.contactDetails(contactDetails);
+            energyApplicationDetailsBuilder = adaptContactDetails(energyApplicationDetailsBuilder, details);
 
             // -- Supply address
-            Optional<Address> houseAddress = MiscUtils.resolve(() ->
-                    energyApplyPostRequestPayload.getUtilities().getApplication().getDetails().getAddress());
-
-            AddressDetails.Builder supplyAddressBuilder = AddressDetails.newBuilder();
-            if (houseAddress.isPresent()) {
-                supplyAddressBuilder.unitNumber(houseAddress.get().getUnitShop())
-                        .streetNumber(houseAddress.get().getStreetNum())
-                        .suburbName(houseAddress.get().getSuburbName())
-                        .state(State.valueOf(houseAddress.get().getState()));
-                if (YesNo.Y.equals(houseAddress.get().getNonStd())) {
-                    supplyAddressBuilder.unitType(houseAddress.get().getNonStdUnitType())
-                            .postcode(houseAddress.get().getNonStdPostCode())
-                            .streetName(houseAddress.get().getNonStdStreet());
-                } else {
-                    supplyAddressBuilder.unitType(houseAddress.get().getUnitType())
-                            .postcode(houseAddress.get().getPostCode())
-                            .streetName(houseAddress.get().getStreetName());
-                }
-            }
+            AddressDetails.Builder supplyAddressBuilder = adaptSupplyAddress(energyApplyPostRequestPayload);
 
             // -- Postal address
-            Optional<Address> postalAddress = MiscUtils.resolve(() ->
-                    energyApplyPostRequestPayload.getUtilities().getApplication().getDetails().getPostal());
-            AddressDetails.Builder postalAddressBuilder = AddressDetails.newBuilder();
-            if (postalAddress.isPresent()) {
-                postalAddressBuilder.unitNumber(postalAddress.get().getUnitShop())
-                        .streetNumber(postalAddress.get().getStreetNum())
-                        .suburbName(postalAddress.get().getSuburbName())
-                        .state(State.valueOf(postalAddress.get().getState()));
-
-                // @TODO FIX ME !!! Bug in form, the postalAddress.nonStd field is not being set
-                // if (YesNo.Y.equals(postalAddress.get().getNonStd()) {
-                postalAddressBuilder.unitType(postalAddress.get().getNonStdUnitType())
-                        .postcode(postalAddress.get().getNonStdPostCode())
-                        .streetName(postalAddress.get().getNonStdStreet());
-                //} else {
-                if (postalAddress.get().getUnitType() != null) {
-                    postalAddressBuilder.unitType(postalAddress.get().getUnitType());
-                }
-                if (postalAddress.get().getPostCode() != null) {
-                    postalAddressBuilder.postcode(postalAddress.get().getPostCode());
-                }
-                if (postalAddress.get().getStreetName() != null) {
-                    postalAddressBuilder.streetName(postalAddress.get().getStreetName());
-                }
-                // }
-            }
+            Optional<AddressDetails> postalAddressBuilder = adaptPostal(energyApplyPostRequestPayload, YesNo.Y.equals(details.get().getPostalMatch()));
 
             // - Household
-            Optional<HouseHoldDetailsWebRequest> householdDetails = MiscUtils.resolve(() -> energyApplyPostRequestPayload.getUtilities().getHouseholdDetails());
-            if (householdDetails.isPresent()) {
-                RelocationDetails.Builder relocationDetailsBuilder = RelocationDetails.newBuilder();
-                if (YesNo.Y.equals(householdDetails.get().getMovingIn())) {
-                    relocationDetailsBuilder
-                            .movingIn(true)
-                            .movingDate(LocalDateUtils.parseAUSLocalDate(details.get().getMovingDate()));
-                } else {
-                    relocationDetailsBuilder
-                            .movingIn(false);
-                }
-                energyApplicationDetailsBuilder = energyApplicationDetailsBuilder.relocationDetails(relocationDetailsBuilder.build());
-            }
-
-            ApplicationAddress address = ApplicationAddress.newBuilder()
-                    .supplyAddressDetails(supplyAddressBuilder.build())
-                    .postalAddressDetails(postalAddressBuilder.build())
-                    .postalMatch(YesNo.Y.equals(details.get().getPostalMatch()))
-                    .build();
-            energyApplicationDetailsBuilder = energyApplicationDetailsBuilder.address(address);
+            energyApplicationDetailsBuilder = adaptHouseHold(energyApplyPostRequestPayload, energyApplicationDetailsBuilder, details, supplyAddressBuilder, postalAddressBuilder);
         }
 
         EnergyApplicationDetails energyApplicationDetails = energyApplicationDetailsBuilder.build();
         LOGGER.debug("energyApplicationDetails={}", MiscUtils.toJson(energyApplicationDetails));
 
         return energyApplicationDetails;
+    }
+
+    private EnergyApplicationDetails.Builder adaptApplicantDetails(EnergyApplicationDetails.Builder energyApplicationDetailsBuilder, Optional<Details> details) {
+        ApplicantDetails applicantDetails = ApplicantDetails.newBuilder()
+                .firstName(details.get().getFirstName())
+                .lastName(details.get().getLastName())
+                .title(details.get().getTitle())
+                .dateOfBirth(LocalDateUtils.parseAUSLocalDate(details.get().getDob()))
+                .build();
+        energyApplicationDetailsBuilder = energyApplicationDetailsBuilder.applicantDetails(applicantDetails);
+        return energyApplicationDetailsBuilder;
+    }
+
+    private EnergyApplicationDetails.Builder adaptContactDetails(EnergyApplicationDetails.Builder energyApplicationDetailsBuilder, Optional<Details> details) {
+        ContactDetails contactDetails = ContactDetails.newBuilder()
+                .email(details.get().getEmail())
+                .mobileNumber(details.get().getMobileNumberinput())
+                .otherPhoneNumber(details.get().getOtherPhoneNumberinput())
+                .build();
+        energyApplicationDetailsBuilder = energyApplicationDetailsBuilder.contactDetails(contactDetails);
+        return energyApplicationDetailsBuilder;
+    }
+
+    private AddressDetails.Builder adaptSupplyAddress(EnergyApplyPostRequestPayload energyApplyPostRequestPayload) {
+        Optional<Address> houseAddress = MiscUtils.resolve(() ->
+                energyApplyPostRequestPayload.getUtilities().getApplication().getDetails().getAddress());
+
+        AddressDetails.Builder supplyAddressBuilder = AddressDetails.newBuilder();
+        if (houseAddress.isPresent()) {
+            supplyAddressBuilder.unitNumber(houseAddress.get().getUnitShop())
+                    .streetNumber(houseAddress.get().getStreetNum())
+                    .suburbName(houseAddress.get().getSuburbName())
+                    .state(State.valueOf(houseAddress.get().getState()));
+            if (YesNo.Y.equals(houseAddress.get().getNonStd())) {
+                supplyAddressBuilder.unitType(houseAddress.get().getNonStdUnitType())
+                        .postcode(houseAddress.get().getNonStdPostCode())
+                        .streetName(houseAddress.get().getNonStdStreet());
+            } else {
+                supplyAddressBuilder.unitType(houseAddress.get().getUnitType())
+                        .postcode(houseAddress.get().getPostCode())
+                        .streetName(houseAddress.get().getStreetName());
+            }
+        }
+        return supplyAddressBuilder;
+    }
+
+    private Optional<AddressDetails> adaptPostal(EnergyApplyPostRequestPayload energyApplyPostRequestPayload, boolean postalMatch) {
+        Optional<Address> postalAddress = MiscUtils.resolve(() ->
+                energyApplyPostRequestPayload.getUtilities().getApplication().getDetails().getPostal());
+        AddressDetails.Builder postalAddressBuilder = AddressDetails.newBuilder();
+        if (!postalMatch && postalAddress.isPresent()) {
+            postalAddressBuilder.unitNumber(postalAddress.get().getUnitShop())
+                    .streetNumber(postalAddress.get().getStreetNum())
+                    .suburbName(postalAddress.get().getSuburbName())
+                    .state(State.valueOf(postalAddress.get().getState()));
+            postalAddressBuilder.unitType(postalAddress.get().getNonStdUnitType())
+                    .postcode(postalAddress.get().getNonStdPostCode())
+                    .streetName(postalAddress.get().getNonStdStreet());
+            if (postalAddress.get().getUnitType() != null) {
+                postalAddressBuilder.unitType(postalAddress.get().getUnitType());
+            }
+            if (postalAddress.get().getPostCode() != null) {
+                postalAddressBuilder.postcode(postalAddress.get().getPostCode());
+            }
+            if (postalAddress.get().getStreetName() != null) {
+                postalAddressBuilder.streetName(postalAddress.get().getStreetName());
+            }
+            return Optional.of(postalAddressBuilder.build());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private EnergyApplicationDetails.Builder adaptHouseHold(EnergyApplyPostRequestPayload energyApplyPostRequestPayload,
+                                                            EnergyApplicationDetails.Builder energyApplicationDetailsBuilder, Optional<Details> details,
+                                                            AddressDetails.Builder supplyAddressBuilder, Optional<AddressDetails> postalAddress) {
+        Optional<HouseHoldDetailsWebRequest> householdDetails = MiscUtils.resolve(() -> energyApplyPostRequestPayload.getUtilities().getHouseholdDetails());
+        if (householdDetails.isPresent()) {
+            RelocationDetails.Builder relocationDetailsBuilder = RelocationDetails.newBuilder();
+            relocationDetailsBuilder
+                    .movingIn(YesNo.Y.equals(householdDetails.get().getMovingIn()));
+            if (YesNo.Y.equals(householdDetails.get().getMovingIn())) {
+                relocationDetailsBuilder.movingDate(LocalDateUtils.parseAUSLocalDate(details.get().getMovingDate()));
+            }
+            energyApplicationDetailsBuilder = energyApplicationDetailsBuilder.relocationDetails(relocationDetailsBuilder.build());
+        }
+
+        ApplicationAddress address = ApplicationAddress.newBuilder()
+                .supplyAddressDetails(supplyAddressBuilder.build())
+                .postalAddressDetails(postalAddress)
+                .postalMatch(YesNo.Y.equals(details.get().getPostalMatch()))
+                .build();
+        energyApplicationDetailsBuilder = energyApplicationDetailsBuilder.address(address);
+        return energyApplicationDetailsBuilder;
     }
 }
