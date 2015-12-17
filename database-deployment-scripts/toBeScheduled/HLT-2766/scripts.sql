562a3e492d4adb431ca951394a7c57483b1be60e
fleo <fernando.leon@comparethemarket.com.au>
2015-12-17 12:06:29 +1000

    HLT-2766 sql changes dropdown description jrny1

---
+++ database-deployment-scripts/toBeScheduled/HLT-2766/scripts.sql
@@ -0,0 +1,39 @@
+SET @TYPE = 'healthSitu';
+
+UPDATE aggregator.general SET description = 'Compare your options'  WHERE type = @TYPE
+   AND code = 'LC' and description = 'Compare my options' and (status IS NULL or status !=0) and orderSeq = 1 limit 1;
+
+UPDATE aggregator.general SET description = 'Replace your current cover'  WHERE type = @TYPE
+   AND code = 'LBC' and description = 'Replace my current cover' and (status IS NULL or status !=0) and orderSeq = 2 limit 1;
+
+UPDATE aggregator.general SET description = 'Grow your family'  WHERE type = @TYPE
+   AND code = 'CSF' and description = 'Grow my family' and (status IS NULL or status !=0) and orderSeq = 3 limit 1;
+
+-- test expect 3
+select count(*) from aggregator.general where type = @TYPE
+and description like '%your%'  and (status IS NULL OR status !=0) order by orderseq;
+
+-- test expect 0
+select count(*) from aggregator.general where type = @TYPE
+and description like '%my%'  and (status IS NULL OR status !=0) order by orderseq;
+
+-- ROLL back
+/*
+UPDATE aggregator.general SET description = 'Compare my options'  WHERE type = @TYPE
+   AND code = 'LC' and description = 'Compare your options' and (status IS NULL or status !=0) and orderSeq = 1 limit 1;
+
+UPDATE aggregator.general SET description = 'Replace my current cover'  WHERE type = @TYPE
+   AND code = 'LBC' and description = 'Replace your current cover' and (status IS NULL or status !=0) and orderSeq = 2 limit 1;
+
+UPDATE aggregator.general SET description = 'Grow my family'  WHERE type = @TYPE
+   AND code = 'CSF' and description = 'Grow your family' and (status IS NULL or status !=0) and orderSeq = 3 limit 1;
+
+-- test expect 0
+select count(*) from aggregator.general where type = @TYPE
+and description like '%your%'  and (status IS NULL OR status !=0) order by orderseq;
+
+-- test expect 3
+select count(*) from aggregator.general where type = @TYPE
+and description like '%my%'  and (status IS NULL OR status !=0) order by orderseq;
+
+ */
