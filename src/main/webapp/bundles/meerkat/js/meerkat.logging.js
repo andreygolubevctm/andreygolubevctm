e9fb1c7ec083d4b58cde0f0304fad0ff488a6eff
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2015-12-18 11:26:59 +1000

    HLT-2767: update copy on compare basket and fix sizes across browsers and resolutions

--- src/main/webapp/bundles/meerkat/js/meerkat.logging.js
+++ src/main/webapp/bundles/meerkat/js/meerkat.logging.js
@@ -553,7 +553,7 @@ meerkat.logging.init = function () {
 	//
 	// One day we will remove the old code in favour of this...
 	if(meerkat.site.useNewLogging) {
-		initializeNewLogging();
+		//initializeNewLogging();
 	}
 
 	var theAppName = '';
