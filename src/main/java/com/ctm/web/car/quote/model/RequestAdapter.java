907205b196e5c9a61c757b4a856acaaf07034021
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2016-01-05 13:14:02 +1000

    CAR-1170 Fixed issue with AI returning Tradie Sure products

--- src/main/java/com/ctm/web/car/quote/model/RequestAdapter.java
+++ src/main/java/com/ctm/web/car/quote/model/RequestAdapter.java
@@ -31,13 +31,14 @@ public class RequestAdapter {
         quoteRequest.setRiskAddress(createRiskAddress(carQuote));
         quoteRequest.setVehicle(createVehicle(carQuote));
 
-        // CAR-1170. For vehicle use, two new radio button introduced. Override created here so that the data team still get Y/N values for their report
-        if (quoteRequest.getVehicle().getGoodsPayment() != null && quoteRequest.getVehicle().getGoodsPayment().equals("Y")) {
-            quoteRequest.getVehicle().setUse("13"); // CAR-1170
-        }
 
         if (quoteRequest.getVehicle().getPassengerPayment() != null && quoteRequest.getVehicle().getPassengerPayment().equals("Y")) {
-            quoteRequest.getVehicle().setUse("15"); // CAR-1170
+            quoteRequest.getVehicle().setUse("13"); // CAR-1170
+        } else {
+            // CAR-1170. For vehicle use, two new radio button introduced. Override created here so that the data team still get Y/N values for their report
+            if (quoteRequest.getVehicle().getGoodsPayment() != null && quoteRequest.getVehicle().getGoodsPayment().equals("Y")) {
+                quoteRequest.getVehicle().setUse("15"); // CAR-1170
+            }
         }
 
 
