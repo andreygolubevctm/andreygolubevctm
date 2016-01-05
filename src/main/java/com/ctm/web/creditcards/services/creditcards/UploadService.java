1296df0c3309343a22bfab2184761c5fdf3a7568
David Degnan <david.degnan@comparethemarket.com.au>
2016-01-06 09:06:46 +1000

    CCA-109

--- src/main/java/com/ctm/web/creditcards/services/creditcards/UploadService.java
+++ src/main/java/com/ctm/web/creditcards/services/creditcards/UploadService.java
@@ -21,13 +21,6 @@ public class UploadService {
 		String providerName = "";
 		String effectiveDate = file.effectiveDate;
 
-		if(file.deleteId.length() > 0) {
-			update.append("DELETE FROM ctm.category_product_mapping WHERE productid IN ("+file.deleteId+");\r\n");
-			update.append("DELETE FROM ctm.product_properties_text WHERE productid IN ("+file.deleteId+");\r\n");
-			update.append("DELETE FROM ctm.product_properties WHERE productid IN ("+file.deleteId+");\r\n");
-			update.append("DELETE FROM ctm.product_master WHERE productid IN ("+file.deleteId+");\r\n");
-		}
-
 		try {
 			SimpleDatabaseConnection dbSource = null;
 			try {
@@ -44,7 +37,6 @@ public class UploadService {
 
 				while (results.next()) {
 					providerId = results.getInt("ProviderId");
-					providerName = results.getString("Name");
 				}
 
 			} catch (SQLException | NamingException e) {
@@ -53,8 +45,14 @@ public class UploadService {
 			finally {
 				dbSource.closeConnection();
 			}
-				int PRODUCT_CODE_COLUMN_NUMBER = 0;
-				int PROVIDER_NAME_COLUMN_NUMBER = 1;
+
+			update.append("DELETE FROM ctm.category_product_mapping WHERE productid IN (SELECT productid FROM ctm.product_master WHERE productCat='CREDITCARD' AND providerid="+providerId+");\r\n");
+			update.append("DELETE FROM ctm.product_properties_text WHERE productid IN (SELECT productid FROM ctm.product_master WHERE productCat='CREDITCARD' AND providerid="+providerId+");\r\n");
+			update.append("DELETE FROM ctm.product_properties WHERE productid IN (SELECT productid FROM ctm.product_master WHERE productCat='CREDITCARD' AND providerid="+providerId+");\r\n");
+			update.append("DELETE FROM ctm.product_master WHERE productCat='CREDITCARD' AND providerid="+providerId+");\r\n");
+
+			int PRODUCT_CODE_COLUMN_NUMBER = 0;
+				//int PROVIDER_NAME_COLUMN_NUMBER = 1;
 
 				int PROPERTY_SLUG_COLUMN_NUMBER = 2;
 				int PROPERTY_META_PAGE_COLUMN_NUMBER = 3;
@@ -148,193 +146,152 @@ public class UploadService {
 						}
 
 						if(part.length > 0){
-							int productId = -1;
+							update.append("\r\n\r\nINSERT INTO ctm.product_master (ProductCat,ProductCode,ProviderId, ShortTitle, LongTitle,EffectiveStart,EffectiveEnd,Status) VALUES('CREDITCARD','"+part[PRODUCT_CODE_COLUMN_NUMBER]+"',"+providerId+",'"+part[PRODUCT_SHORT_DESC_COLUMN_NUMBER]+"','"+part[PRODUCT_SHORT_DESC_COLUMN_NUMBER]+"','"+effectiveDate+"','2040-12-31','');\r\n");
+							update.append("SET @product_id = LAST_INSERT_ID();\r\n");
 
-							productCode = part[PRODUCT_CODE_COLUMN_NUMBER];
+							if (productCode != prevProductCode){
+								prevProductCode = productCode;
+							}
 
-							if (part[PROVIDER_NAME_COLUMN_NUMBER].equals(providerName)){
+							map.clear();
+							map.put("slug",PROPERTY_SLUG_COLUMN_NUMBER);
+							map.put("meta-page-title",PROPERTY_META_PAGE_COLUMN_NUMBER);
+							map.put("meta-page-desc",PROPERTY_META_PAGE_DESC_COLUMN_NUMBER);
+							map.put("product-type",PROPERTY_TYPE_COLUMN_NUMBER);
+							map.put("card-class",PROPERTY_CARD_CLASS_COLUMN_NUMBER);
 
-								try {
-									PreparedStatement stmt;
+							map.put("intro-rate",PROPERTY_INTRO_INTEREST_RATE_COLUMN_NUMBER);
+							map.put("intro-rate-period",PROPERTY_INTRO_INTEREST_RATE_PERIOD_COLUMN_NUMBER);
+							map.put("interest-rate",PROPERTY_INTEREST_RATE_COLUMN_NUMBER);
+							map.put("intro-balance-transfer-rate",PROPERTY_INTRO_BALANCE_TRANSFER_RATE_COLUMN_NUMBER);
+							map.put("intro-balance-transfer-rate-period",PROPERTY_INTRO_BALANCE_TRANSFER_RATE_PERIOD_COLUMN_NUMBER);
 
-									dbSource = new SimpleDatabaseConnection();
+							map.put("balance-transfer-rate",PROPERTY_BALANCE_TRANSFER_RATE_COLUMN_NUMBER);
+							map.put("balance-transfer-fee",PROPERTY_BALANCE_TRANSFER_FEE_COLUMN_NUMBER);
 
-									stmt = dbSource.getConnection().prepareStatement(
-											"SELECT productId FROM ctm.product_master WHERE ProductCode = ? AND ProductID NOT IN ("+file.deleteId+") AND Status != 'X'"
-									);
+							map.put("intro-annual-fee",PROPERTY_INTRO_ANNUAL_FEE_COLUMN_NUMBER);
+							map.put("intro-annual-fee-period",PROPERTY_INTRO_ANNUAL_FEE_PERIOD_COLUMN_NUMBER);
 
-									stmt.setString(1, part[PRODUCT_CODE_COLUMN_NUMBER]);
+							map.put("annual-fee",PROPERTY_ANNUAL_FEE_COLUMN_NUMBER);
+							map.put("cash-advance-rate",PROPERTY_CASH_ADVANCE_COLUMN_NUMBER);
+							map.put("product-desc",PROPERTY_LONG_DESC_COLUMN_NUMBER);
+							map.put("interest-free-days",PROPERTY_INTEREST_FREE_DAYS_COLUMN_NUMBER);
 
-									ResultSet results = stmt.executeQuery();
+							map.put("minimum-income",PROPERTY_MINIMUM_INCOME_COLUMN_NUMBER);
+							map.put("minimum-credit-limit",PROPERTY_MINIMUM_CREDIT_LIMIT_COLUMN_NUMBER);
+							map.put("maximum-credit-limit",PROPERTY_MAXIMUM_CREDIT_LIMIT_COLUMN_NUMBER);
+							map.put("minimum-monthly-repayment",PROPERTY_MINIMUM_MONTHLY_REPAYMENT_COLUMN_NUMBER);
 
-									while (results.next()) {
-										productId = results.getInt("productId");
-									}
-								} catch (SQLException | NamingException e) {
+							map.put("late-payment-fee",PROPERTY_LATE_PAYMENT_FEE_COLUMN_NUMBER);
+							map.put("additional-card-holder",PROPERTY_ADDITIONAL_CARD_HOLDER_COLUMN_NUMBER);
+							map.put("foreign-exchange-fees",PROPERTY_FOREIGN_EXCHANGE_FEES_COLUMN_NUMBER);
+							map.put("complimentary-travel-insurance",PROPERTY_COMPLIMENTARY_TRAVEL_INSURANCE_COLUMN_NUMBER);
+							map.put("extended-warranty",PROPERTY_EXTENDED_WARRANTY_COLUMN_NUMBER);
+							map.put("available-temporary-residents",PROPERTY_AVAILABLE_TEMPORARY_RESIDENTS_COLUMN_NUMBER);
 
-								}
-								finally {
-									dbSource.closeConnection();
-								}
+							map.put("rewards-desc",PROPERTY_REWARDS_DESC_COLUMN_NUMBER);
+							//map.put("rewards-standard-card-class",PROPERTY_REWARDS_CARD_CLASS_COLUMN_NUMBER);
+							map.put("rewards-standard-card-points",PROPERTY_REWARDS_STANDARD_POINTS_COLUMN_NUMBER);
+							map.put("rewards-amex-card-points",PROPERTY_REWARDS_AMEX_POINTS_COLUMN_NUMBER);
+							map.put("rewards-bonus-points",PROPERTY_BONUS_POINTS_COLUMN_NUMBER);
+							map.put("rewards-bonus-points-type",PROPERTY_BONUS_POINTS_TYPE);
 
-								if (productId == -1){
-									update.append("\r\n\r\nINSERT INTO ctm.product_master (ProductCat,ProductCode,ProviderId, ShortTitle, LongTitle,EffectiveStart,EffectiveEnd,Status) VALUES('CREDITCARD','"+part[PRODUCT_CODE_COLUMN_NUMBER]+"',"+providerId+",'"+part[PRODUCT_SHORT_DESC_COLUMN_NUMBER]+"','"+part[PRODUCT_SHORT_DESC_COLUMN_NUMBER]+"','"+effectiveDate+"','2040-12-31','');\r\n");
-									update.append("SET @product_id = LAST_INSERT_ID();\r\n");
+							map.put("terms-interest-rate",PROPERTY_INTEREST_RATE_TERMS_COLUMN_NUMBER);
+							map.put("terms-rewards",PROPERTY_REWARDS_TERMS_COLUMN_NUMBER);
+							map.put("terms-balance-transfer",PROPERTY_BALANCE_TRANSFER_TERMS_COLUMN_NUMBER);
+							map.put("terms-other-features",PROPERTY_OTHER_FEATURES_TERMS_COLUMN_NUMBER);
+							map.put("terms-general",PROPERTY_GENERAL_TERMS_COLUMN_NUMBER);
+							map.put("special-offer",PROPERTY_SPECIAL_OFFER_COLUMN_NUMBER);
+							map.put("other-features",PROPERTY_OTHER_FEATURES_COLUMN_NUMBER);
+							map.put("categories",PROPERTY_CATEGORIES_COLUMN_NUMBER);
+							map.put("handover-url",PROPERTY_HANDOVER_URL_COLUMN_NUMBER);
 
-								} else {
-									update.append("\r\n\r\nUPDATE ctm.product_master SET ShortTitle = '"+part[PRODUCT_SHORT_DESC_COLUMN_NUMBER]+"', LongTitle = '"+part[PRODUCT_SHORT_DESC_COLUMN_NUMBER]+"' WHERE productId = "+productId+";\r\n");
-									update.append("SET @product_id = "+productId+";\r\n");
-									update.append("DELETE FROM ctm.product_properties WHERE productid = @product_id;\r\n");
-									update.append("DELETE FROM ctm.product_properties_text WHERE productid = @product_id;\r\n");
-								}
+							for (String key : map.keySet()){
 
-								if (productCode != prevProductCode){
-									prevProductCode = productCode;
-								}
+								int idx = map.get(key);
 
-									map.clear();
-									map.put("slug",PROPERTY_SLUG_COLUMN_NUMBER);
-									map.put("meta-page-title",PROPERTY_META_PAGE_COLUMN_NUMBER);
-									map.put("meta-page-desc",PROPERTY_META_PAGE_DESC_COLUMN_NUMBER);
-									map.put("product-type",PROPERTY_TYPE_COLUMN_NUMBER);
-									map.put("card-class",PROPERTY_CARD_CLASS_COLUMN_NUMBER);
+								if (idx < part.length && !(part[idx].equals(""))) {
 
-									map.put("intro-rate",PROPERTY_INTRO_INTEREST_RATE_COLUMN_NUMBER);
-									map.put("intro-rate-period",PROPERTY_INTRO_INTEREST_RATE_PERIOD_COLUMN_NUMBER);
-									map.put("interest-rate",PROPERTY_INTEREST_RATE_COLUMN_NUMBER);
-									map.put("intro-balance-transfer-rate",PROPERTY_INTRO_BALANCE_TRANSFER_RATE_COLUMN_NUMBER);
-									map.put("intro-balance-transfer-rate-period",PROPERTY_INTRO_BALANCE_TRANSFER_RATE_PERIOD_COLUMN_NUMBER);
+									//see if the variable actually needs a currency format
+									String formatted = "";
 
-									map.put("balance-transfer-rate",PROPERTY_BALANCE_TRANSFER_RATE_COLUMN_NUMBER);
-									map.put("balance-transfer-fee",PROPERTY_BALANCE_TRANSFER_FEE_COLUMN_NUMBER);
+									switch(idx) {
+										case 8:
+										case 9:
+										case 10:
+										case 11:
+										case 12:
+										case 13:
+										case 14:
+										case 15:
+										case 16:
+										case 17:
+										case 18:
+										case 19:
+										case 20:
+										case 21:
+										case 36:
+										case 37:
+										case 38:
+										case 39:
+										case 40:
+										case 41:
+											if(part[idx].equals("N/A")) {
+												part[idx] = part[idx].replaceAll("N/A", "");
+												formatted = "NULL";
+											} else {
+												part[idx] = part[idx].replaceAll(",", "");
 
-									map.put("intro-annual-fee",PROPERTY_INTRO_ANNUAL_FEE_COLUMN_NUMBER);
-									map.put("intro-annual-fee-period",PROPERTY_INTRO_ANNUAL_FEE_PERIOD_COLUMN_NUMBER);
-
-									map.put("annual-fee",PROPERTY_ANNUAL_FEE_COLUMN_NUMBER);
-									map.put("cash-advance-rate",PROPERTY_CASH_ADVANCE_COLUMN_NUMBER);
-									map.put("product-desc",PROPERTY_LONG_DESC_COLUMN_NUMBER);
-									map.put("interest-free-days",PROPERTY_INTEREST_FREE_DAYS_COLUMN_NUMBER);
-
-									map.put("minimum-income",PROPERTY_MINIMUM_INCOME_COLUMN_NUMBER);
-									map.put("minimum-credit-limit",PROPERTY_MINIMUM_CREDIT_LIMIT_COLUMN_NUMBER);
-									map.put("maximum-credit-limit",PROPERTY_MAXIMUM_CREDIT_LIMIT_COLUMN_NUMBER);
-									map.put("minimum-monthly-repayment",PROPERTY_MINIMUM_MONTHLY_REPAYMENT_COLUMN_NUMBER);
-
-									map.put("late-payment-fee",PROPERTY_LATE_PAYMENT_FEE_COLUMN_NUMBER);
-									map.put("additional-card-holder",PROPERTY_ADDITIONAL_CARD_HOLDER_COLUMN_NUMBER);
-									map.put("foreign-exchange-fees",PROPERTY_FOREIGN_EXCHANGE_FEES_COLUMN_NUMBER);
-									map.put("complimentary-travel-insurance",PROPERTY_COMPLIMENTARY_TRAVEL_INSURANCE_COLUMN_NUMBER);
-									map.put("extended-warranty",PROPERTY_EXTENDED_WARRANTY_COLUMN_NUMBER);
-									map.put("available-temporary-residents",PROPERTY_AVAILABLE_TEMPORARY_RESIDENTS_COLUMN_NUMBER);
-
-									map.put("rewards-desc",PROPERTY_REWARDS_DESC_COLUMN_NUMBER);
-									//map.put("rewards-standard-card-class",PROPERTY_REWARDS_CARD_CLASS_COLUMN_NUMBER);
-									map.put("rewards-standard-card-points",PROPERTY_REWARDS_STANDARD_POINTS_COLUMN_NUMBER);
-									map.put("rewards-amex-card-points",PROPERTY_REWARDS_AMEX_POINTS_COLUMN_NUMBER);
-									map.put("rewards-bonus-points",PROPERTY_BONUS_POINTS_COLUMN_NUMBER);
-									map.put("rewards-bonus-points-type",PROPERTY_BONUS_POINTS_TYPE);
-
-									map.put("terms-interest-rate",PROPERTY_INTEREST_RATE_TERMS_COLUMN_NUMBER);
-									map.put("terms-rewards",PROPERTY_REWARDS_TERMS_COLUMN_NUMBER);
-									map.put("terms-balance-transfer",PROPERTY_BALANCE_TRANSFER_TERMS_COLUMN_NUMBER);
-									map.put("terms-other-features",PROPERTY_OTHER_FEATURES_TERMS_COLUMN_NUMBER);
-									map.put("terms-general",PROPERTY_GENERAL_TERMS_COLUMN_NUMBER);
-									map.put("special-offer",PROPERTY_SPECIAL_OFFER_COLUMN_NUMBER);
-									map.put("other-features",PROPERTY_OTHER_FEATURES_COLUMN_NUMBER);
-									map.put("categories",PROPERTY_CATEGORIES_COLUMN_NUMBER);
-									map.put("handover-url",PROPERTY_HANDOVER_URL_COLUMN_NUMBER);
-
-									for (String key : map.keySet()){
-
-										int idx = map.get(key);
-
-										if (idx < part.length && !(part[idx].equals(""))) {
-
-											//see if the variable actually needs a currency format
-											String formatted = "";
-
-											switch(idx) {
-												case 8:
-												case 9:
-												case 10:
-												case 11:
-												case 12:
-												case 13:
-												case 14:
-												case 15:
-												case 16:
-												case 17:
-												case 18:
-												case 19:
-												case 20:
-												case 21:
-												case 36:
-												case 37:
-												case 38:
-												case 39:
-												case 40:
-												case 41:
-													if(part[idx].equals("N/A")) {
-														part[idx] = part[idx].replaceAll("N/A", "");
-														formatted = "NULL";
-													} else {
-														part[idx] = part[idx].replaceAll(",", "");
-
-														Pattern p = Pattern.compile("([\\d\\.,]+)");
-														Matcher m = p.matcher(part[idx]);
-														double j = 0.0;
-														if (m.find()) {
-															j = Double.valueOf(m.group(1));
-														}
-														formatted = String.valueOf(j);
-													}
-													break;
-												case 31:
-												case 32:
-												case 33:
-												case 34:
-												case 35:
-													if (part[idx].equals("Yes")) {
-														formatted = "1";
-													} else {
-														formatted = "0";
-													}
-													break;
-												default:
-													if(part[idx].equals("N/A")) {
-														formatted = "NULL";
-													} else {
-														formatted = "0";
-													}
-													part[idx] = part[idx].replaceAll("N/A", "");
-													break;
+												Pattern p = Pattern.compile("([\\d\\.,]+)");
+												Matcher m = p.matcher(part[idx]);
+												double j = 0.0;
+												if (m.find()) {
+													j = Double.valueOf(m.group(1));
+												}
+												formatted = String.valueOf(j);
 											}
-
-												switch(idx) {
-												case 42:
-													if (productId != -1) {
-														update.append("DELETE FROM ctm.category_product_mapping WHERE productId = @product_id;\r\n");
-													}
-													update.append("INSERT INTO ctm.category_product_mapping (categoryId, productId) SELECT categoryId, @product_id FROM category_master WHERE categoryCode IN ('"+part[idx].replaceAll(",", "','")+"');\r\n");
-
-													break;
-												case 24:
-												case 25:
-												case 26:
-												case 27:
-												case 28:
-												case 29:
-												case 30:
-													update.append("INSERT INTO ctm.product_properties_text VALUES(@product_id,'"+key+"',"+sequenceNo+",'"+part[idx]+"','"+effectiveDate+"','2040-12-31','');\r\n");
-													break;
-												default:
-													update.append("INSERT INTO ctm.product_properties VALUES(@product_id,'"+key+"',"+sequenceNo+","+formatted+",'"+part[idx]+"',NULL,'"+effectiveDate+"','2040-12-31','',0);\r\n");
-													break;
+											break;
+										case 31:
+										case 32:
+										case 33:
+										case 34:
+										case 35:
+											if (part[idx].equals("Yes")) {
+												formatted = "1";
+											} else {
+												formatted = "0";
 											}
-
-										}
+											break;
+										default:
+											if(part[idx].equals("N/A")) {
+												formatted = "NULL";
+											} else {
+												formatted = "0";
+											}
+											part[idx] = part[idx].replaceAll("N/A", "");
+											break;
 									}
+
+										switch(idx) {
+										case 42:
+											update.append("INSERT INTO ctm.category_product_mapping (categoryId, productId) SELECT categoryId, @product_id FROM category_master WHERE categoryCode IN ('"+part[idx].replaceAll(",", "','")+"');\r\n");
+
+											break;
+										case 24:
+										case 25:
+										case 26:
+										case 27:
+										case 28:
+										case 29:
+										case 30:
+											update.append("INSERT INTO ctm.product_properties_text VALUES(@product_id,'"+key+"',"+sequenceNo+",'"+part[idx]+"','"+effectiveDate+"','2040-12-31','');\r\n");
+											break;
+										default:
+											update.append("INSERT INTO ctm.product_properties VALUES(@product_id,'"+key+"',"+sequenceNo+","+formatted+",'"+part[idx]+"',NULL,'"+effectiveDate+"','2040-12-31','',0);\r\n");
+											break;
+									}
+
+								}
 							}
 						}
 
