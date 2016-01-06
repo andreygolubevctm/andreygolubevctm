dd3b0c13374b47bb2555c33e7bdd14f903a6df43
cancheta <carlos.ancheta@comparethemarket.com.au>
2016-01-06 14:18:48 +1000

    CTMIT-684: implementation of requestAt

--- src/main/java/com/ctm/web/energy/apply/model/request/EnergyApplyPostRequestPayload.java
+++ src/main/java/com/ctm/web/energy/apply/model/request/EnergyApplyPostRequestPayload.java
@@ -1,44 +1,11 @@
 package com.ctm.web.energy.apply.model.request;
 
-import com.ctm.web.core.model.formData.Request;
+import com.ctm.web.core.model.formData.RequestImpl;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class EnergyApplyPostRequestPayload implements Request {
-    private Long transactionId;
-    private String clientIpAddress;
-    private String environmentOverride;
+public class EnergyApplyPostRequestPayload extends RequestImpl {
     private EnergyApplicationPayload utilities;
-
-    @Override
-    public Long getTransactionId() {
-        return transactionId;
-    }
-
-    @Override
-    public void setTransactionId(Long transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    @Override
-    public String getClientIpAddress() {
-        return clientIpAddress;
-    }
-
-    @Override
-    public void setClientIpAddress(String clientIpAddress) {
-        this.clientIpAddress = clientIpAddress;
-    }
-
-    @Override
-    public String getEnvironmentOverride() {
-        return environmentOverride;
-    }
-
-    @Override
-    public void setEnvironmentOverride(String environmentOverride) {
-        this.environmentOverride = environmentOverride;
-    }
 
     public EnergyApplicationPayload getUtilities() {
         return utilities;
@@ -47,5 +14,16 @@ public class EnergyApplyPostRequestPayload implements Request {
     public void setUtilities(EnergyApplicationPayload utilities) {
         this.utilities = utilities;
     }
+
+    public String toString() {
+        return "EnergyApplyPostRequestPayload{" +
+                "transactionId=" + getTransactionId() +
+                ", clientIpAddress='" + getClientIpAddress() + '\'' +
+                ", utilities=" + utilities +
+                ", environmentOverride='" + getEnvironmentOverride() + '\'' +
+                ", requestAt='" +getRequestAt() + '\'' +
+                '}';
+    }
+
 
 }
