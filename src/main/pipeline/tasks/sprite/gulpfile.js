e58d3c01b2a280551575f2ca25e41df29cb070c9
gdebever <guillaume.debever@comparethemarket.com.au>
2016-01-07 12:35:57 +1000

    HLT-2741: Fixing generation of health sprite + adding retina support for all logos.

--- src/main/pipeline/tasks/sprite/gulpfile.js
+++ src/main/pipeline/tasks/sprite/gulpfile.js
@@ -28,11 +28,15 @@ function SpriteTasks(gulp) {
 
                 gulp.task(taskName, function() {
                     var spriteSmithConfig = {
+                        cssName: "logosSprites.less",
                         imgName: "spritesheet.png",
                         imgPath: imgPathPrefix + "/spritesheet.png",
-                        cssName: "logosSprites.less",
-                        padding: 2,
                         cssSpritesheetName: bundle + "-logo",
+                        retinaImgName: "spritesheet@2x.png",
+                        retinaImgPath: imgPathPrefix + "/spritesheet@2x.png",
+                        cssRetinaSpritesheetName: bundle + "-logo-2x",
+                        retinaSrcFilter: [path.join(srcPath, "*@2x.png")],
+                        padding: 2,
                         cssVarMap: function(sprite) {
                             sprite.name = bundle + "-logo-" + sprite.name;
                         },
@@ -41,14 +45,6 @@ function SpriteTasks(gulp) {
                             variableNameTransforms: []
                         }
                     };
-
-                    // Not sure why, but health shouldn't have retina images
-                    if(bundle !== "health") {
-                        spriteSmithConfig.retinaSrcFilter = [path.join(srcPath, "*@2x.png")];
-                        spriteSmithConfig.retinaImgName = "spritesheet@2x.png";
-                        spriteSmithConfig.retinaImgPath = imgPathPrefix + "/spritesheet@2x.png";
-                        spriteSmithConfig.cssRetinaSpritesheetName = bundle + "-logo-2x";
-                    }
 
                     var spriteData = gulp.src(path.join(srcPath, "*.png"))
                         .pipe(spritesmith(spriteSmithConfig));
