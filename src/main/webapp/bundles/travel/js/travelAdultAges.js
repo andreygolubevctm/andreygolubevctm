ded727be12767014cac47d76c16433596351c45f
David Degnan <david.degnan@comparethemarket.com.au>
2016-01-22 10:37:28 +1000

    TRV-1110 CR feedback

--- src/main/webapp/bundles/travel/js/travelAdultAges.js
+++ src/main/webapp/bundles/travel/js/travelAdultAges.js
@@ -7,6 +7,7 @@
 	var meerkat = window.meerkat,
 		$travel_party,
 		$travel_adults,
+		$adult_dob_2_row,
 		$adult_dob_2,
 		$children_row;
 
@@ -14,9 +15,10 @@
 		$(document).ready(function travelSortingInitDomready() {
 			$travel_party = $('.travel_party'),
 			$travel_adults = $('#travel_adults'),
-			$adult_dob_2 = $('.second_traveller_age_row');
+			$adult_dob_2_row = $('.second_traveller_age_row'),
+			$adult_dob_2 = $('#travel_travellers_traveller2DOB');
 
-			$adult_dob_2.hide();
+			$adult_dob_2_row.hide();
 			$children_row = $('.children_row');
 			$children_row.hide();
 
@@ -29,21 +31,19 @@
 		$travel_party.off().on("change", function changeAdultCount() {
 			var selected = $(this).find("input[type='radio']:checked").val();
 			if (selected === "S") {
-				$adult_dob_2[hideMethod]();
+				$adult_dob_2_row[hideMethod]();
 				$children_row[hideMethod]();
 				$travel_adults.val(1);
 			} else if (selected === "C") {
-				$adult_dob_2[showMethod]();
+				$adult_dob_2_row[showMethod]();
 				$children_row[hideMethod]();
 				$travel_adults.val(2);
-				$('#travel_travellers_traveller2DOB').addClass('validate');
-				$('#travel_travellers_traveller2DOB').attr('required','required');
+				$adult_dob_2.addClass('validate').attr('required','required');
 			} else if (selected === "F") {
-				$adult_dob_2[showMethod]();
+				$adult_dob_2_row[showMethod]();
 				$children_row[showMethod]();
 				$travel_adults.val(2);
-				$('#travel_travellers_traveller2DOB').removeClass('validate');
-				$('#travel_travellers_traveller2DOB').removeAttr('required');
+				$adult_dob_2.removeClass('validate').removeAttr('required');
 			}
 		});
 	}
