2f2e3cd84e22832ee44cf772072d8d828cda7557
Adrian Diente <adrian.diente@comparethemarket.com.au>
2016-01-25 09:53:15 +1000

    UTL-331: removed moving property snapshot content

--- database-deployment-scripts/toBeScheduled/UTL-331-snapshot-content.sql
+++ database-deployment-scripts/toBeScheduled/UTL-331-snapshot-content.sql
@@ -7,7 +7,7 @@ SET @CCID = (SELECT LAST_INSERT_ID());
 
 -- SELECT count(*) AS total FROM `ctm`.`content_supplementary` WHERE contentControlId = @CCID;
 -- RESULT BEFORE: 0
--- RESULT AFTER: 6
+-- RESULT AFTER: 4
 
 INSERT INTO `ctm`.`content_supplementary` (`contentControlId`, `supplementaryKey`, `supplementaryValue`) VALUES (@CCID, 'freeServiceTitle', 'Free service');
 INSERT INTO `ctm`.`content_supplementary` (`contentControlId`, `supplementaryKey`, `supplementaryValue`) VALUES (@CCID, 'freeServiceContent', 'Our service is 100% free and we don\'t markup plans with any additional fees or charges');
@@ -15,15 +15,12 @@ INSERT INTO `ctm`.`content_supplementary` (`contentControlId`, `supplementaryKey
 INSERT INTO `ctm`.`content_supplementary` (`contentControlId`, `supplementaryKey`, `supplementaryValue`) VALUES (@CCID, 'switchingProvidersTitle', 'Switching providers');
 INSERT INTO `ctm`.`content_supplementary` (`contentControlId`, `supplementaryKey`, `supplementaryValue`) VALUES (@CCID, 'switchingProvidersContent', 'If you stay in the same property and switch providers, your new provider will handle everything for you, even cancellation of your contract with your current provider (based on the State you live in)');
 
-INSERT INTO `ctm`.`content_supplementary` (`contentControlId`, `supplementaryKey`, `supplementaryValue`) VALUES (@CCID, 'movingPropertyTitle', 'Moving property?');
-INSERT INTO `ctm`.`content_supplementary` (`contentControlId`, `supplementaryKey`, `supplementaryValue`) VALUES (@CCID, 'movingPropertyContent', 'If you are moving property and need energy connected urgently, call us to prioritise your application');
-
 
 -- ========================= ROLLBACK
 -- SET @CCID = (SELECT `contentControlId` FROM `ctm`.`content_control` WHERE contentKey = 'snapshot');
 
 -- SELECT count(*) AS total FROM `ctm`.`content_supplementary` WHERE contentControlId = @CCID;
--- RESULT BEFORE: 6
+-- RESULT BEFORE: 4
 -- RESULT AFTER: 0
 -- DELETE FROM `ctm`.`content_supplementary` WHERE `contentControlId` = @CCID LIMIT 6;
 
