904a82b7f644bec0fd72886d57c9969545c56f5f
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2016-02-05 13:05:29 +1000

    CTMIT-782 Modified HNC rankings to put non-quoting providers at bottom of list (same as car)

--- src/main/webapp/bundles/home/js/homeResults.js
+++ src/main/webapp/bundles/home/js/homeResults.js
@@ -68,7 +68,8 @@
 				paths: {
 					price: price,
 					availability: {
-						product: productAvailable
+						product: productAvailable,
+						price: price
 					},
 					productId: "productId",
 					productBrandCode: "brandCode",
@@ -195,7 +196,7 @@
 				rankings: {
 					paths: {
 						rank_productId: "productId",
-						rank_premium: price.annual
+						rank_premium: "price.annualPremium"
 					},
 					filterUnavailableProducts: false
 				},
