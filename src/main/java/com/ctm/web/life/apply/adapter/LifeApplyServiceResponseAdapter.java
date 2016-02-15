85fc6a83cb4de647df7251234e27a86ee1119b70
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2016-02-15 11:17:19 +1000

    BER-106: fix for null pointer

--- src/main/java/com/ctm/web/life/apply/adapter/LifeApplyServiceResponseAdapter.java
+++ src/main/java/com/ctm/web/life/apply/adapter/LifeApplyServiceResponseAdapter.java
@@ -7,28 +7,32 @@ import com.ctm.web.life.apply.response.SelectDetails;
 import com.ctm.web.life.apply.response.Selection;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class LifeApplyServiceResponseAdapter implements WebResponseAdapter<LifeApplyWebResponseResults.Builder, LifeApplyResponse> {
 
     @Override
     public LifeApplyWebResponseResults.Builder adapt(LifeApplyResponse applyResponse) {
         LifeApplyWebResponseResults.Builder builder= new LifeApplyWebResponseResults.Builder();
-        Map<String, String> additionalInformation = applyResponse.getAdditionalInformation();
-        if(!additionalInformation.isEmpty()) {
-            SelectDetails client = new SelectDetails.Builder()
-                    .info_url(applyResponse.getAdditionalInformation().get(LifeApplyResponse.PRIMARY_LIFEBROKER_INFO_URL))
-                    .pds(applyResponse.getAdditionalInformation().get(LifeApplyResponse.PRIMARY_LIFEBROKER_PDS))
-                    .build();
-            SelectDetails partner = new SelectDetails.Builder()
-                    .info_url(applyResponse.getAdditionalInformation().get(LifeApplyResponse.PARTNER_LIFEBROKER_INFO_URL))
-                    .pds(applyResponse.getAdditionalInformation().get(LifeApplyResponse.PARTNER_LIFEBROKER_PDS))
-                    .build();
-            Selection selection = new Selection.Builder()
-                    .client(client)
-                    .partner(partner)
-                    .build();
-            builder.selection(selection);
-        }
+        Selection.Builder selectionBuilder = new Selection.Builder();
+        Optional<Map<String, String>> additionalInformationMaybe = applyResponse.getAdditionalInformation();
+        additionalInformationMaybe.ifPresent(additionalInformation -> {
+            if(!additionalInformation.containsKey(LifeApplyResponse.PRIMARY_LIFEBROKER_INFO_URL)) {
+                SelectDetails client = new SelectDetails.Builder()
+                        .info_url(additionalInformation.get(LifeApplyResponse.PRIMARY_LIFEBROKER_INFO_URL))
+                        .pds(additionalInformation.get(LifeApplyResponse.PRIMARY_LIFEBROKER_PDS))
+                        .build();
+                selectionBuilder.client(client);
+            }
+            if(!additionalInformation.containsKey(LifeApplyResponse.PARTNER_LIFEBROKER_INFO_URL)) {
+                SelectDetails partner = new SelectDetails.Builder()
+                        .info_url(additionalInformation.get(LifeApplyResponse.PARTNER_LIFEBROKER_INFO_URL))
+                        .pds(additionalInformation.get(LifeApplyResponse.PARTNER_LIFEBROKER_PDS))
+                        .build();
+                selectionBuilder.partner(partner);
+            }
+            builder.selection(selectionBuilder.build());
+        });
         builder.success(com.ctm.interfaces.common.types.Status.REGISTERED.equals(applyResponse.getResponseStatus()));
         return builder;
     }
