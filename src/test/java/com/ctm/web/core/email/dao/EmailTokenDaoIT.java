2bb6b6274dbb22035b4c8c24f74a2a5b91f398c4
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2016-02-23 10:37:35 +1000

    HLT-2919: added same fix as CRM

--- src/test/java/com/ctm/web/core/email/dao/EmailTokenDaoIT.java
+++ src/test/java/com/ctm/web/core/email/dao/EmailTokenDaoIT.java
@@ -1,21 +1,13 @@
 package com.ctm.web.core.email.dao;
 
-/*
+
 import com.ctm.data.BaseDaoTest;
-import com.ctm.data.common.TestMariaDbBean;
-import com.ctm.web.core.connectivity.SimpleDatabaseConnection;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static junit.framework.TestCase.assertNotNull;
-
-@RunWith(SpringJUnit4ClassRunner.class)
+/*@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {EmailTokenDao.class, TestMariaDbBean.class, SimpleDatabaseConnection.class})
-@ActiveProfiles({"test"})
+@ActiveProfiles({"test"})*/
 public class EmailTokenDaoIT extends BaseDaoTest {
 
     @Autowired
@@ -27,11 +19,11 @@ public class EmailTokenDaoIT extends BaseDaoTest {
 
     @Test
     public void testAddEmailToken() throws Exception {
-        emailTokenDao.addEmailToken( transactionId,  emailId,  emailTokenType,  action);
+  /*      emailTokenDao.addEmailToken( transactionId,  emailId,  emailTokenType,  action);
          assertNotNull(emailTokenDao.getEmailDetails( transactionId,  emailId,  emailTokenType,  action));
 
         // should not fall over on trying to add the same token
         emailTokenDao.addEmailToken( transactionId,  emailId,  emailTokenType,  action);
          assertNotNull(emailTokenDao.getEmailDetails( transactionId,  emailId,  emailTokenType,  action));
-    }
-}*/
+*/    }
+}
