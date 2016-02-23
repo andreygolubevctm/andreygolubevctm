82026c855d5009ac54ca1013221f0f069b0d03af
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2016-02-23 13:28:47 +1000

    HLT-2919: wooo

--- src/main/java/com/ctm/web/core/email/services/token/EmailTokenServiceFactory.java
+++ src/main/java/com/ctm/web/core/email/services/token/EmailTokenServiceFactory.java
@@ -1,6 +1,5 @@
 package com.ctm.web.core.email.services.token;
 
-import com.ctm.web.core.connectivity.SimpleDatabaseConnection;
 import com.ctm.web.core.dao.EmailMasterDao;
 import com.ctm.web.core.email.dao.EmailTokenDao;
 import com.ctm.web.core.exceptions.ConfigSettingException;
@@ -20,7 +19,7 @@ public class EmailTokenServiceFactory {
 
     public static EmailTokenService getEmailTokenServiceInstance(PageSettings pageSettings) throws ConfigSettingException {
         String tokenEncryptionKey = pageSettings.getSetting(EMAIL_TOKEN_ENCRYPTION_KEY);
-        EmailTokenDao emailTokenDao = new EmailTokenDao(SimpleDatabaseConnection.getInstance());
+        EmailTokenDao emailTokenDao = new EmailTokenDao();
         EmailMasterDao emailMasterDao = new EmailMasterDao();
 
         return new EmailTokenService(tokenEncryptionKey, emailTokenDao, emailMasterDao);
