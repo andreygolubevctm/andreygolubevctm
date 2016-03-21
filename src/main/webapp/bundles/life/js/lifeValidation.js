67ce082abd089da0fbed700859750f91b361614a
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2016-03-21 12:45:51 +1000

    LIF-379: cleanup commented code

--- src/main/webapp/bundles/life/js/lifeValidation.js
+++ src/main/webapp/bundles/life/js/lifeValidation.js
@@ -1,15 +1,5 @@
 ;(function ($) {
 
-    //$.validator.addMethod('validateOkToEmailRadio', function(value, element, param) {
-    //    var $optin = $("#quote_contactFieldSet input[name='quote_contact_marketing']:checked");
-    //    var noOptin = $optin.length === 0;
-    //    var email = $('#quote_contact_email').val();
-    //    if(!_.isEmpty(email) && noOptin) {
-    //        return false;
-    //    }
-    //    return true;
-    //}, "Please choose if OK to email");
-
     $.validator.addMethod('validateInsuranceAmount', function(value, element, param) {
         var $this = $(element);
         $this.val($this.val().replace(/[^0-9]/g, ''));
