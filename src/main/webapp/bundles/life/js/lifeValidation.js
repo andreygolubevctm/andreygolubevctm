340351768df00db96e7109e633d7098b7bc2242a
Christopher Dingli <christopher.dingli@comparethemarket.com.au>
2016-03-21 11:08:12 +1000

    LIF-379: Add dummy validation file

---
+++ src/main/webapp/bundles/life/js/lifeValidation.js
@@ -0,0 +1,13 @@
+;(function ($) {
+
+    //$.validator.addMethod('validateOkToEmailRadio', function(value, element, param) {
+    //    var $optin = $("#quote_contactFieldSet input[name='quote_contact_marketing']:checked");
+    //    var noOptin = $optin.length === 0;
+    //    var email = $('#quote_contact_email').val();
+    //    if(!_.isEmpty(email) && noOptin) {
+    //        return false;
+    //    }
+    //    return true;
+    //}, "Please choose if OK to email");
+
+})(jQuery);
