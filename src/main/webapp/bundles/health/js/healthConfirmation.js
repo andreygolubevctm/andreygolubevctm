50fdc191a1e9d3e3e2ca50f12791fc1232494bb3
David Degnan <david.degnan@comparethemarket.com.au>
2016-03-24 12:06:10 +1000

    HLT-2859 force rebuild

--- src/main/webapp/bundles/health/js/healthConfirmation.js
+++ src/main/webapp/bundles/health/js/healthConfirmation.js
@@ -129,6 +129,7 @@
 		// if pending, it might not have the about fund info so let's get it
 		if(confirmationProduct.about === ''  || !confirmationProduct.hasOwnProperty('warningAlert') || confirmationProduct.warningAlert === '') {
 			meerkat.modules.healthMoreInfo.retrieveExternalCopy(confirmationProduct).then(function confirmationExternalCopySuccess() {
+				
 				$(".aboutFund").html(confirmationProduct.aboutFund).parents(".displayNone").first().removeClass("displayNone");
 
 				if (confirmationProduct.hasOwnProperty('warningAlert') && confirmationProduct.warningAlert !== '') {
