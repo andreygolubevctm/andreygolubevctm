bae95c7240ceadaa07d82bbb9de78a68e596416f
sjiang <steve.jiang@comparethemarket.com.au>
2016-03-24 16:33:44 +1000

    HLT-2789: fix medicare (cherry picked from commit 3675be3)

--- src/main/webapp/bundles/health/js/health.js
+++ src/main/webapp/bundles/health/js/health.js
@@ -668,7 +668,7 @@
 					if($surnameField.val() === '') $surnameField.val($("#health_application_primary_surname").val());
 
 					var product = meerkat.modules.healthResults.getSelectedProduct();
-					var mustShowList = ["GMHBA","Frank","Budget Direct","Bupa","HIF","QCHF"];
+                    var mustShowList = ["GMHBA","Frank","Budget Direct","Bupa","HIF","QCHF","Navy Health"];
 
 					var $rebateEl = $('input[name=health_healthCover_rebate]:checked');
 					if( (!_.isEmpty($rebateEl) && $rebateEl.val() == "N") && $.inArray(product.info.providerName, mustShowList) == -1) {
