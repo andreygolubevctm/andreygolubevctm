3a987f3045161663b26931f9b45ff706b59496b7
Ben Thompson <ben.thompson@comparethemarket.com.au>
2016-04-12 09:31:37 +1000

    HLT-3113 WIP....

--- src/main/pipeline/helpers/bundlesHelper.js
+++ src/main/pipeline/helpers/bundlesHelper.js
@@ -170,14 +170,23 @@ Bundles.prototype.getDependencies = function (bundle) {
     var rootDependencies = [],
         returnDependencies = [];
     var originalBundleName = bundle;
-    if (typeof this.collection[bundle] !== "undefined" && typeof this.collection[bundle].extends !== "undefined")
+    if (typeof this.collection[bundle] !== "undefined" && typeof this.collection[bundle].extends !== "undefined") {
+        // Add the dependencies of the original bundle:
+        if (typeof this.collection[bundle] !== "undefined" && typeof this.collection[bundle].dependencies !== "undefined")
+            rootDependencies = rootDependencies.concat(this.collection[bundle].dependencies);
+        else
+            rootDependencies = rootDependencies.concat(getBundleDependenciesList(bundle));
+        console.log(bundle, rootDependencies);
+        // Now reset it to the bundle its extending
         bundle = this.collection[bundle].extends;
 
-    if (typeof this.collection[bundle] !== "undefined" && typeof this.collection[bundle].dependencies !== "undefined")
-        rootDependencies = this.collection[bundle].dependencies;
-    else
-        rootDependencies = getBundleDependenciesList(bundle);
+    }
 
+    if (typeof this.collection[bundle] !== "undefined" && typeof this.collection[bundle].dependencies !== "undefined")
+        rootDependencies = rootDependencies.concat(this.collection[bundle].dependencies);
+    else
+        rootDependencies = rootDependencies.concat(getBundleDependenciesList(bundle));
+    console.log(bundle, rootDependencies);
     for (var i = 0; i < rootDependencies.length; i++) {
         var dependency = rootDependencies[i],
             dependencies = this.getDependencies(dependency);
