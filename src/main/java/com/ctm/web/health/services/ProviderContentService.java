ddd275be13eef4c7ef2c0f2235524646171e7bc0
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2016-05-12 13:24:30 +1000

    HLT-2852: added logging

--- src/main/java/com/ctm/web/health/services/ProviderContentService.java
+++ src/main/java/com/ctm/web/health/services/ProviderContentService.java
@@ -3,13 +3,23 @@ package com.ctm.web.health.services;
 import com.ctm.web.core.dao.ProviderDao;
 import com.ctm.web.core.exceptions.ConfigSettingException;
 import com.ctm.web.core.exceptions.DaoException;
+import com.ctm.web.core.exceptions.ServiceException;
 import com.ctm.web.core.services.ApplicationService;
+import com.ctm.web.core.web.go.Data;
 import com.ctm.web.health.dao.ProviderContentDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.NumberUtils;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
+import static com.ctm.commonlogging.common.LoggingArguments.kv;
+
 public class ProviderContentService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Data.class);
 
     private final ProviderContentDao providerContentDao = new ProviderContentDao();
 
@@ -31,11 +41,22 @@ public class ProviderContentService {
         return providerContentDao.getProviderContentText(providerId, providerContentTypeCode, "HEALTH", currDate);
     }
 
-    public String getProviderContentText(HttpServletRequest request) throws DaoException, ConfigSettingException {
-        int providerId = Integer.parseInt(request.getParameter("providerId"));
+    public String getProviderContentText(HttpServletRequest request) throws ServiceException {
+        String providerIdString = request.getParameter("providerId");
         String providerContentTypeCode = request.getParameter("providerContentTypeCode");
+        Integer providerId;
+        try {
+            providerId = NumberUtils.parseNumber(providerIdString, Integer.class);
+         } catch (NumberFormatException e) {
+            LOGGER.info("Failed to convert providerId to number. {}", kv("providerIdString", providerIdString), kv("providerContentTypeCode", providerContentTypeCode), e);
+            throw new ServiceException("Failed to convert providerId to number.", e);
+        }
         Date currDate = ApplicationService.getApplicationDate(request);
-        return providerContentDao.getProviderContentText(providerId, providerContentTypeCode, "HEALTH", currDate);
+        try {
+            return providerContentDao.getProviderContentText(providerId, providerContentTypeCode, "HEALTH", currDate);
+        } catch (DaoException e) {
+            throw new ServiceException("Failed to get provider content text.", e);
+        }
     }
 
 }
