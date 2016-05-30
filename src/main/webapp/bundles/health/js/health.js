9d0e9ef6643103a207ee396c659eb28fe7823f9a
David Degnan <david.degnan@comparethemarket.com.au>
2016-05-31 08:44:48 +1000

    HLT-3310 Shift to run after checkbox is checked

--- src/main/webapp/bundles/health/js/health.js
+++ src/main/webapp/bundles/health/js/health.js
@@ -312,8 +312,28 @@
 					_.defer(function() {
 						meerkat.modules.healthBenefits.open('journey-mode');
 
-						if(event.isForward)
+						if(event.isForward) {
 							$('input[name="health_situation_accidentOnlyCover"]').prop('checked', ($('#health_situation_healthSitu').val() === 'ATP'));
+
+							if (meerkat.site.isCallCentreUser === true){
+
+								var accidentOnly = $('input[name="health_situation_accidentOnlyCover"]');
+								var dialog44 = $(".simples-dialogue-44");
+								dialog44.hide();
+								if($(accidentOnly).is(':checked')) {
+									dialog44.show();
+								}
+
+								accidentOnly.on('change', function() {
+									if($(this).is(':checked')) {
+										dialog44.show();
+									} else {
+										dialog44.hide();
+									}
+								});
+							}
+
+						}
 					});
 				}
 
@@ -324,21 +344,6 @@
 
 				if (event.isForward && meerkat.site.isCallCentreUser === true){
 					meerkat.modules.simplesCallInfo.fetchCallInfo();
-
-					var accidentOnly = $('input[name="health_situation_accidentOnlyCover"]');
-					var dialog44 = $(".simples-dialogue-44");
-					dialog44.hide();
-					if($(accidentOnly).is(':checked')) {
-						dialog44.show();
-					}
-
-					accidentOnly.on('change', function() {
-						if($(this).is(':checked')) {
-							dialog44.show();
-						} else {
-							dialog44.hide();
-						}
-					});
 				}
 			},
 			onAfterLeave:function(event){
