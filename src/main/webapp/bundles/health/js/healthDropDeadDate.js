fa0367a210e9ec79431f2c6fd09e5152fb04cfeb
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2016-06-07 10:25:48 +1000

    HLT-3162: added number check

--- src/main/webapp/bundles/health/js/healthDropDeadDate.js
+++ src/main/webapp/bundles/health/js/healthDropDeadDate.js
@@ -16,10 +16,37 @@
         return typeof obj.dropDeadDate === 'string' ? new Date(obj.dropDeadDate) : obj.dropDeadDate;
     }
 
+    function setDropDeadDate(providerContentText, product) {
+        var formattedDate = '';
+
+        if ($.trim(providerContentText) !== '') {
+            var dateSplit = providerContentText.split("/");
+            var rearrangedDate = dateSplit[1]+"/"+dateSplit[0]+"/"+dateSplit[2];
+            var newDate = new Date(rearrangedDate);
+            if ( isNaN( newDate.getTime() ) ) {
+                setDefaultDropDeadDate(product);
+            } else {
+                formattedDate = meerkat.modules.dateUtils.format(newDate, "Do of MMMM, YYYY");
+                product.dropDeadDateFormatted =  formattedDate;
+                product.dropDeadDate =  new Date(rearrangedDate);
+            }
+        } else {
+            setDefaultDropDeadDate(product);
+        }
+    }
+
+    function setDefaultDropDeadDate(product) {
+        var d = new Date();
+        product.dropDeadDateFormatted = '31st March '+d.getFullYear();
+        product.dropDeadDate =  new Date('31/3/'+d.getFullYear());
+    }
+
+
     meerkat.modules.register("healthDropDeadDate", {
         init : init,
         getDropDatePassed: getDropDatePassed,
-        getDropDeadDate : getDropDeadDate
+        getDropDeadDate : getDropDeadDate,
+        setDropDeadDate : setDropDeadDate
     });
 
 })(jQuery);
