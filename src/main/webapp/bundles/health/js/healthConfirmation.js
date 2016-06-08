d74d5d8bf81a479215a7584368c188e47894c799
Adrian Diente <adrian.diente@comparethemarket.com.au>
2016-06-08 21:45:44 +1000

    HLT-3322: remove all unnecessary JS

--- src/main/webapp/bundles/health/js/healthConfirmation.js
+++ src/main/webapp/bundles/health/js/healthConfirmation.js
@@ -39,7 +39,6 @@
 
 				// prepare data
 				confirmationProduct = $.extend({},result.data);
-				confirmationProduct.mode = "lhcInc";
 
 				// if confirmation has been loaded from the confirmations table in the db, confirmationProduct.product should exist
 				if( confirmationProduct.product ){
@@ -77,37 +76,7 @@
 					confirmationProduct.pending = true;
 				}
 
-				// prepare hospital and extras covers inclusions, exclusions and restrictions
-				meerkat.modules.moreInfo.setProduct(confirmationProduct);
-
-				//Now prepare cover.
-				meerkat.modules.healthMoreInfo.prepareCover();
-
-				// prepare necessary frequency values
-				if( confirmationProduct.frequency.length == 1) { // if found frequency is a letter code, translate it to full word
-					confirmationProduct.frequency = meerkat.modules.healthResults.getFrequencyInWords(confirmationProduct.frequency);
-				}
-				confirmationProduct._selectedFrequency = confirmationProduct.frequency;
-				meerkat.modules.healthPaymentStep.initFields(); // not sure why this works to allow the next call to work but it seems to be the only way to figure out what payment type they selected
-
-				if(!confirmationProduct.hasOwnProperty('premium')) {
-                    if (confirmationProduct.paymentType) {
-                        var paymentType;
-                        switch (confirmationProduct.paymentType) {
-                            case 'cc':
-                                paymentType = 'CreditCard';
-                                break;
-                            case 'ba':
-                                paymentType = 'BankAccount';
-                                break;
-
-                        }
-                        confirmationProduct.premium = confirmationProduct.paymentTypePremiums[paymentType];
-                    }
-				}
-
 				fillTemplate();
-				meerkat.modules.healthPriceComponent.initHealthPriceComponent();
 
 				/// TODO: Fix this -why is it needed though?
 				//meerkat.modules.healthMoreInfo.applyEventListeners();
@@ -138,38 +107,6 @@
 		var htmlTemplate = _.template(confirmationTemplate);
 		var htmlString = htmlTemplate(confirmationProduct);
 		$("#confirmation").html(htmlString);
-
-		meerkat.messaging.subscribe(meerkatEvents.healthPriceComponent.INIT, function(selectedProduct){
-			// inject the price and product summary
-			meerkat.modules.healthPriceComponent.updateProductSummaryHeader(confirmationProduct, confirmationProduct.frequency, true);
-			meerkat.modules.healthPriceComponent.updateProductSummaryDetails(confirmationProduct, confirmationProduct.startDate, false);
-		});
-
-		// if pending, it might not have the about fund info so let's get it
-		if(confirmationProduct.about === ''  || !confirmationProduct.hasOwnProperty('warningAlert') || confirmationProduct.warningAlert === '') {
-			meerkat.modules.healthMoreInfo.retrieveExternalCopy(confirmationProduct).then(function confirmationExternalCopySuccess() {
-				$(".aboutFund").html(confirmationProduct.aboutFund).parents(".displayNone").first().removeClass("displayNone");
-
-				if (confirmationProduct.hasOwnProperty('warningAlert') && confirmationProduct.warningAlert !== '') {
-					$("#health_confirmation-warning").find(".fundWarning").show().html(confirmationProduct.warningAlert);
-				} else {
-					$("#health_confirmation-warning").find(".fundWarning").hide().empty();
-				}
-
-				_.defer(function(){
-					// Backup in case warning contains html but no text
-					if(_.isEmpty($.trim($("#health_confirmation-warning").text()))) {
-						$("#health_confirmation-warning").find(".fundWarning").empty().hide();
-					}
-				});
-			});
-		}
-
-		if (typeof meerkat.site.healthAlternatePricingActive !== 'undefined' && meerkat.site.healthAlternatePricingActive === true) {
-			// render dual pricing
-			meerkat.modules.healthDualPricing.initHealthDualPricing();
-			meerkat.modules.healthDualPricing.renderTemplate('.policySummary.dualPricing', meerkat.modules.moreInfo.getProduct(), false, true);
-		}
 
 		// hide the sidebar frequncy. only needed for payment page
 		$('.hasDualPricing .sidebarFrequency').hide();
