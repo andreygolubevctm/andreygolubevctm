5b25311e25de3d5cf6e268e89bdca6fa71378fe6
Louisa Buchanan <louisa.buchanan@comparethemarket.com.au>
2016-06-16 10:44:40 +1000

    BER-106: code clean up

--- src/test/java/com/ctm/web/factory/EmailServiceFactoryTest.java
+++ src/test/java/com/ctm/web/factory/EmailServiceFactoryTest.java
@@ -5,6 +5,8 @@ import com.ctm.web.core.email.model.EmailMode;
 import com.ctm.web.core.email.services.EmailServiceHandler;
 import com.ctm.web.core.model.settings.PageSettings;
 import com.ctm.web.core.security.IPAddressHandler;
+import com.ctm.web.core.services.ApplicationService;
+import com.ctm.web.core.transaction.dao.TransactionDetailsDao;
 import com.ctm.web.core.web.go.Data;
 import com.ctm.web.health.email.services.HealthEmailService;
 import com.ctm.web.life.dao.OccupationsDao;
@@ -21,13 +23,17 @@ public class EmailServiceFactoryTest {
 	private IPAddressHandler ipHandler;
 	@Mock
 	private OccupationsDao occupationsDao;
+    @Mock
+    private TransactionDetailsDao transactionDetailsDao;
+    @Mock
+    private ApplicationService applicationService;
 
-	private EmailServiceFactory emailServiceFactory;
+    private EmailServiceFactory emailServiceFactory;
 
-	@Before
+    @Before
     public void setUp() throws Exception {
         initMocks(this);
-		 emailServiceFactory = new EmailServiceFactory(occupationsDao, ipHandler);
+		 emailServiceFactory = new EmailServiceFactory(occupationsDao, ipHandler,transactionDetailsDao,applicationService);
     }
 
     @Test
