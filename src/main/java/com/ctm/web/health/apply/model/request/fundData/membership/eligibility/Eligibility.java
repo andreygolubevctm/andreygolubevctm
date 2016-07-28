b36f3f02bf56cbf0fc9340937ef3254f29f844e7
cancheta <carlos.ancheta@comparethemarket.com.au>
2016-07-28 16:58:28 +1000

    HLT-3510: Changes to eligibility

--- src/main/java/com/ctm/web/health/apply/model/request/fundData/membership/eligibility/Eligibility.java
+++ src/main/java/com/ctm/web/health/apply/model/request/fundData/membership/eligibility/Eligibility.java
@@ -1,14 +1,10 @@
 
 package com.ctm.web.health.apply.model.request.fundData.membership.eligibility;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Eligibility {
 
-    @JsonProperty("EligibilityReasonID")
     private EligibilityReasonID eligibilityReasonID;
 
-    @JsonProperty("EligibilitySubReasonID")
     private EligibilitySubReasonID eligibilitySubReasonID;
 
     public Eligibility(final EligibilityReasonID eligibilityReasonID,
