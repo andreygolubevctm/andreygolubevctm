e6490da139ca2e562545cfda4f0925ac479f13e1
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2016-09-22 15:28:34 +1000

    CAR-1298 Modified to make motorweb the default journey and also hardcode the error messaging.

--- src/main/webapp/bundles/car/js/car.js
+++ src/main/webapp/bundles/car/js/car.js
@@ -157,9 +157,7 @@
                         $emailQuoteBtn.removeClass("privacyOptinChecked");
                     }
                 });
-                if(meerkat.modules.splitTest.isActive(4)) {
-                    meerkat.modules.carRegoLookup.lookup();
-                }
+                meerkat.modules.carRegoLookup.lookup();
             },
             validation: {
                 validate: true,
@@ -309,14 +307,11 @@
                 navigationId: steps.resultsStep.navigationId
             }
         ];
-        // Split Test for Rego Lookup Combines the first two steps in the progress bar.
-        if(meerkat.modules.splitTest.isActive(4)
-        || meerkat.modules.splitTest.isActive(41)) {
-            // Make car details match your car.
-            progressBarConfig[0].matchAdditionalSteps = [steps.optionsStep.navigationId];
-            //Remove the car details step.
-            progressBarConfig.splice(1,1);
-        }
+        // Combines the first two steps in the progress bar.
+        // Make car details match your car.
+        progressBarConfig[0].matchAdditionalSteps = [steps.optionsStep.navigationId];
+        //Remove the car details step.
+        progressBarConfig.splice(1,1);
         meerkat.modules.journeyProgressBar.configure(progressBarConfig);
     }
 
