0b532e5977717b1a30842bd652df1e5090361999
Leto Kauler <leto.kauler@comparethemarket.com.au>
2016-10-18 16:46:31 +1000

    HLT-3626: Fixes for call me back endpoint

--- src/main/java/com/ctm/web/health/router/HealthCallBackController.java
+++ src/main/java/com/ctm/web/health/router/HealthCallBackController.java
@@ -1,23 +1,27 @@
 package com.ctm.web.health.router;
 
 import com.ctm.web.core.exceptions.RouterException;
+import com.ctm.web.core.resultsData.model.ErrorInfo;
 import com.ctm.web.core.router.CommonQuoteRouter;
 import com.ctm.web.core.security.IPAddressHandler;
 import com.ctm.web.core.services.SessionDataServiceBean;
+import com.ctm.web.core.validation.SchemaValidationError;
 import com.ctm.web.core.web.go.Data;
 import com.ctm.web.health.callback.model.HealthCallBackData;
 import com.ctm.web.health.services.HealthCallBackService;
 import io.swagger.annotations.Api;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.Collections;
 
 import static com.ctm.web.core.leadService.model.LeadType.CALL_ME_BACK;
 import static com.ctm.web.core.leadService.model.LeadType.CALL_ME_NOW;
@@ -26,6 +30,7 @@ import static com.ctm.web.core.leadService.model.LeadType.CALL_ME_NOW;
 @RestController
 @RequestMapping("/rest/health")
 public class HealthCallBackController extends CommonQuoteRouter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HealthCallBackController.class);
 
     @Autowired
     private HealthCallBackService healthCallBackService;
@@ -40,10 +45,12 @@ public class HealthCallBackController extends CommonQuoteRouter {
             method= RequestMethod.POST,
             consumes={MediaType.APPLICATION_FORM_URLENCODED_VALUE, "application/x-www-form-urlencoded;charset=UTF-8"})
     public void callMeBack(@Valid @NotNull final HealthCallBackData data, HttpServletRequest request) throws Exception {
-        final Data dataBucket = getDataBucket(request, data.getTransactionId());
         if (StringUtils.isBlank(data.getScheduledDateTime())) {
-            throw new RouterException("Scheduled date time is mandatory");
+            SchemaValidationError schemaValidationError = new SchemaValidationError();
+            schemaValidationError.setMessage("Scheduled date time is mandatory");
+            throw new RouterException(data.getTransactionId(), Collections.singletonList(schemaValidationError));
         }
+        final Data dataBucket = getDataBucket(request, data.getTransactionId());
         healthCallBackService.sendLead(data, dataBucket, request, CALL_ME_BACK);
     }
 
@@ -53,6 +60,15 @@ public class HealthCallBackController extends CommonQuoteRouter {
     public void callMeNow(@Valid @NotNull final HealthCallBackData data, HttpServletRequest request) throws Exception {
         final Data dataBucket = getDataBucket(request, data.getTransactionId());
         healthCallBackService.sendLead(data, dataBucket, request, CALL_ME_NOW);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorInfo handleException(final Exception e) {
+        LOGGER.error("Failed to handle request", e);
+        ErrorInfo errorInfo = new ErrorInfo();
+        errorInfo.getErrors().putIfAbsent("error", e.getMessage());
+        return errorInfo;
     }
 
 }
