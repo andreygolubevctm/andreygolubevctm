ad0a56831065286a00a160cfaa09ca23ae335587
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2016-12-07 05:55:47 +1000

    HNC-458: added delay when slide 1 validates and auto slides to slide 2. Also removed unnecessary delay to configure journey steps as added 200ms delay by default to journeyEngine process method (used when loading quotes)

--- src/main/webapp/bundles/home/js/home.js
+++ src/main/webapp/bundles/home/js/home.js
@@ -80,44 +80,31 @@
 				startStepId = meerkat.modules.emailLoadQuote.getStartStepOverride(startStepId);
 			}
 
-			$(document).ready(function(){
-				var callback = function delayedJourneyEngineInit() {
-					meerkat.modules.journeyEngine.configure({
-						startStepId: startStepId,
-						steps: _.toArray(steps)
-					});
-
-					// Call initial supertag call
-					var transaction_id = meerkat.modules.transactionId.get();
-
-					if (meerkat.site.isNewQuote === false) {
-						meerkat.messaging.publish(meerkatEvents.tracking.EXTERNAL, {
-							method: 'trackQuoteEvent',
-							object: {
-								action: 'Retrieve',
-								transactionID: parseInt(transaction_id, 10)
-							}
-						});
-					} else {
-						meerkat.messaging.publish(meerkatEvents.tracking.EXTERNAL, {
-							method: 'trackQuoteEvent',
-							object: {
-								action: 'Start',
-								transactionID: parseInt(transaction_id, 10)
-							}
-						});
-					}
-				};
-
-				/* create delay when quote is not new so that journey
-				 can finish setting up before directing to results.
-				 And yes!, a defer will not cut the mustard */
-				if(meerkat.site.isNewQuote === false) {
-					_.delay(callback, 500);
-				} else {
-					callback();
-				}
+			meerkat.modules.journeyEngine.configure({
+				startStepId: startStepId,
+				steps: _.toArray(steps)
 			});
+
+			// Call initial supertag call
+			var transaction_id = meerkat.modules.transactionId.get();
+
+			if (meerkat.site.isNewQuote === false) {
+				meerkat.messaging.publish(meerkatEvents.tracking.EXTERNAL, {
+					method: 'trackQuoteEvent',
+					object: {
+						action: 'Retrieve',
+						transactionID: parseInt(transaction_id, 10)
+					}
+				});
+			} else {
+				meerkat.messaging.publish(meerkatEvents.tracking.EXTERNAL, {
+					method: 'trackQuoteEvent',
+					object: {
+						action: 'Start',
+						transactionID: parseInt(transaction_id, 10)
+					}
+				});
+			}
 
 		}
 	}
@@ -153,7 +140,8 @@
 				customValidation: function (callback) {
 					// prevent from jumping to the next step if the selections are incorrect
 					var doContinue = meerkat.modules.homeCoverTypeWarning.validateSelections();
-					callback(doContinue);
+					var boundCallback = _.bind(callback, this, doContinue);
+					_.delay(boundCallback,doContinue ? 500 : 0);
 				}
 			},
 			onInitialise: function onStartInit(event) {
