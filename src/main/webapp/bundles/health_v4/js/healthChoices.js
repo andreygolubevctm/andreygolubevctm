a24d2f19b3a1070a6947daf17626427bbc363109
Adrian Diente <adrian.diente@comparethemarket.com.au>
2016-12-13 11:29:54 +1000

    HLT-3774: removed functionality no longer required as there's no a separate healthLocation.js module

--- src/main/webapp/bundles/health_v4/js/healthChoices.js
+++ src/main/webapp/bundles/health_v4/js/healthChoices.js
@@ -31,8 +31,7 @@
 			healthCover: $('input[name=health_situation_healthCvr]'),
 			state: $('#health_situation_state'),
 			postcode: $('#health_situation_postcode'),
-			suburb: $('#health_situation_suburb'),
-			location: $('#health_situation_location')
+			suburb: $('#health_situation_suburb')
 		};
 	}
 
@@ -41,24 +40,9 @@
 			meerkat.messaging.publish(moduleEvents.health.SNAPSHOT_FIELDS_CHANGE);
 		});
 
-		// we need to wait till the field gets proparly populated from the address search ajax
-		$elements.location.on('change',function() {
-			setTimeout(function() {
-				meerkat.messaging.publish(moduleEvents.health.SNAPSHOT_FIELDS_CHANGE);
-			},250);
-
-		}).on('blur',function() {
-			setLocation($(this).val());
-		});
-
 		$elements.healthSitGroup.on('change',function() {
 			meerkat.messaging.publish(moduleEvents.health.SNAPSHOT_FIELDS_CHANGE);
 		});
-
-		// For loading in.
-		if($elements.location.val() !== '') {
-			setLocation($elements.location.val());
-		}
 	}
 
 	function hasSpouse() {
@@ -71,41 +55,12 @@
 		}
 	}
 
+	function getCover() {
+		return _cover;
+	}
+
 	function setCover(cover) {
 		_cover = cover || 'SM'; // default to a single
-	}
-
-	function _isValidLocation( location ) {
-
-		var search_match = new RegExp(/^((\s)*([^~,])+\s+)+\d{4}((\s)+(ACT|NSW|QLD|TAS|SA|NT|VIC|WA)(\s)*)$/),
-		value = $.trim(String(location));
-
-		if( value !== '' ) {
-			if( value.match(search_match) ) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	function setLocation(location) {
-
-		if( _isValidLocation(location) ) {
-			var value = $.trim(String(location));
-			var pieces = value.split(' ');
-			var state = pieces.pop();
-			var postcode = pieces.pop();
-			var suburb = pieces.join(' ');
-			$elements.state.val(state);
-			$elements.postcode.val(postcode).trigger("change");
-			$elements.suburb.val(suburb);
-
-			setState(state);
-		} else if (meerkat.site.isFromBrochureSite) {
-			//Crappy input which doesn't get validated on brochureware quicklaunch should be cleared as they didn't get the opportunity to see results via typeahead on our side.
-			$elements.location.val("");
-		}
 	}
 
 	function setState (state) {
@@ -127,7 +82,7 @@
 		hasSpouse: hasSpouse,
 		returnCoverCode: returnCoverCode,
 		setCover: setCover,
-		setLocation: setLocation,
+		getCover: getCover,
 		setState: setState,
 		shouldPerformUpdate: shouldPerformUpdate
 	});
