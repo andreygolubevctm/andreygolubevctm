03a1a2967f9b7bc8779183d72659fe1c4742adcf
Leto Kauler <leto.kauler@comparethemarket.com.au>
2017-01-17 08:19:58 +1000

    HLT-3942: Correct confirmation data test

--- src/test/java/com/ctm/web/health/services/HealthConfirmationServiceTest.java
+++ src/test/java/com/ctm/web/health/services/HealthConfirmationServiceTest.java
@@ -83,7 +83,7 @@ public class HealthConfirmationServiceTest {
                 "<phoneNumber>" + expectedPhoneNumber + "</phoneNumber>" +
                 "<email>" + expectedEmail + "</email>" +
                 "<website>" + expectedWebsite + "</website>" +
-                "</providerInfo><whatsNext/><product/><policyNo>"+policyNo+"</policyNo><paymentType/><encryptedOrderLineId></encryptedOrderLineId></data>";
+                "</providerInfo><whatsNext/><product/><policyNo>"+policyNo+"</policyNo><paymentType/><redemptionId></redemptionId></data>";
 
         ArgumentCaptor<Confirmation> argument = ArgumentCaptor.forClass(Confirmation.class);
         verify(confirmationService).addConfirmation(argument.capture());
