e70d631f016643fc0320b7e70205cae5b4285393
Leto Kauler <leto.kauler@comparethemarket.com.au>
2017-01-30 16:52:33 +1000

    HLT-3941: Write some reward responses manually so I can customise the jackson mapper; Import model fixes;

--- src/main/java/com/ctm/web/reward/router/RewardController.java
+++ src/main/java/com/ctm/web/reward/router/RewardController.java
@@ -1,14 +1,22 @@
 package com.ctm.web.reward.router;
 
+import com.ctm.interfaces.common.util.SerializationMappers;
 import com.ctm.reward.model.*;
 import com.ctm.web.core.router.CommonQuoteRouter;
 import com.ctm.web.core.security.IPAddressHandler;
 import com.ctm.web.core.services.SessionDataServiceBean;
 import com.ctm.web.reward.services.RewardService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -24,22 +32,32 @@ public class RewardController extends CommonQuoteRouter {
     @Value("${ctm.reward.url}")
     private String rewardServiceBase;
 
-    private RewardService rewardService;
+	private SerializationMappers objectMapper;
+
+	private RewardService rewardService;
 
     @Autowired
     public RewardController(SessionDataServiceBean sessionDataServiceBean,
 							IPAddressHandler ipAddressHandler,
-							RewardService rewardService) {
+							RewardService rewardService,
+							SerializationMappers objectMapper) {
 		super(sessionDataServiceBean, ipAddressHandler);
         this.rewardService = rewardService;
-    }
+        this.objectMapper = objectMapper;
+
+		this.objectMapper.getJsonMapper()
+				.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+				.registerModule(new Jdk8Module())
+				.registerModule(new JavaTimeModule());
+	}
 
     @RequestMapping(value = CAMPAIGNS_GET,
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public GetCampaignsResponse getCampaigns(final HttpServletRequest request) {
-		return rewardService.getAllActiveCampaigns(request);
-    }
+    public String getCampaigns(final HttpServletRequest request) throws JsonProcessingException {
+		GetCampaignsResponse campaigns = rewardService.getAllActiveCampaigns(request);
+		return objectMapper.getJsonMapper().writeValueAsString(campaigns);
+	}
 
 	@RequestMapping(value = ORDER_CREATE,
             method = RequestMethod.POST,
@@ -61,7 +79,8 @@ public class RewardController extends CommonQuoteRouter {
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public FindResponse findOrders(@Valid @RequestBody final FindRequest form, final HttpServletRequest request) {
-    	return rewardService.findOrders(form, request);
+    public String findOrders(@Valid @RequestBody final FindRequest form, final HttpServletRequest request) throws JsonProcessingException {
+		FindResponse findResponse = rewardService.findOrders(form, request);
+		return objectMapper.getJsonMapper().writeValueAsString(findResponse);
     }
 }
