72bdbd18197fb6ea9fbfdb4df3ec7e7cb6da66a5
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2017-02-28 08:27:03 +1000

    HLT-4087 Fixed implementation of addMethod calls as they're not chainable (apparently)

--- src/main/webapp/bundles/shared/validation/js/validationNameRules.js
+++ src/main/webapp/bundles/shared/validation/js/validationNameRules.js
@@ -8,16 +8,15 @@
      * To enable this rule on an element, it needs the "data-rule-personName='true'" attribute to be added.
      * To override the message, use data-msg-personName="your new message".
      */
-    $.validator
-        .addMethod(
+    $.validator.addMethod(
         "personName", function (value, element, param) {
             var isURL = value.match(urlRegex) !== null;
             return !isURL && personNameRegex.test(value);
         }, "Please enter alphabetic characters only. " +
         "Unfortunately, international alphabetic characters, numbers and symbols are not " +
         "supported by many of our partners at this time."
-    )
-        .addMethod(
+    );
+    $.validator.addMethod(
             "personNameLtd", function (value, element, param) {
                 var isURL = value.match(urlRegex) !== null;
                 return !isURL && personNameLtdRegex.test(value);
