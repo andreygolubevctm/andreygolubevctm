7dbdc35f35590cc534bb16de34fff3f50cd23715
ljohnson <louis.johnson@comparethemarket.com.au>
2017-07-06 16:15:40 +1000

    TRV-1395 - removed useles code

--- src/main/webapp/bundles/travel/js/travellers.js
+++ src/main/webapp/bundles/travel/js/travellers.js
@@ -178,7 +178,6 @@
 	
 	function insertValues() {
 		values = state.hiddenValues;
-		$('input[name=travel_adults]').val(values.length);
 		for (var i = 0; values.length > i; i++) {
 			var indexFix = i + 1;
 			if ($('input[name=travellers-age-'+ indexFix +']').length > 0) {
