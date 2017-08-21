e47dcff1fc0ccc0f6f59118d75c8bd94a12dcb67
pgopisetty <prateep.gopisetty@gmail.com>
2017-08-21 14:57:37 +1000

    HLT-4574: call only for simples

--- src/main/webapp/bundles/health/js/health.js
+++ src/main/webapp/bundles/health/js/health.js
@@ -170,7 +170,9 @@
 			onInitialise: function onStartInit(event){
 
 				meerkat.modules.jqueryValidate.initJourneyValidator();
-                meerkat.modules.simplesInteraction.storeCallId();
+                if(meerkat.site.isCallCentreUser) {
+                    meerkat.modules.simplesInteraction.storeCallId();
+                }
 
 				if(meerkat.site.choices) {
 					meerkat.modules.healthChoices.initialise(meerkat.site.choices.cover, meerkat.site.choices.situation, meerkat.site.choices.benefits);
