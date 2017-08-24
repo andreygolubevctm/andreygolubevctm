720e7fcfee89f2d8f4130d1ec67394500cc1cf0c
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2017-08-24 13:10:29 +1000

    HLT-3960 Fixed error rendering - trying to append error label after element that doesn't exist (unless primary error has been triggered)

--- src/main/webapp/bundles/health_remember_me/js/health_remember_me.js
+++ src/main/webapp/bundles/health_remember_me/js/health_remember_me.js
@@ -88,7 +88,7 @@
 	        $elements.errors.primary.show();
         } else if (type === errorTypes.NOMATCH) {
             if(!$elements.errors.additional.length) {
-            	$('<label id="rememberme_additional-error" class="error">The date of birth you entered didn\'t match our records, enter your date of birth again or start a new quote</label>').insertAfter('#rememberme_primary_dob-error');
+            	$('<label id="rememberme_additional-error" class="error">The date of birth you entered didn\'t match our records, enter your date of birth again or start a new quote</label>').insertAfter('#rememberme_primary_dob');
 	        }
 	        _.defer($elements.errors.additional.show);
         } else {
@@ -126,7 +126,11 @@
                     	if (result.validAnswer === true && result.transactionId !== "") {
                             _track('success');
                             meerkat.modules.leavePageWarning.disable();
-                            $elements.loadingMessage.text('Loading Products & Prices Please wait...');
+		                    if(result.reviewEdit === true) {
+		                    	$elements.loadingMessage.text('Loading your quote... Please wait...');
+		                    } else {
+			                    $elements.loadingMessage.text('Loading Products & Prices... Please wait...');
+		                    }
                             showLoadingPage();
                             var newQSParams = {
                                 action : "remember",
