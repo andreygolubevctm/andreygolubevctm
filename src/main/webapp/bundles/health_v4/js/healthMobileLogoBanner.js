3c7610d1afd3bf1c01ad98b42f212335783a9425
Jason McKechnie <jason.mckechnie@comparethemarket.com.au>
2017-09-01 08:42:52 +1000

    HLT-4684 -implement mobile logo banner for AB tests - mobile friendly changes

--- src/main/webapp/bundles/health_v4/js/healthMobileLogoBanner.js
+++ src/main/webapp/bundles/health_v4/js/healthMobileLogoBanner.js
@@ -4,14 +4,16 @@
         meerkatEvents = meerkat.modules.events;
 
     function trigger() {
-        $('<div class="sidebar-widget sidebar-widget-padded sidebar-widget-background-contained mobile-logo-container visible-xs" style="border: 1px solid #ddd; background-color: #fff; margin-bottom: 15px;"><h3>You&apos;re one step away from comparing cover!</h3><p>Find the right plan that suits you from one of our 16 partners</p><img class="logo-panel hidden" src="/static/health/images/partner-logos.png" width="231px" height="262px"><img class="logo-panel-collapsed" src="/static/health/images/partner-logos-collapsed.png" width="231px" height="81px"><div class="logo-panel-collapsed-link"><a href="javascript:;" >View all partners  <span class="icon-angle-down"></span></a></div><div class="logo-panel-link hidden"><a href="javascript:;" >hide <span class="icon-angle-up"></span></a></div></div>').insertBefore('#contactForm');
+        $('<div class="sidebar-widget sidebar-widget-padded sidebar-widget-background-contained mobile-logo-container visible-xs"><h3>You&apos;re one step away from comparing cover!</h3><p>Find the right plan that suits you from one of our 16 partners</p><div class="logo-images-container"><div class="logo-panel collapsed">&nbsp;</div></div><div class="logo-panel-collapsed-link"><a href="javascript:;" >View all partners  <span class="icon-angle-down"></span></a></div><div class="logo-panel-link hidden"><a href="javascript:;" >hide <span class="icon-angle-up"></span></a></div></div>').insertBefore('#contactForm');
         $(".logo-panel-collapsed-link").click(function(){
-            $('.logo-panel, .logo-panel-link').removeClass('hidden');
-            $('.logo-panel-collapsed-link, .logo-panel-collapsed').addClass('hidden');
+            $('.logo-panel-link').removeClass('hidden');
+            $('.logo-panel-collapsed-link').addClass('hidden');
+            $('.logo-panel.collapsed').removeClass('collapsed');
         });
         $(".logo-panel-link").click(function(){
-            $('.logo-panel-collapsed-link, .logo-panel-collapsed').removeClass('hidden');
-            $('.logo-panel, .logo-panel-link').addClass('hidden');
+            $('.logo-panel-collapsed-link').removeClass('hidden');
+            $('.logo-panel').addClass('collapsed');
+            $('.logo-panel-link').addClass('hidden');
         });
     }
 
