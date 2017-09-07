b74874e089e12c2146471807138a2b220c7b9f93
ljohnson <louis.johnson@comparethemarket.com.au>
2017-09-08 08:49:28 +1000

    TRV-1394 - updating functionality of date picker

--- src/main/webapp/WEB-INF/tags/travel/date_picker.tag
+++ src/main/webapp/WEB-INF/tags/travel/date_picker.tag
@@ -2,6 +2,8 @@
 <%@ tag language="java" pageEncoding="UTF-8"%>
 <%@ include file="/WEB-INF/tags/taglib.tagf"%>
 
+<%@ attribute name="xpath" required="true" rtexprvalue="true" description="field group's xpath"%>
+
 <div class="clearfix dp">
   <div class="col-sm-4 col-xs-12 row-content dp__textContainer">
     <p class="traveler-heading">
@@ -16,11 +18,13 @@
       <span class="dp__input__header">Departure</span>
       <input id="departureDisplay" class="dp__input__item" />
       <input id="departure" class="dp__input__hidden" />
+      <field_v1:hidden xpath="${xpath}/dates/fromDate" defaultValue="${callId}" />
     </div>
     <div class="dp__input">
       <span class="dp__input__header">Return</span>
       <input id="returnDisplay" class="dp__input__item" />
       <input id="return" class="dp__input__hidden" />
+      <field_v1:hidden xpath="${xpath}/dates/toDate" defaultValue="${callId}" />
     </div>
   </div>
 </div>
--- src/main/webapp/bundles/travel/js/datepicker.js
+++ src/main/webapp/bundles/travel/js/datepicker.js
@@ -5,7 +5,9 @@
   
   var elements = {
     departure: document.getElementById('departure'),
-    returned: document.getElementById('return')
+    returned: document.getElementById('return'),
+    hiddenDeparture: document.getElementById('travel_dates_fromDate'),
+    hiddenReturned: document.getElementById('travel_dates_toDate')
   };
   
   var display = {
@@ -15,13 +17,14 @@
 
   var options = {
     departureOptions: {
-      dateFormat: "d/m/y",
+      dateFormat: "dd/mm/y",
       minDate: 'today',
       onOpen: function() {
         display.departure.classList.add('dp__input__item--active');
       },
       onChange: function(selectedDates, dateStr, instance) {
         display.departure.value = formatDate(instance.selectedDates[0]);
+        elements.hiddenDeparture.value = formatDate(instance.selectedDates[0]);    
       },
     	onClose: function(selectedDates, dateStr) {
         display.departure.classList.remove('dp__input__item--active');
@@ -31,7 +34,7 @@
       }
     },
     returnOptions: {
-      dateFormat: "d/m/y",
+      dateFormat: "dd/mm/y",
     	mode: 'range',
       minDate: 'today',
       onOpen: function() {
@@ -40,6 +43,7 @@
       onChange: function(selectedDates, dateStr, instance) {
         if (instance.selectedDates.length > 1) {
           display.returned.value = formatDate(instance.selectedDates[1]);
+          elements.hiddenReturned.value = formatDate(instance.selectedDates[1]);
         }
       },
       onClose: function() {
@@ -50,8 +54,8 @@
     
   function formatDate(date) {
     if (typeof date.getMonth === 'function') {
-      var day = date.getDate();
-      var month = date.getMonth();
+      var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+      var month = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth();
       var year = date.getFullYear();
       return day + '/' + month + '/' + year;
     }
