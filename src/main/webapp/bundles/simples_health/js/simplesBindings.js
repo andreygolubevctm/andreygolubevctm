3246c4f74f1d58df5133e25e854ee334e8e61811
John Vuong <john.vuong@comparethemarket.com.au>
2017-09-07 15:59:40 +1000

    HLT-4764: removed call on init

--- src/main/webapp/bundles/simples_health/js/simplesBindings.js
+++ src/main/webapp/bundles/simples_health/js/simplesBindings.js
@@ -80,7 +80,6 @@
             toggleBenefitsDialogue();
             initDBDrivenCheckboxes();
             toggleFollowupCallDialog();
-            toggleAffiliateRewardsDialogue();
 
             applyEventListeners();
             eventSubscriptions();
