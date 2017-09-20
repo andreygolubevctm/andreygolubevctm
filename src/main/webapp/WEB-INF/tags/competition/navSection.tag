2a07a5b3b9d40be23241d58cfe728111e0794ef3
Louis Johnson <louis.johnson@comparethemarket.com.au>
2017-09-20 14:09:19 +1000

    AGG-2726 - fixed logic issue - added ds_store to gitignore

--- src/main/webapp/WEB-INF/tags/competition/navSection.tag
+++ src/main/webapp/WEB-INF/tags/competition/navSection.tag
@@ -2,8 +2,8 @@
 <%@ tag description="Competition Snapshot"%>
 <%@ include file="/WEB-INF/tags/taglib.tagf" %>
 <c:if test="${octoberComp}">
-  <div class="octoberComp__navbarContainer">
-    <div style="display: none;" class="octoberComp__navbar">
+  <div class="octoberComp__navbarContainer" style="display: none;">
+    <divclass="octoberComp__navbar">
       <div class="col-sm-3 octoberComp__navbar__header">A winner every day!</div>
       <div class="col-sm-9">Buy home & contents insurance for your chance to win a share of $81,000. <a target="_blank" href="https://secure.comparethemarket.com.au/static/legal/october_promotion.pdf">T&C's apply</a></div>
     </div>
--- src/main/webapp/bundles/shared/competition/js/octoberComp.js
+++ src/main/webapp/bundles/shared/competition/js/octoberComp.js
@@ -1,11 +1,10 @@
 /*jshint esversion: 6 */
 
 ;(function($, undefined) {
-
   const trigger = document.querySelector('.octoberComp__mobile__trigger');
   const banner = document.querySelector('.octoberComp__mobile');
-  const nav = document.querySelector('.octoberComp__navbar');
-  const octoberComp = meerkat.site.octoberComp;
+  const nav = document.querySelector('.octoberComp__navbarContainer');
+  const octoberComp = trigger && banner && nav;
   var bannerTxt;
 
   function setFooterPos() {
@@ -31,11 +30,11 @@
 
   function showNav() {
       nav.style.display = '';
-      nav.classList.add('octoberComp__navbar--active');
+      nav.classList.add('octoberComp__navbarContainer--active');
   }
 
   function hideNav() {
-      nav.classList.remove('octoberComp__navbar--active');
+      nav.classList.remove('octoberComp__navbarContainer--active');
   }
 
   var modules = octoberComp ? {
@@ -43,6 +42,6 @@
     showNav: showNav,
     hideNav: hideNav
   } : {};
-
+  console.log(modules);
 	meerkat.modules.register("octoberComp", modules);
 })(jQuery);
