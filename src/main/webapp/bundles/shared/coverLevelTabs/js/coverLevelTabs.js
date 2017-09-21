266c356f04b6bda091ef163d375b1bab88ad36cc
Pascoal Gomes <Pascoal.Gomes@comparethemarket.com.au>
2017-09-21 16:54:15 +1000

    [TRV-1497] added the trip type filters

--- src/main/webapp/bundles/shared/coverLevelTabs/js/coverLevelTabs.js
+++ src/main/webapp/bundles/shared/coverLevelTabs/js/coverLevelTabs.js
@@ -245,11 +245,15 @@
 				originatingTab = settings.verticalMapping[tab.rankingFilter];
 			}
 		}
-		out += '<div class="col-xs-4 has-success">';
-		out +=		'<div class="col-xs-12 clt-trip-type">Additional activities covered</div>';
-		out +=		'<div class="col-xs-4 clt-trip-type"><input type="checkbox" name="snow_sports" id ="snow_sports" class="checkbox-custom checkbox has-success" value="SnowSports"><label for="snow_sports">Snow Sports</label></div>';
-		out +=		'<div class="col-xs-3 clt-trip-type"><input type="checkbox" name="cruise" id ="cruise" class="checkbox-custom checkbox has-success" value="Cruise"><label for="cruise">Cruise</label></div>';
-		out +=		'<div class="col-xs-5 clt-trip-type"><input type="checkbox" name="adventure_sports" id ="adventure_sports" class="checkbox-custom checkbox has-success" value="AdventureSports"><label for="adventure_sports">Adventure Sports</label></div>';
+		out += '<div class="col-xs-4 clt-trip-filter">';
+		out +=		'<div class="col-xs-12 no-padding"><b>Additional activities covered</b></div>';
+		out +=		'<div class="col-xs-4 no-padding"><div class="checkbox"><input type="checkbox" name="snow_sports" id ="snow_sports" class="checkbox-custom checkbox" value="SnowSports"><label for="snow_sports">Snow Sports</label></div></div>';
+		out +=		'<div class="col-xs-3 no-padding"><div class="checkbox"><input type="checkbox" name="cruise" id ="cruise" class="checkbox-custom checkbox" value="Cruise"><label for="cruise">Cruise</label></div></div>';
+		out +=		'<div class="col-xs-5 no-padding"><div class="checkbox"><input type="checkbox" name="adventure_sports" id ="adventure_sports" class="checkbox-custom checkbox" value="AdventureSports"><label for="adventure_sports">Adventure Sports</label></div></div>';
+		out += '</div>';
+		out += '<div class="col-xs-1 clt-trip-filter">';
+		out += 		'<div class="col-xs-12 no-padding"><b>Excess up to</b></div>';
+		out +=		'<div class="col-xs-12 no-padding">$150</div>';
 		out += '</div>';
 
 		$currentTabContainer.empty().html(out);
