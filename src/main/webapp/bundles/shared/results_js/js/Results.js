736157bea346162ba68069d958ab8fa0d4fa0d99
jvuong <john.vuong@comparethemarket.com.au>
2017-10-06 11:36:50 +1000

    HLT-4672: selecting pinned product on different results set fix

--- src/main/webapp/bundles/shared/results_js/js/Results.js
+++ src/main/webapp/bundles/shared/results_js/js/Results.js
@@ -580,6 +580,8 @@ var Results = {
         }
         product.isPinned = 'Y';
 
+        Results.setPinnedProduct(product);
+
         // Must copy the element before filtering or it gets unnecessary classes etc.
         Results.$pinnedResultRow = $('.result_' + pinnedProductId).clone(true);
 
@@ -610,9 +612,6 @@ var Results = {
         }
 
         var product = Results.model.getResult("productId", pinnedProductId);
-        if (!product) {
-            return;
-        }
 
         if(Results.$pinnedResultRow) {
             Results.$pinnedResultRow.remove();
@@ -624,5 +623,19 @@ var Results = {
         product.isPinned = 'N';
         $(Results.settings.elements.resultsOverflow).removeClass('product-pinned');
         Results.pagination.hasPinnedProduct = false;
+
+
+	},
+
+	setPinnedProduct: function(product) {
+		Results.model.setPinnedProduct(product);
+	},
+
+    getPinnedProduct: function() {
+        return Results.model.pinnedProduct;
+    },
+
+    removePinnedProduct: function() {
+        Results.model.removePinnedProduct();
 	}
 };
