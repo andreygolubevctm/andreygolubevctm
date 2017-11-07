edd8227f5c6bfdba69c80c84d0290f4c3e0ee216
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2017-11-07 12:00:14 +1000

    HLT-4974 Fixed brandCode checks to only apply to WFDD and updated links to confirmation page

--- src/main/webapp/bundles/health/js/health.js
+++ src/main/webapp/bundles/health/js/health.js
@@ -1290,7 +1290,7 @@
 
 					meerkat.modules.leavePageWarning.disable();
 
-					var redirectURL = "health_confirmation.jsp?action=confirmation&transactionId="+meerkat.modules.transactionId.get()+"&token=";
+					var redirectURL = "health_confirmation.jsp?action=confirmation&transactionId="+meerkat.modules.transactionId.get()+(!_.isEmpty(meerkat.site.urlStyleCodeId) && meerkat.site.urlStyleCodeId === "wfdd" ? "&brandCode=" + meerkat.site.urlStyleCodeId : "")+"&token=";
 					var extraParameters = "";
 
 					if (meerkat.site.utm_source !== '' && meerkat.site.utm_medium !== '' && meerkat.site.utm_campaign !== ''){
