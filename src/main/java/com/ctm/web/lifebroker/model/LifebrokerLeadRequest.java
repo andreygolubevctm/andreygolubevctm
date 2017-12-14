5c346b7b168f8abd3a4e6557897490a25399e281
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2017-12-15 05:06:07 +1000

    LIF-461 Fixed up request XML to match partner requirements (which weren't specified and didnt error)

--- src/main/java/com/ctm/web/lifebroker/model/LifebrokerLeadRequest.java
+++ src/main/java/com/ctm/web/lifebroker/model/LifebrokerLeadRequest.java
@@ -12,7 +12,7 @@ public class LifebrokerLeadRequest {
     private final LifebrokerLeadRequestContact contact;
 
     public LifebrokerLeadRequest(String affiliateId, String email, String phone, String postcode, String name, String mediaCode, String callTime) {
-        this.contact = new LifebrokerLeadRequestContact(affiliateId, email, phone, postcode, callTime, new LifebrokerLeadRequestClient(name), new LifebrokerLeadRequestAdditional(mediaCode));
+        this.contact = new LifebrokerLeadRequestContact(affiliateId, email, phone, postcode, new LifebrokerLeadRequestClient(name), new LifebrokerLeadRequestAdditional(mediaCode, callTime));
     }
 
 
--- src/main/java/com/ctm/web/lifebroker/model/LifebrokerLeadRequestContact.java
+++ src/main/java/com/ctm/web/lifebroker/model/LifebrokerLeadRequestContact.java
@@ -17,23 +17,19 @@ class LifebrokerLeadRequestContact {
     @JacksonXmlProperty(namespace = LifebrokerLeadRequest.LIFEBROKER_NAMESPACE)
     private final String postcode;
 
-    @JacksonXmlProperty(namespace = LifebrokerLeadRequest.LIFEBROKER_NAMESPACE, localName = "Call_time")
-    private final String callTime;
-
     @JacksonXmlProperty(namespace = LifebrokerLeadRequest.LIFEBROKER_NAMESPACE)
     private final LifebrokerLeadRequestClient client;
 
     @JacksonXmlProperty(namespace = LifebrokerLeadRequest.LIFEBROKER_NAMESPACE)
     private final LifebrokerLeadRequestAdditional additional;
 
-    LifebrokerLeadRequestContact(String affiliateId, String email, String phone, String postcode, String callTime, LifebrokerLeadRequestClient client, LifebrokerLeadRequestAdditional additional) {
+    LifebrokerLeadRequestContact(String affiliateId, String email, String phone, String postcode, LifebrokerLeadRequestClient client, LifebrokerLeadRequestAdditional additional) {
         this.affiliateId = affiliateId;
         this.email = email;
         this.phone = phone;
         this.postcode = postcode;
         this.client = client;
         this.additional = additional;
-        this.callTime = callTime;
     }
 
     public String getAffiliateId() {
@@ -50,10 +46,6 @@ class LifebrokerLeadRequestContact {
 
     public String getPostcode() {
         return postcode;
-    }
-
-    public String getCallTime() {
-        return callTime;
     }
 
     public LifebrokerLeadRequestClient getClient() {
