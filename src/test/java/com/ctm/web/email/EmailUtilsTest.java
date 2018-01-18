69e8b22b0b6eeaae53222394d2773debe4194246
Matthew Edwards <mathew.edwards@comparethemarket.com.au>
2018-01-18 10:38:52 +1000

    HLT-4962: Code Review Feedback
    * Consolidated EmailFunctions into the EmailUtils class

--- src/test/java/com/ctm/web/email/EmailUtilsTest.java
+++ src/test/java/com/ctm/web/email/EmailUtilsTest.java
@@ -1,9 +1,12 @@
 package com.ctm.web.email;
 
+import com.google.common.collect.ImmutableList;
 import junitx.framework.ListAssert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -41,4 +44,44 @@ public class EmailUtilsTest {
 
         assertThat(testInstance.getParamFromXml(xmlString, "gaclientid", ""), equalTo(valueToExtract));
     }
+
+    @Test
+    public void whenListOfStringsWithHtmlTags_thenStripWhilstMaintainingSpacesInEachElement() {
+        List<String> expectedResult = ImmutableList.of("Hi ", "There, ", "No ", "Tags ", "Remain ");
+        List<String> testStrings = ImmutableList.of("Hi <span/>", "There, <span/>", "No <span/>", "Tags <span/>", "Remain <span/>");
+        List<String> result = EmailUtils.stripHtmlFromStrings.apply(testStrings);
+
+        assertThat(result, equalTo(expectedResult));
+
+    }
+
+    @Test
+    public void whenStringContainsNestedHtmlElements_thenReturnOnlyText() {
+        String testStrings = "Join on combined cover before the end of February and skip 2 and 6 month waits on extras<div><p></p><p></p></div>";
+        String expectedResult = "Join on combined cover before the end of February and skip 2 and 6 month waits on extras";
+        String result = EmailUtils.stripHtml.apply(testStrings);
+
+        assertThat(result, equalTo(expectedResult));
+    }
+
+    @Test
+    public void whenListElementsAreNull_thenSafelyHandleNull() {
+        String[] testStrings = new String[]{"Join on combined cover before the end of February and skip 2 and 6 month waits on extras<div><p></p><p></p></div>", null};
+        ImmutableList<String> expectedResult = ImmutableList.of("Join on combined cover before the end of February and skip 2 and 6 month waits on extras", "");
+        List<String> result = EmailUtils.stripHtmlFromStrings.apply(Arrays.asList(testStrings));
+
+        assertThat(result, equalTo(expectedResult));
+    }
+
+
+    @Test
+    public void givenAnInvalidBigDecimalString_thenReturnZero() {
+        assertThat(EmailUtils.bigDecimalOrZero.apply("NaN"), equalTo(BigDecimal.ZERO));
+    }
+
+    @Test
+    public void givenAvalidBigDecimalString_thenReturnAsBigDecimal() {
+        assertThat(EmailUtils.bigDecimalOrZero.apply("3.14"), equalTo(BigDecimal.valueOf(3.14)));
+    }
+
 }
