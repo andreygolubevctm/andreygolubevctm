c1ae21b756df63434089988c8cf9a7474fc78514
jvuong <john.vuong@comparethemarket.com.au>
2018-02-02 10:27:55 +1000

    HLT-5140: removed PO box re-applied address validation code

--- src/main/webapp/bundles/health/js/healthApplyStep.js
+++ src/main/webapp/bundles/health/js/healthApplyStep.js
@@ -82,7 +82,6 @@
 
         $unitElements.appPostalUnitType.on('change', function toggleUnitRequiredFields() {
             _changeStreetNoLabel(this.value);
-            _toggleStreetRules(this.value);
         });
 
         $unitElements.appAddressUnitShop.add($unitElements.appPostalUnitShop).on('change', function toggleUnitShopRequiredFields() {
@@ -141,18 +140,6 @@
 
         if ($errorField.length > 0) {
             $errorField.text(msgRequired);
-        }
-    }
-
-    function _toggleStreetRules(unitType) {
-        if (unitType === 'PO') {
-            $unitElements.appPostalNonStdStreet
-                .removeRule('regex')
-                .removeRule('validAddress');
-        } else {
-            $unitElements.appPostalNonStdStreet
-                .addRule('regex', '[a-zA-Z0-9 ]+')
-                .addRule('validAddress', 'health_application_postal');
         }
     }
 
