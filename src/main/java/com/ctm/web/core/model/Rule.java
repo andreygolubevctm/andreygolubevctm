01434c2c3ecea5a1b22b256c0435fe6ecee75b73
Matthew Edwards <mathew.edwards@comparethemarket.com.au>
2018-04-17 14:25:53 +1000

    CTMIT-1090: Serializable Session Data

--- src/main/java/com/ctm/web/core/model/Rule.java
+++ src/main/java/com/ctm/web/core/model/Rule.java
@@ -1,9 +1,13 @@
 package com.ctm.web.core.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  * Created by voba on 30/04/2015.
  */
-public class Rule {
+public class Rule implements Serializable {
+    private static final long serialVersionUID = 1L;
     private long id;
     private String description;
     private String value;
@@ -30,5 +34,21 @@ public class Rule {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Rule)) return false;
+        Rule rule = (Rule) o;
+        return getId() == rule.getId() &&
+                Objects.equals(getDescription(), rule.getDescription()) &&
+                Objects.equals(getValue(), rule.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId(), getDescription(), getValue());
     }
 }
