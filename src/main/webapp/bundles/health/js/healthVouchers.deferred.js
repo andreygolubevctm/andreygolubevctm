8a392ac06f3bdf7eb09949709c0e985030f7081e
jmcKechnie <jason.mcKechnie@gmail.com>
2018-04-30 10:39:41 +1000

    HLT-5486 - Adding coupon option for Simples user: Bonus $100

--- src/main/webapp/bundles/health/js/healthVouchers.deferred.js
+++ src/main/webapp/bundles/health/js/healthVouchers.deferred.js
@@ -39,6 +39,10 @@
                 '2018Seniors$100-offer': {
                     min: 100,
                     max: 100
+                },
+                'booster-bonus': {
+                    min: 100,
+                    max: 100
                 }
             }
     };
@@ -328,7 +332,7 @@
             $elements.inputs.value.empty().append('<option value="'+giftCardAmount+'">$'+giftCardAmount+'</option>');
         }
 
-        if(_.indexOf(['q-jun$100-offer','q-jun$200-offer', '2017oct$100', '2018Seniors$100-offer'],reason) !== -1) {
+        if(_.indexOf(['q-jun$100-offer','q-jun$200-offer', '2017oct$100', '2018Seniors$100-offer', 'booster-bonus'],reason) !== -1) {
             $elements.inputs.value.val(valueRange.custom[reason].max);
         }
     }
