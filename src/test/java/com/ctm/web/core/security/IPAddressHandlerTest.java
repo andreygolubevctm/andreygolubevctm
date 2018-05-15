195b42a5a75aaa74993babd4d17cf291d7834f5a
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2018-05-16 09:44:01 +1000

    AGG-2808 Added new tests for QA IPs

--- src/test/java/com/ctm/web/core/security/IPAddressHandlerTest.java
+++ src/test/java/com/ctm/web/core/security/IPAddressHandlerTest.java
@@ -87,6 +87,30 @@ public class IPAddressHandlerTest {
     }
 
     @Test
+    public void testIsLocalIP4() throws Exception {
+        boolean result = ipAddressHandler.isLocalIP("192.168.1.116");
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testIsLocalIP5() throws Exception {
+        boolean result = ipAddressHandler.isLocalIP("10.46.41.235");
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testIsQAIP1() throws Exception {
+        boolean result = ipAddressHandler.isLocalIP("13.54.55.150");
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testIsQAIP2() throws Exception {
+        boolean result = ipAddressHandler.isLocalIP("13.54.250.208");
+        assertEquals(true, result);
+    }
+
+    @Test
     public void testIsIPInRange1() throws Exception {
         boolean result = ipAddressHandler.isIPInRange("202.56.60.0");
         assertEquals(true, result);
@@ -101,18 +125,6 @@ public class IPAddressHandlerTest {
     @Test
     public void testIsIPInRange3() throws Exception {
         boolean result = ipAddressHandler.isIPInRange("202.56.61.255");
-        assertEquals(true, result);
-    }
-
-    @Test
-    public void testIsLocalIP4() throws Exception {
-        boolean result = ipAddressHandler.isLocalIP("192.168.1.116");
-        assertEquals(true, result);
-    }
-
-    @Test
-    public void testIsLocalIP5() throws Exception {
-        boolean result = ipAddressHandler.isLocalIP("10.46.41.235");
         assertEquals(true, result);
     }
 
