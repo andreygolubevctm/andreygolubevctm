9c3cf090f342989be238c611b47a25a67a493b9f
jmcKechnie <jason.mcKechnie@gmail.com>
2018-06-07 14:21:33 +1000

    HLT-5478 - Brand specific error message for no medicare card

--- src/main/webapp/bundles/shared/validation/js/validationMedicare.js
+++ src/main/webapp/bundles/shared/validation/js/validationMedicare.js
@@ -1,7 +1,12 @@
 (function ($) {
 
+    var additionalBrandSpecificErrorMsg = "";
+    if (meerkat.site.tracking.brandCode === 'ctm') {
+        additionalBrandSpecificErrorMsg = ", please <a href='https://www.comparethemarket.com.au/health-insurance/health-insurance-overseas-visitors/'>click here</a>";
+    }
+
     $.validator.addMethod('medicareCardColour', function (value, element) {
         return (value !== 'none');
-    }, "To proceed with this policy, you must have a green, blue or yellow medicare card. For overseas visitor cover, please <a href='https://www.comparethemarket.com.au/health-insurance/health-insurance-overseas-visitors/'>click here</a>");
+    }, "To proceed with this policy, you must have a green, blue or yellow medicare card. For overseas visitor cover" + additionalBrandSpecificErrorMsg + ".");
 
 })(jQuery);
