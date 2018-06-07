9415951e210ac4980e986943cded2b88bd59e375
jmcKechnie <jason.mcKechnie@gmail.com>
2018-06-07 15:41:38 +1000

    HLT-5478 - went too far

--- src/main/webapp/bundles/shared/validation/js/validationMedicare.js
+++ src/main/webapp/bundles/shared/validation/js/validationMedicare.js
@@ -1,6 +1,6 @@
 (function ($) {
     $.validator.addMethod('medicareCardColour', function (value, element) {
         return (value !== 'none');
-    }, "To proceed with this policy, you must have a green, blue or yellow medicare card. For overseas visitor cover.");
+    }, "To proceed with this policy, you must have a green, blue or yellow medicare card. For overseas visitor cover, please <a href='https://www.comparethemarket.com.au/health-insurance/health-insurance-overseas-visitors/'>click here</a>.");
 
 })(jQuery);
