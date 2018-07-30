f0aac225268f7294fa66ab8fd5126587384c9ac1
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2018-07-30 13:26:14 +1000

    CTM-372 Fixed environemnt check

--- src/main/java/com/ctm/web/core/rememberme/services/RememberMeService.java
+++ src/main/java/com/ctm/web/core/rememberme/services/RememberMeService.java
@@ -88,10 +88,8 @@ public class RememberMeService {
     }
 
     private static void setCookieDomain(final Cookie cookie) {
-        switch (EnvironmentService.getEnvironment()) {
-            case PRO:
-                cookie.setDomain("secure.comparethemarket.com.au");
-                break;
+        if (EnvironmentService.getEnvironment() ==  EnvironmentService.Environment.PRO) {
+			cookie.setDomain("secure.comparethemarket.com.au");
         }
     }
 
@@ -104,12 +102,8 @@ public class RememberMeService {
      * except LOCALHOST and NXI
      */
     private static void setCookieSecure(final Cookie cookie) {
-        switch (EnvironmentService.getEnvironment()) {
-            case LOCALHOST:
-            case NXI:
-                break;
-            default:
-                cookie.setSecure(true);
+        if (EnvironmentService.getEnvironment() != EnvironmentService.Environment.LOCALHOST) {
+			cookie.setSecure(true);
         }
     }
 
@@ -418,6 +412,7 @@ public class RememberMeService {
             final Cookie cookie = new Cookie(cookieName, "");
             cookie.setMaxAge(0);
             cookie.setPath("/");
+			setCookieDomain(cookie);
             response.addCookie(cookie);
             return true;
         }
