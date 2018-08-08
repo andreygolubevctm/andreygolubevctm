4b1e9770e189ab9cf96dfc212fee23d45771a0c7
Rick Upadhyay <Prateek.Upadhyay@comparethemarket.com.au>
2018-08-09 08:21:32 +1000

    HNC-690: Retired person question to show always other than in landlord case.
    - Match the behaviour with everest and show the `is any person retired` question always, except in case the policy holder is a landlord.
    - Tested locally
    - Build success

--- src/main/webapp/bundles/home/js/homePolicyHolder.js
+++ src/main/webapp/bundles/home/js/homePolicyHolder.js
@@ -16,6 +16,7 @@
 	var elements = {
 			name:						'home_policyHolder',
 			principalResidence:			"home_occupancy_principalResidence",
+			isLandlord:					"home_occupancy_isLandlord",
         	retired:					".retired",
 			otherOccupantsRow:			"#home_policyHolder_other_occupants",
 			toggleJointPolicyHolder:	$(".toggleJointPolicyHolder"),
@@ -29,47 +30,29 @@
 		var isPrincipalResidence = meerkat.modules.homeOccupancy.isPrincipalResidence();
 		if(isPrincipalResidence){
 			$(elements.otherOccupantsRow).slideDown(speed);
-			toggleRetired(speed);
 		} else {
 			$(elements.retired+', '+elements.otherOccupantsRow).slideUp(speed);
 		}
-
+        toggleRetired(speed);
 	}
 
-	function toggleRetired(speed){
-		var isPrincipalResidence = meerkat.modules.homeOccupancy.isPrincipalResidence();
-		var dateFormat = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
-		var dob = $("#"+elements.name+"_dob");
-		var jointDob = $("#"+elements.name+"_jointDob");
-
-		if(isPrincipalResidence &&
-			(
-				( dob.val().match(dateFormat) && getAge( dob.val() ) >= 55 ) ||
-				( jointDob.val().match(dateFormat) && getAge( jointDob.val() ) >= 55 )
-			)
-		) {
-			$(elements.retired).slideDown(speed);
-		} else {
-			$(elements.retired).slideUp(speed);
-		}
-	}
-
-	function getAge (dateString) {
-		var today = new Date();
-		var dateSplit = dateString.split("/").reverse();
-		var age = today.getFullYear() - parseInt(dateSplit[0],10);
-		var m = today.getMonth()+1 - parseInt(dateSplit[1],10);
-		if (m < 0 || (m === 0 && today.getDate() < parseInt(dateSplit[2],10))) {
-			age--;
-		}
-		return age;
-	}
+    /**
+     * Responsible for show/hide `is any person retired` question.
+     * - show question when applicant is principle resident, else hide (if landlord).
+     *
+     * @param speed
+     */
+    function toggleRetired(speed) {
+        var isLandlord = meerkat.site.isLandlord;
+        if (isLandlord) {
+            $(elements.retired).slideUp(speed); //hide
+        } else {
+            $(elements.retired).slideDown(speed); //show
+        }
+    }
 
 	function applyEventListeners() {
 		$(document).ready(function() {
-			$("#"+elements.name+"_dob, #"+elements.name+"_jointDob").on('change', function(){
-				toggleRetired();
-			});
 			elements.toggleJointPolicyHolder.on("click", function(){
 				if ( elements.jointPolicyHolder.is(":visible") ){
                     hasJointHolder = false;
