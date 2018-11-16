f696d54ba4d10ebc811e99cf058bdc5e7fb9b182
Matthew Klemm <matthew.klemm@comparethemarket.com.au>
2018-11-16 15:52:01 +1000

    updated intercom script

--- src/main/webapp/bundles/health_v4/intercom.js
+++ src/main/webapp/bundles/health_v4/intercom.js
@@ -5,24 +5,25 @@
 /*library*/
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/d29h2i1h';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
 
-
-var intercomElement = createIntercomElement();
-
-    injectCss();
-    document.body.appendChild(intercomElement);
-
-
     /*launch widget*/
     window.Intercom("boot", {
-        app_id: "d29h2i1h",
-        custom_launcher_selector: '#intercom-element',
-        hide_default_launcher: true
+        app_id: "d29h2i1h"
     });
 
     /*get last page*/
 
     window.intercomSettings = {
         "Last_Page_Viewed":"{{get path with fragment}}"
+    }
+    window.Intercom('update');
+
+    var intercomElement = createIntercomElement();
+    injectCss();
+    document.body.appendChild(intercomElement);
+
+    window.intercomSettings = {
+        custom_launcher_selector: '#intercom-element',
+        hide_default_launcher: true
     }
     window.Intercom('update');
 
