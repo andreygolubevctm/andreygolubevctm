0e597896010ccf713ca788dc2dcf58f5cd3bd614
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2018-12-13 15:56:15 +1000

    CTM-817 added new str util to strip 4byte utf8 chars from a string and implemented when writing textValue to transaction_details

--- src/main/java/com/ctm/web/core/utils/common/utils/StringUtils.java
+++ src/main/java/com/ctm/web/core/utils/common/utils/StringUtils.java
@@ -2,6 +2,30 @@ package com.ctm.web.core.utils.common.utils;
 
 public class StringUtils {
 
+    public static final String LAST_3_BYTE_UTF_CHAR = "\uFFFF";
+    public static final String REPLACEMENT_CHAR = "\uFFFD";
+
+    public static String toValid3ByteUTF8String(String str)  {
+        final int length = str.length();
+        StringBuilder b = new StringBuilder(length);
+        for (int offset = 0; offset < length; ) {
+            final int codepoint = str.codePointAt(offset);
+
+            // do something with the codepoint
+            if (codepoint > StringUtils.LAST_3_BYTE_UTF_CHAR.codePointAt(0)) {
+                b.append(StringUtils.REPLACEMENT_CHAR);
+            } else {
+                if (Character.isValidCodePoint(codepoint)) {
+                    b.appendCodePoint(codepoint);
+                } else {
+                    b.append(StringUtils.REPLACEMENT_CHAR);
+                }
+            }
+            offset += Character.charCount(codepoint);
+        }
+        return b.toString();
+    }
+
     public static String sqlQuestionMarkStringBuilder(int repeatNumber){
         String csvString="";
         for (int i = 0; i < repeatNumber; i++) {
