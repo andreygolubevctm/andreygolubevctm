2997e0df866b1b8ed840759b683bfa652924b3ed
Mark Smerdon <mark.smerdon@comparethemarket.com.au>
2019-02-12 13:50:09 +1000

    CTM-1207 updated selectedProductService to include method which returns a minimalist json object for use by the confirmation service

--- src/main/java/com/ctm/web/health/services/HealthSelectedProductService.java
+++ src/main/java/com/ctm/web/health/services/HealthSelectedProductService.java
@@ -4,11 +4,17 @@ import com.ctm.web.core.exceptions.DaoException;
 import com.ctm.web.core.transaction.dao.TransactionDetailsDao;
 import com.ctm.web.core.transaction.model.TransactionDetail;
 import com.ctm.web.health.dao.HealthSelectedProductDao;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class provides public accessors/modifiers to store product data in the database. Previously
@@ -39,6 +45,52 @@ public class HealthSelectedProductService {
 	public String getProductXML(final long transactionId) throws DaoException {
     	TransactionDetail detail = getProductIdFromTransactionDetails(transactionId);
 		return detail != null ? selectedProductDao.getSelectedProduct(transactionId, Long.parseLong(detail.getTextValue().replaceAll("\\D",""))) : null;
+	}
+
+	/**
+	 * getMinimalistProductXML strips excess content from the productXML (ie actually JSON).
+	 * Presently is removes the benefits data from the custom, extras and hospital nodes.
+	 *
+	 * @param transactionId
+	 * @param productId
+	 * @return
+	 * @throws DaoException
+	 */
+	public String getMinimalistProductXML(final long transactionId, final long productId) throws DaoException {
+    	String productXML = getProductXML(transactionId, productId);
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode productJSON = mapper.readTree(productXML);
+			if (productJSON != null) {
+				JsonNode price = productJSON.path("price").get(0);
+				if (!price.isMissingNode()) {
+					((ObjectNode) price.path("custom").path("reform").path("tab1")).remove("benefits");
+					JsonNode hospital = price.path("hospital");
+					if (!hospital.isMissingNode()) {
+						((ObjectNode) hospital).remove("benefits");
+					}
+					JsonNode extras = price.path("extras");
+					if (!extras.isMissingNode()) {
+						List<String> safeProps = new ArrayList<>();
+						safeProps.add("ClassificationGeneralHealth");
+						safeProps.add("PreferredProviderServices");
+						List<String> benefits = new ArrayList<>();
+						extras.fields().forEachRemaining(entry -> {
+							if(!safeProps.contains(entry.getKey())) {
+								benefits.add(entry.getKey());
+							}
+						});
+						benefits.stream().forEach(x -> {
+							((ObjectNode) extras).remove(x);
+						});
+					}
+				}
+				productXML = productJSON.toString();
+			}
+		} catch (IOException e) {
+			throw new DaoException(e);
+		}
+		return productXML;
 	}
 
 	// Cannot access overloaded methods from beans so distinct name required
