62feb53860b93920293a6c2a9eb4fae3b40feed0
Matt Edwards <mathew.edwards@comparethemarket.com.au>
2019-02-20 09:46:35 +1000

    HREFORM-444: Fixes Min Age Defect
    * Modifies calculation function to use an explicitly pre-calculated age and
    percentage map for simplicity
    * Renames method names to be clearer
    * Adds some Javadoc describing the ABD Intro Date

--- src/main/java/com/ctm/web/health/quote/model/abd/ABD.java
+++ src/main/java/com/ctm/web/health/quote/model/abd/ABD.java
@@ -2,6 +2,7 @@ package com.ctm.web.health.quote.model.abd;
 
 import com.ctm.web.core.utils.common.utils.LocalDateUtils;
 import com.ctm.web.health.quote.model.request.ProductType;
+import org.elasticsearch.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,7 @@ import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.IntStream;
@@ -20,16 +22,16 @@ import static java.util.Arrays.asList;
  * {@link ABD} provides support methods/functions for calculating Age Based Discount.
  */
 public final class ABD {
+    private static final Logger logger = LoggerFactory.getLogger(ABD.class);
     /**
      * Describes the product types for which ABD may be applied.
      */
     public static final List<ProductType> APPLICABLE_PRODUCT_TYPES = asList(ProductType.COMBINED, ProductType.HOSPITAL);
-
-    private static final Logger logger = LoggerFactory.getLogger(ABD.class);
     /**
      * The Maximum percentage applicable for Age Based Discount.
      */
-    public static final int MAX_ABD_FACTOR = 10;
+    public static final int MAX_ABD_PERCENTAGE = 10;
+
     /**
      * Constant representing thirty years of age.
      * Before reaching the age of thirty, funds may choose to apply an age based discount.
@@ -41,6 +43,25 @@ public final class ABD {
      * per year, per policy holder.
      */
     public static final int FOURTY_ONE = 41;
+    /**
+     * Pre-calculated list of applicable Age Based Discount Percentages by age in years.
+     */
+    public static final Map<Integer, Integer> ABD_PERCENTAGES = ImmutableMap.<Integer, Integer>builder()
+            .put(18, MAX_ABD_PERCENTAGE)
+            .put(19, MAX_ABD_PERCENTAGE)
+            .put(20, MAX_ABD_PERCENTAGE)
+            .put(21, MAX_ABD_PERCENTAGE)
+            .put(22, MAX_ABD_PERCENTAGE)
+            .put(23, MAX_ABD_PERCENTAGE)
+            .put(24, MAX_ABD_PERCENTAGE)
+            .put(25, MAX_ABD_PERCENTAGE)
+            .put(26, 8)
+            .put(27, 6)
+            .put(28, 4)
+            .put(29, 2)
+            .build();
+
+
     /**
      * The First Date from which ABD may be calculated.
      */
@@ -62,6 +83,9 @@ public final class ABD {
     /**
      * Calculate the number of years from {@code dateOfBirth} until {@code untilDay}.
      * If a the {@code dateOfBirth} is not before {@code untilDay}, zero will be returned.
+     * <p>
+     * NB: ABD is not introduced until {@link #ABD_INTRO_DATE}, if untilDay is prior to {@link #ABD_INTRO_DATE}, then
+     * we use {@link #ABD_INTRO_DATE} (01-Apr-2019).
      *
      * @param dateOfBirth the date a person was born.
      * @param untilDay    the date on which to calculate a person's age.
@@ -79,30 +103,29 @@ public final class ABD {
     }
 
     /**
-     * Calculates an Age Based Discount given a provided Age In Years.
+     * Returns the applicable Age Based Discount for the specified age.
      *
      * @param ageInYears the ageInYears from which to calculate the Age Based Discount
      * @return the Age Based Discount percentage
      */
-    public static int calculateAgeBasedDiscountPercentage(int ageInYears) {
-        int abdFactor = Math.max(THIRTY - ageInYears, 0) * 2;
-        return Math.min(abdFactor, MAX_ABD_FACTOR);
+    public static int getAgeBasedDiscount(int ageInYears) {
+        return ABD_PERCENTAGES.getOrDefault(ageInYears, 0);
     }
 
     /**
      * Calculate an Age Based Discount for multiple ages.
      * <p>
-     * This algorithm uses {@link #calculateAgeBasedDiscountPercentage(int)}, for each age, and then takes the average.
+     * This algorithm uses {@link #getAgeBasedDiscount(int)}, for each age, and then takes the average.
      * In practice, this will either only be one, or two age elements, but this function will accept more.
      *
      * @param agesInYears a variadic param containing the ages in years from which to calculate the Age Based Discount
      * @return the Age Based Discount percentage considering all ages.
      */
-    public static int calculateAgeBasedDiscountPercentage(int... agesInYears) {
+    public static int getAgeBasedDiscount(int... agesInYears) {
         Optional<int[]> ages = Optional.ofNullable(agesInYears);
         final int divisor = ages.map(a -> a.length).orElse(1);
         return ages.map(Arrays::stream).orElse(IntStream.empty())
-                .map(ABD::calculateAgeBasedDiscountPercentage)
+                .map(ABD::getAgeBasedDiscount)
                 .reduce(Integer::sum).orElse(0) / divisor;
     }
 
@@ -114,7 +137,7 @@ public final class ABD {
      * @param assessmentDate the ABD assessment date.
      * @return the Age Based Discount percentage.
      */
-    public static int getAbdPercentage(Optional<String> primaryDob, Optional<String> partnerDob, LocalDate assessmentDate) {
+    public static int calculateAgeBasedDiscount(Optional<String> primaryDob, Optional<String> partnerDob, LocalDate assessmentDate) {
         Function<String, Optional<Integer>> calculateABDAge = applicantDob -> Optional.ofNullable(applicantDob)
                 .flatMap(safeParseDate)
                 .map(dob -> calculateAgeInYearsFrom(dob, assessmentDate));
@@ -124,7 +147,7 @@ public final class ABD {
         Optional<Integer> secondaryApplicantAge = partnerDob.flatMap(calculateABDAge);
 
         return secondaryApplicantAge
-                .map(partnerAge -> calculateAgeBasedDiscountPercentage(primaryApplicantAge, partnerAge))
-                .orElseGet(() -> calculateAgeBasedDiscountPercentage(primaryApplicantAge));
+                .map(partnerAge -> getAgeBasedDiscount(primaryApplicantAge, partnerAge))
+                .orElseGet(() -> getAgeBasedDiscount(primaryApplicantAge));
     }
 }
