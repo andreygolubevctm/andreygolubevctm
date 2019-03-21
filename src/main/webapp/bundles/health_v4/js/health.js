5f44e8907ae323746af4ab4c8e4a7a52a3c122d1
Matthew Klemm <matthew.klemm@comparethemarket.com.au>
2019-03-21 13:15:35 +1000

    [HREFORM-466] Fixed duplicate methods in health.js

--- src/main/webapp/bundles/health_v4/js/health.js
+++ src/main/webapp/bundles/health_v4/js/health.js
@@ -885,14 +885,6 @@
         $("#reforms-banner").removeClass("hidden");
     }
 
-    function hideReformsBanner() {
-        $("#reforms-banner").addClass("hidden");
-    }
-
-    function showReformsBanner() {
-        $("#reforms-banner").removeClass("hidden");
-    }
-
     meerkat.modules.register("health", {
         init: initHealth,
         events: moduleEvents,
