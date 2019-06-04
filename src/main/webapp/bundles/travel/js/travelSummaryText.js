7bd41fd5fb66c5f30bbd945d0ae5d3e6f0dca085
Patrick Jones <patrick.jones@comparethemarket.com.au>
2019-06-04 12:42:58 +1000

    cleaned up summary text

--- src/main/webapp/bundles/travel/js/travelSummaryText.js
+++ src/main/webapp/bundles/travel/js/travelSummaryText.js
@@ -75,13 +75,10 @@
 			var durationVal = $('#amtDurationsfs').find('.active').text().trim();
 			var travelTxt = 'to ';
 
-			if (regionTags.length > 1 && regionStr.indexOf('pacific') || regionStr.indexOf('united') || regionStr.indexOf('cook')) {
+			if (regionTags[0].textContent.indexOf('pacific') !== -1 || regionTags[0].textContent.indexOf('united') !== -1) {
 				travelTxt = 'to the ';
-			} else if (regionStr === 'worldwide' || regionStr === 'wwExAmericas' || regionStr === 'asia' || regionStr === 'europe') {
+			} else if (regionStr === 'worldwide' || regionStr === 'asia' || regionStr === 'europe') {
 				travelTxt = '';
-				if (regionStr === 'wwExAmericas') {
-					regionStr = 'Worldwide, excluding Americas';
-				}
 			}
 			txt+="</span> travelling multiple times in one year "+travelTxt+"<span class='highlight'>"+regionStr+"</span> for a maximum single trip duration of <span class='highlight'>"+durationVal+".</span>" +
 				 "\nWe have also inlcuded products that cover longer trips as they may be of greater value.";
