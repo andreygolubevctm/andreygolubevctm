7f35c91f22dfc30d39230cb7bd861efce73e6a5f
Matthew Klemm <matthew.klemm@comparethemarket.com.au>
2019-07-15 10:04:27 +1000

    [CTM-1741] Fixed qip popovers not rendering

--- src/main/webapp/bundles/shared/popovers/js/popovers.deferred.js
+++ src/main/webapp/bundles/shared/popovers/js/popovers.deferred.js
@@ -126,8 +126,7 @@
 					x: settings.element.attr('data-adjust-x') ? parseInt(settings.element.attr('data-adjust-x')) : 0,
 					y: settings.element.attr('data-adjust-y') ? parseInt(settings.element.attr('data-adjust-y')) : 0,
 					scroll: settings.element.attr('data-scroll') != 'false'
-				},
-				viewport: $(window)
+				}
 			},
 			show: {
 			    solo: showSolo,
