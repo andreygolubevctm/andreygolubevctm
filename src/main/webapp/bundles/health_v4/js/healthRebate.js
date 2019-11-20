4d4f0f0a2ab245112ef9f98263b9492b45d3aefe
jamcanete <52897575+jamcanete@users.noreply.github.com>
2019-11-20 10:29:47 +1000

    CTM-2036 Updated rebate text (#36)

--- src/main/webapp/bundles/health_v4/js/healthRebate.js
+++ src/main/webapp/bundles/health_v4/js/healthRebate.js
@@ -171,7 +171,7 @@
             if (!isNaN(rates.rebate) && parseFloat(rates.rebate) > 0) {
 
                 if ($elements.incomeSelect.filter(':checked').length > 0) {
-                    $elements.rebateLegend.html('You are eligible for a ' + rates.rebate + '% rebate.');
+                    $elements.rebateLegend.html('You are eligible for a ' + rates.rebate + '% rebate. This will be applied as a discount to your premiums.');
                 }
 
                 rebate = rates.rebate;
