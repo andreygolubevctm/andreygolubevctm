8b9ebb46c4f9b48bbccff23d5c470d7005d42cc0
Mark Smerdon <12780154+thesm3rdo@users.noreply.github.com>
2020-08-31 11:48:59 +1000

    CTM-3100 fix to ensure CAE is always an integer (truncate - no rounding) (#237)
    * CTM-3100 fix to ensure CAE is always an integer (truncate - no rounding)
    
    * CTM-3100 removed testing output which I thought I'd removed already but hey none of us are perfect hey

--- src/main/webapp/bundles/health/js/healthLHC.js
+++ src/main/webapp/bundles/health/js/healthLHC.js
@@ -286,7 +286,7 @@
             return '65';
         }
 
-        return ((primaryLhc / 2) + 30).toString();
+        return Math.floor(((primaryLhc / 2) + 30)).toString();
     }
 
     function getNewPartnerCAE() {
@@ -304,7 +304,7 @@
             return '65';
         }
 
-        return ((partnerLhc / 2) + 30).toString();
+        return Math.floor(((partnerLhc / 2) + 30)).toString();
     }
 
     function resetNewLHC() {
