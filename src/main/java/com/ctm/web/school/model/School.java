d8d7ee806485c0e071ef622661ede4726c69c4ce
ColinCTM <69825020+ColinCTM@users.noreply.github.com>
2021-10-21 16:18:57 +1000

    Feature/ctm 3965 nib and qantas graduation date of student dependent (#500)
    * back-end callout to provide educational institutions
    
    * Institution Drop Down Feature
    
    * Added graduation date field
    
    * Unit Tests added
    
    * Fixed ^M's in these files
    
    * Wired up the school and graduationDate attribues to health-apply

---
+++ src/main/java/com/ctm/web/school/model/School.java
@@ -0,0 +1,28 @@
+package com.ctm.web.school.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class School {
+    @JsonProperty("Code")
+    private String code;
+    @JsonProperty("CodeDescription")
+    private String codeDescription;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCodeDescription() {
+        return codeDescription;
+    }
+
+    public void setCodeDescription(String codeDescription) {
+        this.codeDescription = codeDescription;
+    }
+}
