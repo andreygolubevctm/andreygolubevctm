a47b225e0f90081e89231ad75b347d6f9f721401
yi-wang-ctm <87353032+yi-wang-ctm@users.noreply.github.com>
2021-11-22 12:36:28 +1000

    Feature/ctm 4266 (#537)
    * Added fallback CoverType function in edge case
    
    * #CTM-4266 fixed the unit test.
    
    Co-authored-by: Matthew Alexander <matthew.alexander@comparethemarket.com.au>

--- src/main/java/com/ctm/web/health/apply/model/request/application/applicant/previousFund/CoverType.java
+++ src/main/java/com/ctm/web/health/apply/model/request/application/applicant/previousFund/CoverType.java
@@ -1,5 +1,6 @@
 package com.ctm.web.health.apply.model.request.application.applicant.previousFund;
 
+import com.ctm.web.health.model.form.Cover;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum CoverType {
@@ -33,6 +34,22 @@ public enum CoverType {
         }
 
         // If no match is found, return null
+        return null;
+    }
+
+
+    /**
+     * Get a CoverType enum value derived from the given healthEverHeld string (Y/N)
+     * @param healthEverHeld Whether the person has held hospital cover, from the xpath `health/healthCover/{person}/healthEverHeld`
+     * @return The 'H' enum value if `healthEverHeld` is 'Y', null if 'N'
+     */
+    public static CoverType fromHealthEverHeld(final String healthEverHeld) {
+        // If the user answered yes to "Have you ever held Hospital Cover?"
+        if(healthEverHeld.equals("Y")) {
+            return CoverType.HOSPITAL;  // Return the HOSPITAL/"H" enum
+        }
+
+        // If the user answered No then return null
         return null;
     }
 }
