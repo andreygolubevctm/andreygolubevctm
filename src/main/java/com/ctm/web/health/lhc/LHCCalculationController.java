3bdf6f0fe1f3156c57ee48935de3d22103654596
andreygolubevctm <82356032+andreygolubevctm@users.noreply.github.com>
2022-04-06 13:12:18 +1000

    Feature/ctm 4682 2 revert 539 deployment (#692)
    * Revert "CTM-4631 Fixing js error in online journey for AUF (#684)"
    
    This reverts commit ee07dc8c5b4c8fc323cdeed9c7c4c07cb2cccaca.
    
    * Revert "Feature/ctm 3957 quote retrieval retain previous fund (#666)"
    
    This reverts commit b852702d35e03bc5e60ff90efc12c7268301b287.
    
    * Revert "Feature/ctm 4311 fix lhc date eofy (#578)"
    
    This reverts commit 2d5688809954da768e02f4a33b05a33883d893f1.

--- src/main/java/com/ctm/web/health/lhc/LHCCalculationController.java
+++ src/main/java/com/ctm/web/health/lhc/LHCCalculationController.java
@@ -2,7 +2,6 @@ package com.ctm.web.health.lhc;
 
 import com.ctm.web.health.lhc.calculation.LHCCalculationStrategy;
 import com.ctm.web.health.lhc.calculation.LHCCalculationStrategyFactory;
-import com.ctm.web.health.lhc.calculation.LHCDateCalculationSupport;
 import com.ctm.web.health.lhc.model.query.LHCBaseDateQuery;
 import com.ctm.web.health.lhc.model.query.LHCCalculationQuery;
 import com.ctm.web.health.lhc.model.response.LHCBaseDateDetails;
@@ -40,7 +39,7 @@ public class LHCCalculationController {
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = MediaType.APPLICATION_JSON_VALUE)
     public LHCCalculationResponse calculateLHC(@RequestBody @Valid LHCCalculationQuery queryWrapper) {
-        LocalDate yesterday = getYesterdayIfNotStartNewFinYear(queryWrapper.getApplicationDate());
+        LocalDate yesterday = getYesterday();
         LHCCalculation primary = new LHCCalculation(LHCCalculationStrategyFactory.getInstance(queryWrapper.getPrimary(), yesterday).calculateLHCPercentage());
         Optional<LHCCalculation> partner = Optional.ofNullable(queryWrapper.getPartner())
                 .map(details -> LHCCalculationStrategyFactory.getInstance(details, yesterday))
@@ -53,12 +52,11 @@ public class LHCCalculationController {
      * Yesterday (or today minus 1) is used for LGC calculation because it needs to be calculated from the last fully
      * completed day to cater for the case that an applicant may purchase cover and would be covered today.
      *
-     * @return yesterday {@code LocalDate.now().minusDays(1)} if now is not 01 July (start of a new financial year)
+     * @return yesterday {@code LocalDate.now().minusDays(1)}
      * @see LocalDate
      */
-    private LocalDate getYesterdayIfNotStartNewFinYear(LocalDate applicationDate) {
-        LocalDate now = applicationDate == null ? LocalDate.now() : applicationDate;
-        return LHCDateCalculationSupport.checkIfStartOfFinancialYear(now) ? now : now.minusDays(1);
+    private LocalDate getYesterday() {
+        return LocalDate.now().minusDays(1);
     }
 
 }
