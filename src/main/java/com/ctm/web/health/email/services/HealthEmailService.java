5ed66e50296f9b33e4d3aa6d2266e77808eaf33e
James Douglas <james.douglas@comparethemarket.com.au>
2022-05-13 16:23:46 +1000

    Feature/ham 172 extras only issue (#723)
    * HAM-172: Updates object mapper config

--- src/main/java/com/ctm/web/health/email/services/HealthEmailService.java
+++ src/main/java/com/ctm/web/health/email/services/HealthEmailService.java
@@ -198,12 +198,11 @@ public class HealthEmailService extends EmailServiceHandler implements BestPrice
 		mailingName = getPageSetting(ProductBrochuresEmailHandler.MAILING_NAME_KEY);
 		ExactTargetEmailSender<HealthProductBrochuresEmailModel> emailSender = new ExactTargetEmailSender<>(pageSettings,transactionId);
 		try {
-			String lastname = Optional.ofNullable(request.getParameter("lastName")).orElse("null");
 			EmailMaster emailDetails = new EmailMaster();
 			emailDetails.setEmailAddress(emailAddress);
 			emailDetails.setOptedInMarketing("Y".equals(request.getParameter("marketing")), VERTICAL);
 			emailDetails.setFirstName(request.getParameter("firstName"));
-			emailDetails.setLastName(lastname.equals("") ? "null" : lastname);
+			emailDetails.setLastName(Optional.ofNullable(request.getParameter("lastName")).orElse(""));
 			emailDetails.setSource("BROCHURE");
 			emailDetails = emailDetailsService.handleReadAndWriteEmailDetails(emailBrochureRequest.transactionId, emailDetails, "ONLINE" ,  ipAddressHandler.getIPAddress(request));
 
