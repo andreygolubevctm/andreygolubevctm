484747d3177d094c83b47d01de2fa7c10a38f8c2
Yi Wang <87353032+yi-wang-ctm@users.noreply.github.com>
2022-06-24 16:04:20 +1000

    #CTM-4925 initialise Xpath Key (#746)

--- src/main/java/com/ctm/web/core/transaction/dao/TransactionDetailsDao.java
+++ src/main/java/com/ctm/web/core/transaction/dao/TransactionDetailsDao.java
@@ -71,7 +71,7 @@ public class TransactionDetailsDao {
 	 * @param transactionId
 	 */
 	private void initialiseXpathSecurity(long transactionId) {
-		if(hasXPathSecurity()) {
+		if(!hasXPathSecurity()) {
 			try {
 				TransactionDao transactionDao = new TransactionDao();
 				long rootId = transactionDao.getRootIdOfTransactionId(transactionId);
